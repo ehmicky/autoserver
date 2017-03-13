6b66e14e7cd497b4a9b2886a51545682ca185985
autoserver-founder <founder@autoserver.io>
2017-03-13 19:29:01 +0100

    Enhange console wrapper

--- src/utilities/console.js
+++ src/utilities/console.js
@@ -1,3 +1,20 @@
 'use strict';
 
-module.exports = console;
+const levels = [
+  'debug',
+  'info',
+  'log',
+  'warn',
+  'error',
+];
+
+const createWrapper = (wrapper, level) => (...args) => {
+  return console[level](...args);
+};
+
+const logWrapper = levels.reduce((wrapper, level) => {
+  wrapper[level] = createWrapper(wrapper, level);
+  return wrapper;
+}, {});
+
+module.exports = logWrapper;
