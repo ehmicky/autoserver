a79ddb345a7d2cc7dc12ac70d61d9acfd9f40ddb
autoserver-founder <founder@autoserver.io>
2017-03-14 12:24:45 +0100

    Modify middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,40 +1,7 @@
 'use strict';
 
-/*
-TODO: modifiers:
-  - bind(MFUNC, this)->MFUNC2: bind context, since chain() rebinds context. If this.next() already exists, uses this._next instead.
-  - repeat(MFUNC)->MFUNC2: repeat accross the chain, e.g. chain(repeat(a), b, c, repeat(d), e, f) -> chain(a, b, a, c, a, d, e, a, d, f)
-*/
 
 const console = require('../utilities/console');
-
-
-/*
-const newFunc = function (label) {
-  return function (input = 'input') {
-    const nextInput = String(Math.random()).replace('.','');
-    console.log(`[${label}] Start with input ${nextInput}`);
-    const val = this.next(nextInput);
-    console.log(`[${label}] Return: ${val}`);
-    return input;
-  };
-};
-
-const alwaysFalse = function () {
-  return false;
-};
-
-setTimeout(function () {
-  chain(
-    newFunc('one'),
-    newFunc('two'),
-    testFunc(alwaysFalse, function () {
-      return this.next('yo');
-    }),
-    newFunc('three')
-  )();
-}, 0);
-*/
 
 
 /**
@@ -46,102 +13,91 @@ setTimeout(function () {
  * The last function calling `this.next()` will actually call a placeholder identity function.
  * Returns a function firing the first (i.e. top-level) middleware. This function can be used itself in other chain(), allowing function composition.
  *
- * @param rawMiddlewares {function|function[]...}: non-function arguments are ignored
+ * @param middlewares {function|function[]...}: arguments that are neither functions nor modifiers are ignored
  * @returns trigger {function}
  */
 const chain = function (...middlewares) {
   // Allow consumer to input an array instead
   middlewares = middlewares[0] instanceof Array ? middlewares[0] : middlewares;
-
   middlewares = middlewares
     // Allow using null or false in input
-    .filter(middleware => typeof middleware === 'function' || middleware instanceof MiddlewareModifier);
-
-  // Make a copy to avoid the manipulation to modify iteration order
-  let processedMiddlewares = middlewares.slice();
-  middlewares
-    // Apply modifiers
-    .forEach(middleware => {
-      if (!middleware instanceof MiddlewareModifier || !middleware.handler) { return; }
-      const currentIndex = processedMiddlewares.findIndex(mdw => mdw === middleware);
-      // A previous modifier might have deleted this modifier
-      if (!currentIndex) { return; }
-      // If returning undefined, do not modify anything
-      processedMiddlewares = middleware.handler(processedMiddlewares, currentIndex) || processedMiddlewares;
-    });
-
-  middlewares = processedMiddlewares
-    // Remove modifiers
-    .filter(middleware => !(middleware instanceof MiddlewareModifier))
-    // Flag middleware that were added by modifiers
-    .map(middleware => {
-      middleware.isInput = middlewares.includes(middleware);
-      return middleware;
-    })
-    // End of iteration function
+    .filter(middleware => typeof middleware === 'function' || middleware instanceof MiddlewareModifier)
+    // End of iteration
     .concat(lastMiddleware)
-    // Iterate from last to first
-    .reverse()
-    // Each middleware point to the next using function.__nextMiddleware
-    .map((middleware, index, allMiddlewares) => {
-      // Binds this.next()
-      const boundMiddleware = bindContext(middleware);
-      // Remember unbounded function, for debugging purpose
-      boundMiddleware.original = middleware;
+    // Make a copy to avoid the manipulation to modify iteration order
+    .slice();
 
-      // Build the chain
-      const previousMiddleware = allMiddlewares[index + 1];
-      if (previousMiddleware) {
-        previousMiddleware.__nextMiddleware = boundMiddleware;
-      }
+  const state = { middlewares };
 
-      return boundMiddleware;
-    })
-    // Put back to normal order
-    .reverse();
-
-  // Starts all the middleware
-  const bootstrap = createBootstrap(middlewares);
-
+  const bootstrap = createBootstrap(state);
   return bootstrap;
 };
 
-// Context bound to every middleware
-const bindContext = function (middleware) {
-  const context = {
-      // Go to next middleware
-      next(...args) {
-        return this.__go(1, ...args);
-      },
-      // Go the nth next middleware. Only used by modifiers
-      __go(offset, ...args) {
-        let nextMiddleware = middleware;
-        for (let i = 0; i < offset; i++) {
-          nextMiddleware = nextMiddleware.__nextMiddleware || nextMiddleware.original.__nextMiddleware || nextMiddleware;
-        }
-        // Do not console.debug() the middleware that were added by modifiers
-        if (middleware.isInput) {
-          console.debug(`Starting new middleware ${nextMiddleware.original.toString().substring(0, 100)}`);
-        }
-        // Call next middleware
-        const returnValue = nextMiddleware(...args);
-        if (middleware.isInput) {
-          console.debug(`Ending middleware ${nextMiddleware.original.toString().substring(0, 100)}`);
-        }
-        return returnValue;
-      },
-  };
-  return middleware.bind(context);
-};
-
 // Starts all the middleware
-const createBootstrap = function (middlewares) {
+const createBootstrap = function (state) {
   return (...args) => {
-    console.debug(`Starting chain of ${middlewares.length} middleware functions`);
-    const returnValue = middlewares[0](...args);
+    console.debug(`Starting chain of ${state.middlewares.length} middleware functions`);
+    const returnValue = callWithContext(state.middlewares[0], state, ...args);
     console.debug('Ending middleware chain');
     return returnValue;
   };
+};
+
+const callWithContext = function (middleware, state, ...args) {
+  // Add this.next to next middleware
+  const context = createContext(middleware, state);
+  // Call next middleware
+  const returnValue = middleware.call(context, ...args);
+  return returnValue;
+};
+
+// Context bound to every middleware
+const createContext = function (middleware, state) {
+  const context = Object.assign({}, middleware.__context, {
+      // Go to next middleware
+      next(...args) {
+        const currentIndex = state.middlewares.findIndex(mdw => mdw === middleware);
+        const next = state.middlewares[currentIndex + 1];
+
+        if (next instanceof MiddlewareModifier) {
+          return applyModifier(next, state, ...args);
+        }
+
+        // Truncated function body
+        const originalFunction = next.__original ? next.__original : next;
+        const functionBody = originalFunction.toString().replace(/\s+/g, ' ').substring(0, 100);
+        console.debug(`Starting new middleware ${functionBody}`);
+        const returnValue = callWithContext(next, state, ...args);
+        console.debug(`Ending new middleware ${functionBody}`);
+
+        return returnValue;
+      },
+  });
+  return context;
+};
+
+// Apply a modifier, i.e. let it change the list of middlewares following that modifier
+const applyModifier = function (modifier, state, ...args) {
+  const modifierIndex = state.middlewares.findIndex(mdw => mdw === modifier);
+  // Pick all the middlewares after the modifier, excluding the last one
+  const nextMiddlewares = state.middlewares
+    .slice(modifierIndex + 1, state.middlewares.length - 1);
+
+  console.debug(`Executing modifier "${modifier.type}"`);
+  // Let modifier decide of the replacement middlewares
+  const modifiedMiddlewares = modifier.handler(nextMiddlewares, ...args);
+  // modifiers can return undefined to mean "unchanged"
+  if (modifiedMiddlewares) {
+    // Replace the middlewares
+    // The last middleware is kept
+    state.middlewares.splice(modifierIndex + 1, nextMiddlewares.length, ...modifiedMiddlewares);
+  }
+
+  // Calls next middleware, which might be a modifier itself
+  const next = state.middlewares[modifierIndex];
+  const context = createContext(next, state);
+  const returnValue = context.next(...args);
+  return returnValue;
 };
 
 // Used as last iteration
@@ -153,24 +109,19 @@ const lastMiddleware = function lastMiddleware(val) {
 /**
  * Modifiers are functions that can be used to manipulate a chain of middleware.
  * E.g. in chain(test(func, middleware), middleware_2, bind(middleware, context)), test() and bind() are modifiers
- * Modifiers are functions that:
- *  - take any argument
- *  - return a new MiddlewareModifier({ type: 'TYPE', handler(...) })
+ * Modifiers are instances of new MiddlewareModifier({ type: 'TYPE', handler(...) })
  * MiddlewareModifier.type:
- *  - only used as an identifier, e.g. if two types of modifiers want to interact with each other
+ *  - only used as an identifier, e.g. for debugging, or if two types of modifiers want to interact with each other
  *  - is usually the same as the variable name
  * MiddlewareModifier.handler(...):
- *  - takes the middleware list as first argument, and returns it modified or not. One can remove or add middleware functions.
- *     - the middleware list is flattened as a single array
- *     - the middleware list include the modifiers themselves
- *     - if a modifier modifies the middleware list, the next modifiers will still receive the modified list as input
- *     - if a modifier removes another one from the list, that modifier won't be fired
- *     - items are iterated using a copy of the original/unchanged middleware list. I.e. modifying it will not change iteration.
- *  - returning undefined is the same as returning the middleware list unchanged
- *  - modifiers also take their position in the (modified) middleware list as second argument
- *  - added middleware functions are regular, except they can:
- *     - use this.__go(number, ...), which is the same as this.skip(...) but can skip several middleware functions
+ *  - fired with the same argument as a middleware function, with an extra argument in the beginning: the "list of next middleware"
+ *  - the "list of next middleware" includes all the coming middleware, but not the modifier itself, nor the last "placeholder" middleware
+ *  - can return a modified "list of next middleware" to modify it, e.g. adding or removing middleware
+ *  - can return undefined to signify "no change"
  * Modifiers only have an effect in the current chain(), not in the upper one (in case of nested chain())
+ * Middleware functions can assign the following properties:
+ *  - __context {object}: to change context
+ *  - __original {function}: when wrapping a function, assign the original one here, so it gets printed by console.debug() (i.e. not the wrapper)
  */
 class MiddlewareModifier {
 
@@ -194,32 +145,96 @@ class MiddlewareModifier {
  * @param middleware {function}
  * @returns modifier {MiddlewareModifier}
  **/
-const testFunc = function (condition, middleware) {
-  const handler = (middlewares, index) => {
-    const jumpFunc = createJumpFunc(condition);
-    middlewares.splice(index, 0, jumpFunc, middleware);
-    return middlewares;
-  };
+const testModifier = function (condition, middleware) {
   const modifier = new MiddlewareModifier({
     type: 'test',
-    handler,
+    handler(middlewares, ...args) {
+      return condition(...args) ? [middleware].concat(middlewares) : middlewares;
+    },
   });
   return modifier;
 }
 
-const createJumpFunc = function(condition) {
-  return function (...args) {
-    if (condition(...args)) {
-      this.next(...args);
-    } else {
-      this.__go(2, ...args);
-    }
-  };
+/**
+ * bind(middleware, context) modifier
+ * Since chain() changes functions' context, middleware.bind(context) does not work. This modifier fixes that.
+ *
+ * @param middleware {function}
+ * @param context {object}
+ * @returns modifier {MiddlewareModifier}
+ **/
+const bindModifier = function (middleware, context) {
+  const modifier = new MiddlewareModifier({
+    type: 'bind',
+    handler(middlewares) {
+      middleware.__context = context;
+      return [middleware].concat(middlewares);
+    },
+  });
+  return modifier;
+}
+
+/**
+ * repeatUnless(condition, middleware) modifier
+ * Repeatedly calls middleware before each non-modifier middleware, unless condition does not apply
+ * Can be stopped by a repeatEnd(middleware) modified.
+ *
+ * @param condition {function}
+ * @param middleware {function}
+ * @param context {object}
+ * @returns modifier {MiddlewareModifier}
+ **/
+const repeatUnlessModifier = function (condition, repeatMiddleware) {
+  const modifier = new MiddlewareModifier({
+    type: 'repeat',
+    handler(middlewares, ...args) {
+      const newMiddlewares = [];
+      let noMoreRepeat = false;
+      middlewares.forEach(middleware => {
+        // Stops iteration on repeatEnd(repeatMiddleware)
+        if (middleware instanceof MiddlewareModifier
+          && middleware.type === 'repeatEnd'
+          && middleware.repeatMiddleware === repeatMiddleware) {
+          noMoreRepeat = true;
+        }
+
+        if (!noMoreRepeat && !middleware.__isRepeat && (!condition || !condition(...args))) {
+          // Need to clone function
+          const clonedRepeatMiddleware = function (...args) {
+            return repeatMiddleware.call(this, ...args);
+          };
+          clonedRepeatMiddleware.__original = repeatMiddleware;
+          clonedRepeatMiddleware.__isRepeat = true;
+
+          newMiddlewares.push(clonedRepeatMiddleware);
+        }
+        newMiddlewares.push(middleware);
+      });
+      return newMiddlewares;
+    },
+  });
+  return modifier;
+}
+
+const repeatEndModifier = function (repeatMiddleware) {
+  const modifier = new MiddlewareModifier({
+    type: 'repeatEnd',
+    handler() {},
+  });
+  modifier.repeatMiddleware = repeatMiddleware;
+  return modifier;
 };
+
+// Same as repeatUnlessModifier, but with no condition
+const repeatModifier = repeatUnlessModifier.bind(null, null);
 
 
 module.exports = {
   chain,
-  test: testFunc,
+  test: testModifier,
+  bind: bindModifier,
+  repeat: repeatModifier,
+  repeatEnd: repeatEndModifier,
+  repeatUnless: repeatUnlessModifier,
 };
 
