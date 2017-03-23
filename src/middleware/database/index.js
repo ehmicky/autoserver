26c3a4f91ba6f37f180d718210ce9fc96f7c24d6
ehmicky <ehmicky@gmail.com>
2017-03-23 19:08:13 +0100

    Remove console message

--- src/middleware/database/index.js
+++ src/middleware/database/index.js
@@ -5,7 +5,6 @@ const queryDatabase = () => async function (input) {
   const { operation, args } = input;
   // Fake data for the moment
   let response;
-  console.log(operation.name);
   if (operation.multiple) {
     response = await [
       { id: 1, name: 'Dog', photo_urls: ['http://dog.com/photo/1', 'http://dog.com/photo/2'], tags: ['adorable'], status: 'happy' },
