9b8a662ed186ec2eec8f79f318f46a41849d1cf3
ehmicky <ehmicky@gmail.com>
2017-03-29 11:15:26 +0200

    Create GraphQL parsing utilities

---
+++ src/middleware/interface/graphql/parse/utilities.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+// Check if a definition is an array
+const isMultiple = function (def) {
+  return def.items !== undefined;
+};
+
+// Gets underlying definition if it is an array, otherwise returns as is
+const getSubDef = function (def) {
+  return isMultiple(def) ? def.items : def;
+};
+
+// Gets either array property, or array item property, with priority for upper level
+const getSubDefProp = function (def, propName) {
+  return def[propName] || getSubDef(def)[propName];
+};
+
+// Retrieves model name from a definition
+const getModelName = function (def) {
+  return getSubDef(def).instanceof;
+};
+
+// Checks whether the definition is associated with a model
+const isModel = function (def) {
+  return getModelName(def) !== undefined;
+};
+
+// Checks whether the definition is associated with a model, or is an array whose items are
+const isDeepModel = function (def) {
+  return getSubDefProp(def, 'instanceof') !== undefined;
+};
+
+
+module.exports = {
+  isMultiple,
+  getSubDef,
+  getSubDefProp,
+  getModelName,
+  isModel,
+  isDeepModel,
+};
