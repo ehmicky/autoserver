121279005895e7a7cc6c5e4773b02e793fcd8ea1
autoserver-founder <founder@autoserver.io>
2017-03-30 17:15:04 +0200

    Move comments

--- src/middleware/database/index.js
+++ src/middleware/database/index.js
@@ -1,6 +1,37 @@
 'use strict';
 
 
+/**
+ * Summary of arguments:
+ *  - {any|any[]} id|ids     - Filter the operation by id
+ *                             For createOne:
+ *                              - it is the id the newly created instance
+ *                              - it is optional, unless there is another nested operation on a submodel
+ *                             Operations on submodels will automatically get filtered by id.
+ *                             If an id is then specified, both filters will be used
+ *  - {object|object[]} data - Attributes to update or create
+ *                             Is an array with createMany, replaceMany or upsertMany
+ *  - {any} filter           - Filter the operation by a specific attribute.
+ *                             The argument name is that attribute name, not `filter`
+ *  - {string} order_by      - Sort results.
+ *                             Value is attribute name, followed by optional + or - for ascending|descending order (default: +)
+ *
+ * Summary of operations:
+ *   findOne(id)
+ *   findMany([filter...])
+ *   deleteOne(id)
+ *   deleteMany([filter...])
+ *   updateOne(data, id)
+ *   updateMany(data[, filter...])
+ *   createOne(data[, id])
+ *   createMany(data[][, ids])
+ *   replaceOne(data, id)
+ *   replaceMany(data[], ids)
+ *   upsertOne(data, id)
+ *   upsertMany(data[], ids)
+ **/
+
+
 const queryDatabase = async function () {
   return async function (input) {
     const { operation, args } = input;
