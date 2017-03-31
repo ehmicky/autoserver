c41d871d650d6f6693087f05a595d7755da9fc49
autoserver-founder <founder@autoserver.io>
2017-03-31 11:25:50 +0200

    Improve require() helper

--- src/utilities/debugging.js
+++ src/utilities/debugging.js
@@ -1,9 +1,18 @@
 'use strict';
 
 
+const requireFile = function (file) {
+  // Make require relative to src/ directory
+  const isPath = file.startsWith('/') || file.startsWith('.');
+  if (isPath) {
+    file = '../../src/' + file;
+  }
+  return require(file);
+};
+
 // So that we can require from dev tools console
 const attachRequire = function () {
-  global.require = require;
+  global.require = requireFile;
 };
 
 
