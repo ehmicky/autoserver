763d92a38800aff05efcfa6b00eab799a3e27fb4
autoserver-founder <founder@autoserver.io>
2017-04-03 10:57:55 +0200

    Separate IDL processing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 'use strict';
 
 
-const { readFileSync } = require('fs');
-
 const { startServer } = require('./server');
-const { getIdl } = require('./idl');
 
 
-const definitions = readFileSync('./examples/schema.yml', { encoding: 'utf-8' });
-const idl = getIdl(definitions);
 startServer({
-  idl,
+  conf: './examples/schema.yml',
 })
 .then(() => {
   global.console.log('Server started');
