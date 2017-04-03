e3ce3c1f6d3a7df47c120a31aee18d851f55c974
ehmicky <ehmicky@gmail.com>
2017-04-03 15:04:33 +0200

    Refactor code

--- src/server/process_options.js
+++ src/server/process_options.js
@@ -1,32 +1,14 @@
 'use strict';
 
 
-const { readFileSync } = require('fs');
-
 const { getIdl } = require('../idl');
-const { EngineStartupError } = require('../error');
 
 
 const processOptions = function (options) {
   const opts = Object.assign({}, options);
-  const idl = processIdl(opts.conf);
+  const idl = getIdl({ conf: opts.conf });
   Object.assign(opts, { idl });
   return opts;
-};
-
-const processIdl = function (idl) {
-  let idlDef;
-
-  if (typeof idl === 'string') {
-    idlDef = idlDef = readFileSync(idl, { encoding: 'utf-8' });
-  } else if (typeof idl === 'object' && idl !== null) {
-    idlDef = idl;
-  } else {
-    throw new EngineStartupError('Missing configuration file or \'conf\' option', { reason: 'MISSING_OPTION' });
-  }
-
-  const processedIdl = getIdl(idlDef);
-  return processedIdl;
 };
 
 
