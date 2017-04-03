f875c2ef5f96bfe0a21ac7b3a71a47f05a1e0a04
ehmicky <ehmicky@gmail.com>
2017-04-03 16:04:21 +0200

    Improve IDL loading

--- src/idl/conf.js
+++ src/idl/conf.js
@@ -7,15 +7,55 @@ const yaml = require('js-yaml');
 const { EngineStartupError } = require('../error');
 
 
+/**
+ * Retrieve the configuration using either:
+ *  - a filename pointing to a JSON or YAML file
+ *  - directly a JavaScript object
+ **/
 const getIdlConf = function ({ conf }) {
   if (typeof conf === 'string') {
-    const idlContent = readFileSync(conf, { encoding: 'utf-8' });
-    return yaml.load(idlContent);
+    const path = conf;
+    const content = getIdlContent({ path });
+    const data = getIdlData({ content, path });
+    return data;
   } else if (conf && conf.constructor === Object) {
     return conf;
   } else {
-    throw new EngineStartupError('Missing configuration file or \'conf\' option', { reason: 'MISSING_OPTION' });
+    throw new EngineStartupError('Missing configuration file or \'conf\' option', { reason: 'CONFIGURATION_LOADING' });
   }
+};
+
+const getIdlContent = function ({ path }) {
+  try {
+    return readFileSync(path, { encoding: 'utf-8' });
+  } catch (exception) {
+    throw new EngineStartupError(exception.message, { reason: 'CONFIGURATION_LOADING' });
+  }
+};
+
+const getIdlData = function ({ content, path }) {
+  try {
+    return yaml.load(content, {
+      // YAML needs to JSON-compatible, since JSON must provide same features as YAML
+      schema: yaml.CORE_SCHEMA,
+      json: true,
+      // Error handling
+      filename: path,
+      onWarning(exception) {
+        throwIdlDataError(exception);
+      },
+    });
+  } catch (exception) {
+    throwIdlDataError(exception);
+  }
+};
+
+const throwIdlDataError = function (exception) {
+  let message = exception.message;
+  if (exception instanceof yaml.YAMLException) {
+    message = `Syntax error in configuration file, ${message}`;
+  }
+  throw new EngineStartupError(message, { reason: 'CONFIGURATION_LOADING' });
 };
 
 
