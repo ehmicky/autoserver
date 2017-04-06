8083cfbcdd88ea5b2c20884937186d332ac91b16
ehmicky <ehmicky@gmail.com>
2017-04-06 17:36:58 +0200

    Make IDL validation reuse IDL validation helper

--- src/idl/validate.js
+++ src/idl/validate.js
@@ -1,25 +1,19 @@
 'use strict';
 
 
-const { mapValues, merge, uniqBy } = require('lodash');
+const { mapValues, merge } = require('lodash');
 const yaml = require('js-yaml');
 const fs = require('fs');
-const Ajv = require('ajv');
-const ajv = new Ajv({ allErrors: true, errorDataPath: 'property', jsonPointers: true, full: true, $data: true });
-// Add future JSON standard keywords
-require('ajv-keywords')(ajv, [ 'if', 'formatMinimum', 'formatMaximum', 'deepRequired', 'deepProperties', ]);
 
-const { EngineStartupError } = require('../error');
+const { validate, memoize } = require('../utilities');
 
 
 // Validate IDL definition against a JSON schema
 const validateIdl = function (idl) {
-  const validateFunc = getValidateFunc();
+  const schema = getSchema();
   const idlCopy = getIdlCopy(idl);
-  const isValid = validateFunc(idlCopy);
-  if (!isValid) {
-    notifyErrors(validateFunc.errors);
-  }
+  const data = { elem: idlCopy, extra: { argName: 'config' } };
+  validate({ schema, data, type: 'idl' });
 };
 
 // Adds some temporary property on IDL, to help validation
@@ -31,27 +25,12 @@ const getIdlCopy = function (idl) {
   return idlCopy;
 };
 
-// Retrieve ajv validate function
-let cachedValidateFunc;
-const getValidateFunc = function () {
-  if (cachedValidateFunc) { return cachedValidateFunc; }
-  const idlSchemaContent = fs.readFileSync('./src/idl/idl_schema.yml');
-  const idlSchema = yaml.load(idlSchemaContent, { schema: yaml.CORE_SCHEMA, json: true });
-  const validateFunc = ajv.compile(idlSchema);
-  cachedValidateFunc = validateFunc;
-  return validateFunc;
-};
-
-// Throw error on IDL invalidation
-const notifyErrors = function (errors) {
-  // Those rules create double error messages, too verbose
-  const verboseRules = ['anyOf', 'allOf', 'not', 'oneOf', '$merge', '$patch'];
-  errors = errors.filter(error => !verboseRules.includes(error.keyword));
-  // When in `anyOf` or similar rules, duplicated messages are created
-  const uniqueErrors = uniqBy(errors, 'message');
-  const message = '\n' + ajv.errorsText(uniqueErrors, { separator: '\n', dataVar: 'config' });
-  throw new EngineStartupError(message, { reason: 'CONFIGURATION_INVALID' });
-};
+// Retrieve IDL schema
+const getSchema = memoize(function () {
+  const schemaContent = fs.readFileSync('./src/idl/idl_schema.yml');
+  const schema = yaml.load(schemaContent, { schema: yaml.CORE_SCHEMA, json: true });
+  return schema;
+});
 
 
 module.exports = {
