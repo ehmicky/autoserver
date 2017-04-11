061c675235884fc064edb92a9d6ea82f57526cae
ehmicky <ehmicky@gmail.com>
2017-04-11 14:20:56 +0200

    Separate YAML loading as a utility

--- src/idl/conf.js
+++ src/idl/conf.js
@@ -4,7 +4,7 @@
 const yaml = require('js-yaml');
 
 const { EngineStartupError } = require('../error');
-const { fs: { readFileAsync } } = require('../utilities');
+const { getYaml } = require('../utilities');
 
 
 /**
@@ -14,48 +14,16 @@ const { fs: { readFileAsync } } = require('../utilities');
  **/
 const getIdlConf = async function ({ conf }) {
   if (typeof conf === 'string') {
-    const path = conf;
-    const content = await getIdlContent({ path });
-    const data = getIdlData({ content, path });
-    return data;
+    try {
+      return await getYaml({ path: conf });
+    } catch ({ message }) {
+      throw new EngineStartupError(message, { reason: 'CONFIGURATION_LOADING' });
+    }
   } else if (conf && conf.constructor === Object) {
     return conf;
   } else {
     throw new EngineStartupError('Missing configuration file or \'conf\' option', { reason: 'CONFIGURATION_LOADING' });
   }
-};
-
-const getIdlContent = async function ({ path }) {
-  try {
-    return await readFileAsync(path, { encoding: 'utf-8' });
-  } catch (exception) {
-    throw new EngineStartupError(exception.message, { reason: 'CONFIGURATION_LOADING' });
-  }
-};
-
-const getIdlData = function ({ content, path }) {
-  try {
-    return yaml.load(content, {
-      // YAML needs to JSON-compatible, since JSON must provide same features as YAML
-      schema: yaml.CORE_SCHEMA,
-      json: true,
-      // Error handling
-      filename: path,
-      onWarning(exception) {
-        throwIdlDataError(exception);
-      },
-    });
-  } catch (exception) {
-    throwIdlDataError(exception);
-  }
-};
-
-const throwIdlDataError = function (exception) {
-  let message = exception.message;
-  if (exception instanceof yaml.YAMLException) {
-    message = `Syntax error in configuration file, ${message}`;
-  }
-  throw new EngineStartupError(message, { reason: 'CONFIGURATION_LOADING' });
 };
 
 
