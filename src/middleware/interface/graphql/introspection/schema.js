043a015c8b3e9937574828cfa1dfd73251327c43
autoserver-founder <founder@autoserver.io>
2017-04-12 12:28:23 +0200

    Temp commit

--- src/middleware/interface/graphql/introspection/schema.js
+++ src/middleware/interface/graphql/introspection/schema.js
@@ -1,8 +1,51 @@
 'use strict';
 
 
+const { GraphQLSchema } = require('graphql');
+const { mapValues } = require('lodash');
+
+const { GeneralCache } = require('../../../../utilities');
+const { stringify } = require('circular-json');
+const { getType } = require('./types');
+const { getModelsByMethod } = require('./models');
+
+
+const schemaCache = new GeneralCache();
+
+// Returns GraphQL schema
 const getSchema = function ({ idl }) {
-  return {};
+  const schemaCacheKey = stringify(idl);
+  if (schemaCache.exists(schemaCacheKey)) {
+    return schemaCache.get(schemaCacheKey);
+  }
+
+  // Each schema gets its own cache instance, to avoid leaking
+  const cache = new GeneralCache();
+
+  // Apply `getType` to each top-level operation, i.e. Query and Mutation
+  const schemaFields = mapValues(rootDef, (def, methodName) => {
+    // Adds query|mutation.properties
+    def.properties = getModelsByMethod(methodName, { idl });
+    // Returns query|mutation type
+    return getType(def, { cache });
+  });
+
+  const schema = new GraphQLSchema(schemaFields);
+  schemaCache.set(schemaCacheKey, schema);
+  return schema;
+};
+
+const rootDef = {
+  query: {
+    propName: 'query',
+    type: 'object',
+    description: 'Fetches information about different entities',
+  },
+  mutation: {
+    propName: 'mutation',
+    type: 'object',
+    description: 'Modifies information about different entities',
+  },
 };
 
 
