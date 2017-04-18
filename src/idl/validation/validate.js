b218e615f00aa748f6178492b7115e45785cfd68
autoserver-founder <founder@autoserver.io>
2017-04-18 16:01:55 +0200

    Validate that IDL file has no circular references

--- src/idl/validation/validate.js
+++ src/idl/validation/validate.js
@@ -1,10 +1,11 @@
 'use strict';
 
 
-const { merge } = require('lodash');
+const { merge, each } = require('lodash');
 const yaml = require('js-yaml');
 
 const { validate, memoize, fs: { readFileAsync } } = require('../../utilities');
+const { EngineError } = require('../../error');
 const IDL_SCHEMA_PATH = './src/idl/validation/idl_schema.yml';
 
 
@@ -12,6 +13,7 @@ const IDL_SCHEMA_PATH = './src/idl/validation/idl_schema.yml';
 const validateIdl = async function (idl) {
   const schema = await getSchema();
   const idlCopy = getIdlCopy(idl);
+  validateCircularRefs({ value: idl });
   validate({ schema, data: idlCopy, reportInfo: { type: 'idl', dataVar: 'config' } });
 };
 
@@ -27,6 +29,28 @@ const getSchema = memoize(async function () {
   const schema = yaml.load(schemaContent, { schema: yaml.CORE_SCHEMA, json: true });
   return schema;
 });
+
+/**
+ * There should be no circular references.
+ * They may be introduced by e.g. dereferencing JSON references `$ref` or YAML anchors `*var`
+ * The only legal way to introduce circular references is by using `model` property, which is dereferenced later.
+ **/
+ const validateCircularRefs = function ({ value, path = 'schema', pathSet = new WeakSet() }) {
+  if (pathSet.has(value)) {
+    throw new EngineError(`Schema cannot contain circular references: ${path}`, { reason: 'IDL_VALIDATION' });
+  }
+  if (typeof value === 'object') {
+    pathSet.add(value);
+  }
+
+  // Recursion
+  if (!value || !(value instanceof Array || value.constructor === Object)) { return; }
+  each(value, (child, childKey) => {
+    const pathPart = value instanceof Array ? `[${childKey}]` : `.${childKey}`;
+    const childPath = `${path}${pathPart}`;
+    validateCircularRefs({ value: child, path: childPath, pathSet });
+  });
+};
 
 
 module.exports = {
