3f4c5b3127805cb07b98662e0be3477d0b9dfe4b
autoserver-founder <founder@autoserver.io>
2017-04-20 11:44:46 +0200

    Check for missing properties

--- src/middleware/interface/graphql/query/resolver/nested_model.js
+++ src/middleware/interface/graphql/query/resolver/nested_model.js
@@ -10,28 +10,38 @@ const { EngineError } = require('../../../../../error');
 
 // Resolver for nested model operations
 const nestedModelResolver = function ({ name, modelsMap, parent, args }) {
-  const { attrName, opType } = parseName({ name });
+  const { model, attrName, opType } = getNestedProp({ modelsMap, parent, name });
+
   // If it is a normal attribute which just returns its parent value
-  if (!attrName || !opType) {
+  if (!model) {
     return attributeResolver({ parent, name });
   }
 
-  // Retrieves parent model
-  const { modelName: parentModel, opType: parentOpType } = getParentModel(parent);
-  // Retrieves nested model
-  const model = modelsMap[parentModel] && modelsMap[parentModel][attrName];
-
-  // This means tried to do a nested operation that does not exist
-  // Or that nested types have different opType than parent
-  if (!model || parentOpType !== opType) {
-    throw new EngineError(`In ${parentModel} model, attribute ${name} does not exist`, { reason: 'INPUT_VALIDATION' });
-  }
-
   const { multiple, model: modelName } = model;
-
   const directReturn = addNestedId({ parent, name: attrName, multiple, args, opType });
 
   return { multiple, modelName, opType, directReturn };
+};
+
+// Retrieves nested property, which can be a model or a simple attribute
+const getNestedProp = function ({ modelsMap, parent, name }) {
+  // Retrieves parent model
+  const { modelName: parentModel, opType: parentOpType } = getParentModel(parent);
+
+  // Retrieves nested property
+  const { attrName, opType } = parseName({ name });
+  const isModel = attrName && opType;
+  const prop = modelsMap[parentModel] && modelsMap[parentModel][isModel ? attrName : name];
+
+  // This means tried to do a nested operation that does not exist
+  if (!prop
+  // This means nested operation is not a simple attribute, or that it has different opType than parent
+  || (isModel && (!prop.model || parentOpType !== opType))) {
+    throw new EngineError(`In ${parentModel} model, attribute ${name} does not exist`, { reason: 'INPUT_VALIDATION' });
+  }
+
+  const model = isModel ? prop : null;
+  return { model, attrName, opType };
 };
 
 /**
