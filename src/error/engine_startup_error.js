2ce713338d2e565c54702d1a959793f4a2b4bf61
ehmicky <ehmicky@gmail.com>
2017-04-21 11:59:15 +0200

    Change EngineStartupError again

--- src/error/engine_startup_error.js
+++ src/error/engine_startup_error.js
@@ -7,6 +7,12 @@ const { ExtendableError } = require('./extendable_error');
 class EngineStartupError extends ExtendableError {
 
   constructor(message, opts) {
+    // Rename options so it looks the same as other errors when printed on console
+    opts.type = opts.reason;
+    delete opts.reason;
+    opts.details = opts.innererror && opts.innererror.stack;
+    delete opts.innererror;
+
     super(message, Object.assign(opts, {
       allowedOpts: ['type', 'details'],
       requiredOpts: ['type'],
--- src/idl/ref_parsing.js
+++ src/idl/ref_parsing.js
@@ -2,14 +2,14 @@
 
 
 const { dereferenceRefs } = require('../utilities');
-const { EngineError } = require('../error');
+const { EngineStartupError } = require('../error');
 
 
 const resolveRefs = async function ({ idl }) {
   try {
     return await dereferenceRefs(idl);
-  } catch ({ message }) {
-    throw new EngineError(`Could not resolve references '$ref': ${message}`, { reason: 'IDL_SYNTAX_ERROR' });
+  } catch (innererror) {
+    throw new EngineStartupError('Could not resolve references \'$ref\'', { reason: 'IDL_SYNTAX_ERROR', innererror });
   }
 };
 
