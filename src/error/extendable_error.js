c7ef30c8e2cedd7250061b01bd3d6cf7c1b25b2a
autoserver-founder <founder@autoserver.io>
2017-04-21 11:41:46 +0200

    Improve EngineStartupError

--- src/error/extendable_error.js
+++ src/error/extendable_error.js
@@ -1,10 +1,16 @@
 'use strict';
 
 
+const { difference } = require('lodash');
+
+
 class ExtendableError extends Error {
 
   constructor(message, opts = {}) {
     super(message);
+
+    this.checkSignature(opts);
+
     this.name = this.constructor.name;
 
     // Adds stack trace
@@ -15,6 +21,31 @@ class ExtendableError extends Error {
     }
 
     Object.assign(this, opts);
+  }
+
+  // Make sure signature is correct
+  checkSignature(opts) {
+    const optsKeys = Object.keys(opts);
+    const { allowedOpts, requiredOpts } = opts;
+    const otherOpts = ['allowedOpts', 'requiredOpts'];
+    // Retrieve child constructor
+    const ThisError = this.constructor;
+
+    // Check whitelisted options
+    const nonAllowedOpts = difference(optsKeys, allowedOpts.concat(otherOpts));
+    if (nonAllowedOpts.length > 0) {
+      throw new ThisError(`Cannot use options ${nonAllowedOpts} when throwing ${ThisError.name}`, {
+        type: 'WRONG_EXCEPTION',
+      });
+    }
+
+    // Check required options
+    const missingOpts = difference(requiredOpts, optsKeys);
+    if (missingOpts.length > 0) {
+      throw new ThisError(`Must specify options ${missingOpts} when throwing ${ThisError.name}`, {
+        type: 'WRONG_EXCEPTION',
+      });
+    }
   }
 
 }
