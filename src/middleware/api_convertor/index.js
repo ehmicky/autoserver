b05bab44a2365dc56b325af392a6484df2b49054
ehmicky <ehmicky@gmail.com>
2017-04-24 18:56:40 +0200

    Pass params to database layer

--- src/middleware/api_convertor/index.js
+++ src/middleware/api_convertor/index.js
@@ -3,8 +3,8 @@
 
 const apiConvertor = async function () {
   return async function (input) {
-    const { api: { action, args, modelName }, info } = input;
-    const response = await this.next({ action, args, modelName, info });
+    const { api: { action, args, modelName }, info, params } = input;
+    const response = await this.next({ action, args, modelName, info, params });
     return response;
   };
 };
