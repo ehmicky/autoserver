2d01b0f434d7e02eeec42572fd2908fbb8c50bdd
ehmicky <ehmicky@gmail.com>
2017-04-24 18:56:57 +0200

    Improve JSL layer

--- src/middleware/jsl/index.js
+++ src/middleware/jsl/index.js
@@ -8,8 +8,14 @@ const { EngineError } = require('../../error');
 
 const handleJsl = async function () {
   return await function (input) {
-    const { args } = input;
-    args.filter = processJsl({ value: args.filter });
+    const { args, info: { ip }, params } = input;
+    const contextInfo = { ip, params };
+    if (args.filter) {
+      args.filter = processJsl({ value: args.filter, contextInfo });
+    }
+    if (args.data) {
+      args.data = processJsl({ value: args.data, contextInfo });
+    }
 
     const response = this.next(input);
     return response;
@@ -17,21 +23,21 @@ const handleJsl = async function () {
 };
 
 // Transform value if it is JSL, otherwise returns as is
-const processJsl = function ({ value }) {
+const processJsl = function ({ value, contextInfo }) {
   if (!value) { return value; }
 
   // Recursion over objects and arrays
   if (value.constructor === Object) {
-    return mapValues(value, child => processJsl({ value: child }));
+    return mapValues(value, child => processJsl({ value: child, contextInfo }));
   }
   if (value instanceof Array) {
-    return value.map(child => processJsl({ value: child }));
+    return value.map(child => processJsl({ value: child, contextInfo }));
   }
 
   // Process anything that contains $variables
   if (!testJsl({ value })) { return value; }
 
-  const context = getContext();
+  const context = getContext(contextInfo);
 
   try {
     value = evalJsl({ value, context });
@@ -42,9 +48,22 @@ const processJsl = function ({ value }) {
 };
 
 // Looks for unescaped `$` to find $variables
-const jslRegExp = /(((?:[^\\])\$)|(^\$))/g;
+const jslRegExp = /((?:(?:[^\\])\$)|(?:^\$))([^{]|$)/g;
 // Test whether a value looks like JSL
 const testJsl = ({ value }) => typeof value === 'string' && jslRegExp.test(value);
+
+// Values available as `$variable` in JSL
+// They are uppercase to avoid name conflict with attributes
+const getContext = function ({ ip, params }) {
+  // Context-related variables in JSL
+  const NOW = (new Date()).toISOString();
+
+  // Request-related variables in JSL
+  const IP = ip;
+  const PARAMS = params;
+
+  return { NOW, IP, PARAMS };
+};
 
 // Execute JSL statements
 const evalJsl = function ({ value, context }) {
@@ -53,20 +72,13 @@ const evalJsl = function ({ value, context }) {
   /* eslint-enable no-unused-vars */
 
   // Replace $var by $.var
-  value = value.replace(jslRegExp, '$1.');
+  value = value.replace(jslRegExp, '$1.$2');
 
   // Beware local (and global) variables are available inside eval(), i.e. must keep it to a minimal
   // TODO: this is highly insecure. E.g. value 'while (true) {}' would freeze the whole server. Also it has access to
   // global variables, including global.process. Problem is that alternatives are much slower, so we need to find a solution.
   const newValue = eval(value);
   return newValue;
-};
-
-// Values available as `$variable` in JSL
-const getContext = function () {
-  const context = {};
-  context.now = (new Date()).toISOString();
-  return context;
 };
 
 
