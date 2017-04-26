a5dc00f1819eb2f84bb5197fff54981326fd18ac
ehmicky <ehmicky@gmail.com>
2017-04-26 16:40:22 +0200

    Add schema.writeOnce

--- src/middleware/database/execute.js
+++ src/middleware/database/execute.js
@@ -1,18 +1,27 @@
 'use strict';
 
 
+const { chain, pickBy } = require('lodash');
+
 // Fake database for the moment
 const database = require('./data.json');
 const { fireAction } = require('./actions');
 
 
-const executeDatabaseAction = async function () {
+const executeDatabaseAction = async function ({ idl: { models } }) {
+  // Gets a map of models' writeOnce attributes, e.g. { my_model: ['my_writeonce_attribute', ...], ... }
+  const writeOnceMap = chain(models)
+    .pickBy(modelIdl => modelIdl && modelIdl.properties)
+    .mapValues(modelIdl => Object.keys(pickBy(modelIdl.properties, prop => prop.writeOnce)))
+    .value();
+
   return async function (input) {
     const { action, args: { order_by: orderBy, data, filter } = {}, modelName, info, params } = input;
     const collection = database[modelName];
     collection.modelName = modelName;
 
-    const response = fireAction({ action, modelName, collection, filter, orderBy, data, info, params });
+    const writeOnceAttributes = writeOnceMap[modelName];
+    const response = fireAction({ action, modelName, collection, filter, orderBy, data, info, params, writeOnceAttributes });
     return response;
   };
 };
