aff4c9bbac520f53f72fd44e733be634deb1bed9
autoserver-founder <founder@autoserver.io>
2017-04-26 14:30:10 +0200

    Allow disabling idempotency check with attribute.idempotent false

--- src/middleware/transform/idempotency.js
+++ src/middleware/transform/idempotency.js
@@ -11,14 +11,18 @@ const { transformInput, transformOutput } = require('./transformer');
 // I.e. when transform[Out] is applied twice, it should return same result as when applied once
 // The reason is: transforms are meant to bring the value to a 'stable' state, not to augment it. Otherwise, it would
 // break basic CRUD semantics clients expect when it comes to request idempotency.
+// Can be turned off using `attribute.idempotent` false in IDL file
 // TODO: do this during IDL validation instead.
-const checkIdempotency = function ({ value, transformArgs, modelName }) {
+const checkIdempotency = function ({ value, transformArgs, modelName, propsIdl = {} }) {
   value = value instanceof Array ? value : [value];
   const beforeInput = value.map(val => merge({}, val));
   const afterInput = transformInput(Object.assign({ value: beforeInput }, transformArgs));
   const afterOutput = transformOutput(Object.assign({ value: afterInput }, transformArgs));
   value.forEach((val, key) => {
     each(val, (attr, attrName) => {
+      // Do not perform this check if schema.idempotent is false
+      if (propsIdl[attrName] && propsIdl[attrName].idempotent === false) { return; }
+
       const afterAttr = afterOutput[key][attrName];
       if (!isEqual(attr, afterAttr)) {
         throw new EngineError(`${modelName}.${attrName} transform is not idempotent, \
