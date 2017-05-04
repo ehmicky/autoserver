040f71eae09501d0c051fedbbe6b61021381ae89
autoserver-founder <founder@autoserver.io>
2017-05-04 15:39:26 +0200

    Temporary commit to move default attributes to plugins

--- src/idl/plugins.js
+++ src/idl/plugins.js
@@ -1,32 +1,45 @@
 'use strict';
 
 
+const { addDefaultAttributes } = require('./default_attributes');
+
+
 // Plugins are functions that take `idl` as input, and returns a modified IDL as output
 // They can also take an `opts` parameter
 // Use cases can be: adding a attribute to each model, extending core functionalities, etc.
 const applyPlugins = async function ({ idl }) {
-  const { plugins } = idl;
-  if (!plugins || !(plugins instanceof Array)) { return idl; }
+  const plugins = !idl.plugins || !(idl.plugins instanceof Array) ? [] : idl.plugins;
+
+  const defaultBuiltinPlugins = Object.entries(builtinPlugins)
+    // Not the plugins that are overriden
+    .filter(([name]) => !plugins.some(({ plugin }) => plugin === name))
+    .map(([, value]) => value);
 
   // Apply each idl.plugins as FUNC({ idl }) returning idl
-  for (const { plugin, enabled = true, opts = {} } of plugins) {
+  const allPlugins = plugins.concat(defaultBuiltinPlugins);
+  for (let pluginConf of allPlugins) {
+    if (typeof pluginConf.plugin === 'string') {
+      Object.assign(pluginConf, builtinPlugins[plugin]);
+    }
+    const { plugin, enabled = true, opts = {} } = pluginConf;
+
     // Plugins are only enabled if specified in `idl.plugins`.
     // But builtin plugins, or plugins added by other plugins, need to be manually disabled if desired.
     if (!enabled) { continue; }
 
-    idl = await applyPlugin({ idl, plugin, opts });
+    // Plugin is either a function, or a string (for builtin plugins)
+    const pluginFunc = typeof plugin === 'string' ? builtinPlugins[plugin] : plugin;
+
+    idl = await pluginFunc({ idl, opts });
   }
 
   return idl;
 };
 
-const applyPlugin = async function ({ idl, plugin, opts }) {
-  // Plugin is either a function, or a string (for builtin plugins)
-  const pluginFunc = typeof plugin === 'string' ? builtinPlugins[plugin] : plugin;
-  return await pluginFunc({ idl, opts });
-};
-
 const builtinPlugins = {
+  timestamps: {
+    plugin: addDefaultAttributes,
+  },
 };
 
 
