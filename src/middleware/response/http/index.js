cadc421aeb14c1ee5d4d58d5cb1560686a0d49e8
ehmicky <ehmicky@gmail.com>
2017-05-08 16:27:24 +0200

    Use the application/x-resource|collection+json content-type

--- src/middleware/response/http/index.js
+++ src/middleware/response/http/index.js
@@ -5,28 +5,51 @@ const { httpBody } = require('../../../parsing');
 const { EngineError } = require('../../../error');
 
 
+// Sends the HTTP response at the end of the request
 const httpSendResponse = async function () {
   return async function (input) {
     const { res } = input;
     const response = await this.next(input);
     const { type, content } = response;
 
-    if (content && type) {
-      if (type === 'object') {
-        httpBody.send.json({ res, message: content });
-      } else if (type === 'html') {
-        httpBody.send.html({ res, message: content });
-      } else if (type === 'text') {
-        httpBody.send.text({ res, message: content });
-      } else {
-        throw new EngineError('Server tried to respond with an unsupported content type', { reason: 'WRONG_RESPONSE' });
-      }
-    } else {
+    if (!content || !type) {
       throw new EngineError('Server sent an empty response', { reason: 'WRONG_RESPONSE' });
     }
 
+    // Use different logic according to the content type
+    const handler = handlers[type];
+    if (!handler) {
+      throw new EngineError('Server tried to respond with an unsupported content type', { reason: 'WRONG_RESPONSE' });
+    }
+    handler({ res, message: content });
+
     return response;
   };
+};
+
+// Each content type is sent differently
+const handlers = {
+
+  model({ res, message }) {
+    httpBody.send.json({ res, message, contentType: 'application/x-resource+json' });
+  },
+
+  collection({ res, message }) {
+    httpBody.send.json({ res, message, contentType: 'application/x-collection+json' });
+  },
+
+  object({ res, message }) {
+    httpBody.send.json({ res, message });
+  },
+
+  html({ res, message }) {
+    httpBody.send.html({ res, message });
+  },
+
+  text({ res, message }) {
+    httpBody.send.text({ res, message });
+  },
+
 };
 
 
