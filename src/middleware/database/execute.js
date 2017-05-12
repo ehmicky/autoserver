77ab0ce677d9997872bc0736ed72f9c86cec56a6
autoserver-founder <founder@autoserver.io>
2017-05-12 13:15:30 +0200

    Add limits to database layer

--- src/middleware/database/execute.js
+++ src/middleware/database/execute.js
@@ -16,13 +16,13 @@ const executeDatabaseAction = async function ({ idl: { models } }) {
     .value();
 
   return async function executeDatabaseAction(input) {
-    const { action, args: { order_by: orderBy, dry_run: dryRun, data, filter } = {}, modelName, info, params } = input;
+    const { action, args: { order_by: orderBy, limit, dry_run: dryRun, data, filter } = {}, modelName, info, params } = input;
     const { ip, timestamp, actionType, helpers, variables } = info;
     const jslInput = { helpers, variables, requestInput: { ip, timestamp, params }, interfaceInput: { actionType } };
     const collection = database[modelName];
 
     const writeOnceAttributes = writeOnceMap[modelName];
-    const opts = { jslInput, orderBy, writeOnceAttributes, dryRun, modelName };
+    const opts = { jslInput, orderBy, limit, dryRun, writeOnceAttributes, modelName };
     const response = fireAction({ action, collection, filter, data, opts });
     return response;
   };
