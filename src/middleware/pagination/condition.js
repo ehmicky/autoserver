7cdb4a45553e35266c7b597eadb2e047e6384011
autoserver-founder <founder@autoserver.io>
2017-05-12 13:20:00 +0200

    Limit pagination to some actions

---
+++ src/middleware/pagination/condition.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+// Whether consumers can specify all pagination arguments, including args.page_size, args.before|after
+// Implies output pagination
+const allowFullPagination = function ({ args: { page_size: pageSize }, action }) {
+  return fullPaginationActions.includes(action) && !isPaginationDisabled({ pageSize });
+};
+const fullPaginationActions = ['findMany'];
+
+// Whether output will be paginated
+const mustPaginateOutput = function ({ args: { page_size: pageSize }, action }) {
+  return outputPaginationActions.includes(action) && !isPaginationDisabled({ pageSize });
+};
+const outputPaginationActions = ['findMany', 'deleteMany', 'updateMany'];
+
+// Using args.page_size 0 or defaultPageSize 0 disables pagination
+const isPaginationDisabled = function ({ pageSize }) {
+  return pageSize === 0 || pageSize === undefined;
+};
+
+module.exports = {
+  allowFullPagination,
+  mustPaginateOutput,
+};
