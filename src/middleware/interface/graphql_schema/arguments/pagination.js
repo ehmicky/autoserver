b2074641df315e121d778efb7a243d01ba059006
autoserver-founder <founder@autoserver.io>
2017-05-12 14:58:09 +0200

    Add GraphQL schema description for page_size for deleteMany and updateMany

--- src/middleware/interface/graphql_schema/arguments/pagination.js
+++ src/middleware/interface/graphql_schema/arguments/pagination.js
@@ -5,20 +5,27 @@ const { GraphQLInt, GraphQLString } = require('graphql');
 
 
 // Pagination arguments
-const paginationActionTypes = ['find'];
+const paginationActionTypes = ['find', 'update', 'delete'];
+const fullPaginationActionTypes = ['find'];
 const getPaginationArgument = function ({ action: { actionType, multiple }, defaultPageSize, maxPageSize }) {
-  // Only with safe operations that return an array, i.e. only with findMany
+  // Only with operations that return an array and do not provide array of data, i.e. only with findMany, deleteMany and
+  // updateMany
   if (!(paginationActionTypes.includes(actionType) && multiple)) { return; }
 
-  return {
+  const paginationArgs = {
     page_size: {
       type: GraphQLInt,
       description: `Sets pagination size.
-Using 0 disables pagination.
-Maximum: ${maxPageSize}`,
+  Using 0 disables pagination.
+  Maximum: ${maxPageSize}`,
       defaultValue: defaultPageSize,
-    },
+    }
+  };
 
+  // Only with safe operations that return an array, i.e. only with findMany
+  if (!(fullPaginationActionTypes.includes(actionType) && multiple)) { return paginationArgs; }
+
+  return Object.assign(paginationArgs, {
     after: {
       type: GraphQLString,
       description: `Retrieves next pagination batch, using the previous response's last model's 'token'.
@@ -38,7 +45,7 @@ Using '' means 'from the end'`,
 Starts at 1.
 Cannot be used with 'before' or 'after'`,
     },
-  };
+  });
 };
 
 
