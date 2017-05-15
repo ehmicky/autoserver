e0c6302390708d1abd31519b4ab8b5bf23e2041a
autoserver-founder <founder@autoserver.io>
2017-05-15 18:30:43 +0200

    Allow system defaults for other than args

--- src/middleware/system_defaults/defaults.js
+++ src/middleware/system_defaults/defaults.js
@@ -6,37 +6,41 @@
 //  - actions are whitelisted
 //  - value is the default value. Can be a function taking the server options as first argument
 const defaults = {
-  filter: {
-    actions: ['findMany', 'deleteMany', 'updateMany'],
-    value: '(true)',
-  },
+  args: {
+    filter: {
+      actions: ['findMany', 'deleteMany', 'updateMany'],
+      value: '(true)',
+    },
 
-  order_by: {
-    actions: ['findMany', 'deleteMany', 'updateMany', 'upsertMany', 'replaceMany', 'createMany'],
-    value: 'id+',
-  },
+    order_by: {
+      actions: ['findMany', 'deleteMany', 'updateMany', 'upsertMany',
+        'replaceMany', 'createMany'],
+      value: 'id+',
+    },
 
-  dry_run: {
-    actions: ['deleteOne', 'deleteMany', 'updateOne', 'updateMany', 'upsertOne', 'upsertMany', 'replaceOne', 'replaceMany',
-    'createOne', 'createMany'],
-    value: false,
-  },
+    dry_run: {
+      actions: ['deleteOne', 'deleteMany', 'updateOne', 'updateMany',
+        'upsertOne', 'upsertMany', 'replaceOne', 'replaceMany', 'createOne',
+        'createMany'],
+      value: false,
+    },
 
-  page_size: {
-    actions: ['findMany', 'deleteMany', 'updateMany'],
-    value: ({ opts: { defaultPageSize } }) => defaultPageSize,
-    // Only if pagination is enabled
-    test: ({ opts: { defaultPageSize } }) => defaultPageSize !== 0,
-  },
+    page_size: {
+      actions: ['findMany', 'deleteMany', 'updateMany'],
+      value: ({ opts: { defaultPageSize } }) => defaultPageSize,
+      // Only if pagination is enabled
+      test: ({ opts: { defaultPageSize } }) => defaultPageSize !== 0,
+    },
 
-  after: {
-    actions: ['findMany'],
-    value: '',
-    // Only if pagination is enabled, and arg.before|page is not specified
-    test: ({ opts: { defaultPageSize }, input: { args: { before, page } } }) =>
-      defaultPageSize !== 0 &&
-        before === undefined &&
-        page === undefined
+    after: {
+      actions: ['findMany'],
+      value: '',
+      // Only if pagination is enabled, and arg.before|page is not specified
+      test: ({
+        opts: { defaultPageSize },
+        input: { args: { before, page } },
+      }) => defaultPageSize !== 0 && before === undefined && page === undefined
+    },
   },
 };
 
--- src/middleware/system_defaults/handler.js
+++ src/middleware/system_defaults/handler.js
@@ -1,16 +1,16 @@
 'use strict';
 
 
+const { merge } = require('lodash');
+
 const { defaults } = require('./defaults');
 
 
 // Apply system-defined defaults to input, including input arguments
 const systemDefaults = async function (opts) {
   return async function systemDefaults(input) {
-    const { args } = input;
-
-    const defaultArgs = getDefaultArgs({ opts, input });
-    Object.assign(args, defaultArgs);
+    const newInput = getDefaultArgs({ opts, input });
+    merge(input, newInput);
 
     const response = await this.next(input);
     return response;
@@ -19,19 +19,26 @@ const systemDefaults = async function (opts) {
 
 // Retrieve default arguments
 const getDefaultArgs = function ({ opts, input }) {
-  const { args, action } = input;
-  // Iterate through every possible default argument
+  const { action } = input;
+  // Iterate through every possible default value
   return Object.entries(defaults)
-    // Whitelist by action
-    .filter(([, { actions }]) => actions.includes(action))
-    // Whitelist by tests
-    .filter(([, { test }]) => !test || test({ opts, input }))
-    // Only if user has not specified that argument
-    .filter(([attrName]) => args[attrName] === undefined)
-    // Reduce to a single object
-    .map(([attrName, { value }]) => {
-      const val = typeof value === 'function' ? value({ opts, input }) : value;
-      return { [attrName]: val };
+    .map(([name, defaultsValue]) => {
+      defaultsValue = Object.entries(defaultsValue)
+        // Whitelist by action
+        .filter(([, { actions }]) => actions.includes(action))
+        // Whitelist by tests
+        .filter(([, { test }]) => !test || test({ opts, input }))
+        // Only if user has not specified that argument
+        .filter(([attrName]) => input[name][attrName] === undefined)
+        // Reduce to a single object
+        .map(([attrName, { value }]) => {
+          const val = typeof value === 'function'
+            ? value({ opts, input })
+            : value;
+          return { [attrName]: val };
+        })
+        .reduce((memo, object) => Object.assign(memo, object), {});
+      return { [name]: defaultsValue };
     })
     .reduce((memo, object) => Object.assign(memo, object), {});
 };
