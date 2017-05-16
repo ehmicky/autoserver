705e4f924064c2bf524cc77ab6e78866b7bd0470
autoserver-founder <founder@autoserver.io>
2017-05-16 18:26:31 +0200

    Fix line width

--- src/middleware/api_convertor/index.js
+++ src/middleware/api_convertor/index.js
@@ -7,9 +7,11 @@ const { actions } = require('../../idl');
 const apiConvertor = async function () {
   return async function apiConvertor(input) {
     const { api: { action, args, modelName }, info, params } = input;
-    const actionType = (actions.find(({ name }) => name === action) || {}).actionType;
+    const { actionType } = actions.find(({ name }) => name === action) || {};
     info.actionType = actionType;
-    const { data, metadata } = await this.next({ action, actionType, args, modelName, info, params });
+    const nextInput = { action, actionType, args, modelName, info, params };
+
+    const { data, metadata } = await this.next(nextInput);
     const response = apiConvertorOutput[info.interface]({ data, metadata });
     return response;
   };
@@ -20,7 +22,9 @@ const apiConvertorOutput = {
   // Metadata are siblings to data in GraphQL
   graphql({ data, metadata }) {
     if (data instanceof Array) {
-      return data.map((datum, index) => Object.assign({}, datum, { __metadata: metadata[index] }));
+      return data.map((datum, index) => {
+        return Object.assign({}, datum, { __metadata: metadata[index] });
+      });
     } else {
       return Object.assign({}, data, { __metadata: metadata });
     }
