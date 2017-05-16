a6968d8f52c986fb38da9a47e0dbcd3d9dd2a80d
ehmicky <ehmicky@gmail.com>
2017-05-16 18:03:55 +0200

    Fix line width

--- src/idl/plugins/apply.js
+++ src/idl/plugins/apply.js
@@ -6,9 +6,11 @@ const { timestampPlugin } = require('./timestamp');
 const { authorPlugin } = require('./author');
 
 
-// Plugins are functions that take `idl` as input, and returns a modified IDL as output
+// Plugins are functions that take `idl` as input,
+// and returns a modified IDL as output
 // They can also take an `opts` parameter
-// Use cases can be: adding a attribute to each model, extending core functionalities, etc.
+// Use cases can be: adding a attribute to each model,
+// extending core functionalities, etc.
 const applyPlugins = async function ({ idl }) {
   const plugins = idl.plugins && idl.plugins instanceof Array
     ? idl.plugins
@@ -21,25 +23,29 @@ const applyPlugins = async function ({ idl }) {
 
   // Apply each idl.plugins as FUNC({ idl }) returning idl
   const allPlugins = plugins.concat(defaultBuiltinPlugins);
-  for (let [pluginIndex, pluginConf] of allPlugins.entries()) {
+  for (let [index, pluginConf] of allPlugins.entries()) {
     // Plugin is either a function, or a string (for builtin plugins)
     if (typeof pluginConf.plugin === 'string') {
       const builtinPlugin = builtinPlugins[pluginConf.plugin];
       if (!builtinPlugin) {
-        throw new EngineStartupError(`The plugin '${pluginConf.plugin}' does not exist`, { reason: 'IDL_VALIDATION' });
+        const message = `The plugin '${pluginConf.plugin}' does not exist`;
+        throw new EngineStartupError(message, { reason: 'IDL_VALIDATION' });
       }
 
-      pluginConf = Object.assign({}, builtinPlugin, pluginConf, { plugin: builtinPlugin.plugin });
+      const { plugin } = builtinPlugin;
+      pluginConf = Object.assign({}, builtinPlugin, pluginConf, { plugin });
     }
 
     const { plugin, enabled = true, opts = {} } = pluginConf;
 
     // Plugins are only enabled if specified in `idl.plugins`.
-    // But builtin plugins, or plugins added by other plugins, need to be manually disabled if desired.
+    // But builtin plugins, or plugins added by other plugins,
+    // need to be manually disabled if desired.
     if (!enabled) { continue; }
 
     if (typeof plugin !== 'function') {
-      throw new EngineStartupError(`The plugin at 'plugins[${pluginIndex}]' is not a function`, { reason: 'IDL_VALIDATION' });
+      const message = `The plugin at 'plugins[${index}]' is not a function`;
+      throw new EngineStartupError(message, { reason: 'IDL_VALIDATION' });
     }
 
     idl = await plugin({ idl, opts });
