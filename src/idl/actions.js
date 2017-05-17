43bd0d169dcb6f616023c51d28db184dd4806dc8
ehmicky <ehmicky@gmail.com>
2017-05-17 12:35:42 +0200

    Fix line width

--- src/idl/actions.js
+++ src/idl/actions.js
@@ -1,22 +1,68 @@
 'use strict';
 
 
-/* eslint-disable no-multi-spaces */
 const actions = [
-  { name: 'findOne',     actionType: 'find',     multiple: false,  },
-  { name: 'findMany',    actionType: 'find',     multiple: true,   },
-  { name: 'createOne',   actionType: 'create',   multiple: false,  },
-  { name: 'createMany',  actionType: 'create',   multiple: true,   },
-  { name: 'replaceOne',  actionType: 'replace',  multiple: false,  },
-  { name: 'replaceMany', actionType: 'replace',  multiple: true,   },
-  { name: 'updateOne',   actionType: 'update',   multiple: false,  },
-  { name: 'updateMany',  actionType: 'update',   multiple: true,   },
-  { name: 'upsertOne',   actionType: 'upsert',   multiple: false,  },
-  { name: 'upsertMany',  actionType: 'upsert',   multiple: true,   },
-  { name: 'deleteOne',   actionType: 'delete',   multiple: false,  },
-  { name: 'deleteMany',  actionType: 'delete',   multiple: true,   },
+  {
+    name: 'findOne',
+    actionType: 'find',
+    multiple: false,
+  },
+  {
+    name: 'findMany',
+    actionType: 'find',
+    multiple: true,
+  },
+  {
+    name: 'createOne',
+    actionType: 'create',
+    multiple: false,
+  },
+  {
+    name: 'createMany',
+    actionType: 'create',
+    multiple: true,
+  },
+  {
+    name: 'replaceOne',
+    actionType: 'replace',
+    multiple: false,
+  },
+  {
+    name: 'replaceMany',
+    actionType: 'replace',
+    multiple: true,
+  },
+  {
+    name: 'updateOne',
+    actionType: 'update',
+    multiple: false,
+  },
+  {
+    name: 'updateMany',
+    actionType: 'update',
+    multiple: true,
+  },
+  {
+    name: 'upsertOne',
+    actionType: 'upsert',
+    multiple: false,
+  },
+  {
+    name: 'upsertMany',
+    actionType: 'upsert',
+    multiple: true,
+  },
+  {
+    name: 'deleteOne',
+    actionType: 'delete',
+    multiple: false,
+  },
+  {
+    name: 'deleteMany',
+    actionType: 'delete',
+    multiple: true,
+  },
 ];
-/* eslint-enable no-multi-spaces */
 
 
 module.exports = {
