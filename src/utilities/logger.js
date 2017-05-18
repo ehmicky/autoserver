27c87277dc3b038b5c7acbcbc1d07ae1812d953a
autoserver-founder <founder@autoserver.io>
2017-05-18 16:38:11 +0200

    Coding style

--- src/utilities/logger.js
+++ src/utilities/logger.js
@@ -12,14 +12,16 @@ const levels = [
 // references to logger.* functions (they can change)
 // but on reference to logger instead
 const logger = {};
-const setLogger = ({ logger: newLogger = defaultLogger } = {}) => {
+const setLogger = function ({ logger: newLogger = defaultLogger } = {}) {
   levels.forEach(level => {
     logger[level] = newLogger(level);
   });
 };
 
 // Do not create a new function, so we do not pollute the stack trace
-const defaultLogger = level => global.console[level].bind(global.console);
+const defaultLogger = function (level) {
+  return global.console[level].bind(global.console);
+};
 
 // By default, uses console, but can be redefined
 setLogger();
