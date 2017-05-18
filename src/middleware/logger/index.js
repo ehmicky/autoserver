3c0950d0e384a4131ea28c4799a5ef40e4c73f78
ehmicky <ehmicky@gmail.com>
2017-05-18 13:01:27 +0200

    Remove unused files

--- src/middleware/logger/index.js
+++ src/middleware/logger/index.js
@@ -1,7 +1,19 @@
 'use strict';
 
 
-module.exports = Object.assign(
-  {},
-  require('./handler')
-);
+const { getSwitchMiddleware } = require('../../utilities');
+const { httpLogger } = require('./http');
+
+
+const middlewares = {
+  http: httpLogger,
+};
+const getKey = ({ input: { info: { protocol } } }) => protocol;
+
+// Sends the response at the end of the request
+const logger = getSwitchMiddleware({ middlewares, getKey });
+
+
+module.exports = {
+  logger,
+};
--- src/middleware/params/index.js
+++ src/middleware/params/index.js
@@ -1,7 +1,19 @@
 'use strict';
 
 
-module.exports = Object.assign(
-  {},
-  require('./handler')
-);
+const { getSwitchMiddleware } = require('../../utilities');
+const { httpFillParams } = require('./http');
+
+
+const middlewares = {
+  http: httpFillParams,
+};
+const getKey = ({ input: { info: { protocol } } }) => protocol;
+
+// Sends the response at the end of the request
+const fillParams = getSwitchMiddleware({ middlewares, getKey });
+
+
+module.exports = {
+  fillParams,
+};
