5cb5727655dbe9183c277c8c70d2ea15b83294d5
autoserver-founder <founder@autoserver.io>
2017-05-18 18:26:22 +0200

    Remove eslint warnings

--- src/middleware/routing/routes.js
+++ src/middleware/routing/routes.js
@@ -5,13 +5,11 @@ const { RoutesManager } = require('./manager');
 
 
 const getRoutes = function () {
-  /* eslint-disable no-multi-spaces */
   const routes = [
-    { method: 'GET',            path: '/graphiql',        route: 'graphiql'     },
-    { method: ['GET', 'POST'],  path: '/graphql',         route: 'graphql'      },
-    { method: 'GET',            path: '/graphql/schema',  route: 'graphqlprint' },
+    { method: 'GET', path: '/graphiql', route: 'graphiql' },
+    { method: ['GET', 'POST'], path: '/graphql', route: 'graphql' },
+    { method: 'GET', path: '/graphql/schema', route: 'graphqlprint' },
   ];
-  /* eslint-enable no-multi-spaces */
   const manager = new RoutesManager(routes);
   return manager;
 };
