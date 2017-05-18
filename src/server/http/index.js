629f7575f31a729d477cdf92d284e178ca432146
ehmicky <ehmicky@gmail.com>
2017-05-18 19:16:17 +0200

    Simplify code

--- src/server/http/index.js
+++ src/server/http/index.js
@@ -1,7 +1,34 @@
 'use strict';
 
 
-module.exports = Object.assign(
-  {},
-  require('./server')
-);
+const http = require('http');
+
+const { log, getPromise } = require('../../utilities');
+const { port, host } = require('../../config');
+
+
+const startServer = async function ({ handleRequest }) {
+  const server = http.createServer(requestHandler.bind(null, handleRequest));
+  const promise = getPromise();
+  server.listen(port, host, function () {
+    listeningHandler();
+    promise.resolve(server);
+  });
+  return promise;
+};
+
+const requestHandler = async function (handleRequest, req, res) {
+  const info = {};
+  const protocol = { specific: { req, res } };
+  const response = await handleRequest({ info, protocol });
+  return response;
+};
+
+const listeningHandler = function () {
+  log.log(`[HTTP] Listening on ${host}:${port}`);
+};
+
+
+module.exports = {
+  httpStartServer: startServer,
+};
