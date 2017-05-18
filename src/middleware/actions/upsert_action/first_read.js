3db7e393fdaaf9a80543a022886bd08b526212e1
ehmicky <ehmicky@gmail.com>
2017-05-18 10:31:53 +0200

    Use sysArgs.authorization false for upsert's first_read and second_read

--- src/middleware/actions/upsert_action/first_read.js
+++ src/middleware/actions/upsert_action/first_read.js
@@ -19,7 +19,11 @@ const getFirstReadInput = function ({ input, prefix }) {
     return type === 'read' && multiple === isMultiple;
   });
   const readArgs = getReadArgs({ input, prefix });
-  Object.assign(sysArgs, { pagination: false });
+  // The "real" commands are "create" and "update".
+  // The first and second "find" commands are just here to patch things up,
+  // and do not provide extra information to consumers, so should be
+  // transparent when it comes to pagination and authorization
+  Object.assign(sysArgs, { pagination: false, authorization: false });
   Object.assign(input, { command, args: readArgs, sysArgs });
 
   return input;
--- src/middleware/actions/upsert_action/second_read.js
+++ src/middleware/actions/upsert_action/second_read.js
@@ -18,7 +18,11 @@ const getSecondReadInput = function ({ input, prefix }) {
     return type === 'read' && multiple === isMultiple;
   });
   const args = getReadArgs({ input, prefix });
-  Object.assign(sysArgs, { pagination: false });
+  // The "real" commands are "create" and "update".
+  // The first and second "find" commands are just here to patch things up,
+  // and do not provide extra information to consumers, so should be
+  // transparent when it comes to pagination and authorization
+  Object.assign(sysArgs, { pagination: false, authorization: false });
   Object.assign(input, { command, args, sysArgs });
 
   return input;
