e7c539d72d27d6b5a4901f3db916ada4f293f5f8
ehmicky <ehmicky@gmail.com>
2017-05-19 10:26:21 +0200

    Fix line width

--- src/parsing/http/body.js
+++ src/parsing/http/body.js
@@ -4,9 +4,9 @@
  * Parses and serializes HTTP request body
  * Handles HTTP compression
  * Max limit 100KB
- * Recognizes: application/json, application/x-www-form-urlencoded, string, binary
+ * Recognizes: application/json, application/x-www-form-urlencoded,
+ * string, binary
  **/
-
 
 const bodyParser = require('body-parser');
 
@@ -15,16 +15,22 @@ const { connectToPromise } = require('../../utilities');
 
 const parse = {};
 
-const addParseFunc = function ({ type, exportsType = type, options = {} } = {}) {
+const addParseFunc = function ({
+  type,
+  exportsType = type,
+  options = {},
+} = {}) {
   const parser = connectToPromise(bodyParser[type](options));
   parse[exportsType] = async function (req) {
     // body-parser will fill req.body = {} even if there is no body.
-    // We want to know if there is a body or not though, so must keep req.body to undefined if there is non
+    // We want to know if there is a body or not though,
+    // so must keep req.body to undefined if there is non
     const emptyBody = {};
     req.body = req.body || emptyBody;
     await parser(req);
 
-    // We just want the body, and will only do this once, so let's not pollute req
+    // We just want the body, and will only do this once,
+    // so let's not pollute req
     const body = req.body === emptyBody ? undefined : req.body;
     delete req.body;
     delete req._body;
@@ -36,15 +42,27 @@ const bodyParams = [
   { type: 'json' },
   { type: 'text' },
   { type: 'raw' },
-  { type: 'urlencoded', exportsType: 'urlencoded', options: { extended: true } },
-  { type: 'text', exportsType: 'graphql', options: { type: 'application/graphql' } },
+  {
+    type: 'urlencoded',
+    exportsType: 'urlencoded',
+    options: { extended: true },
+  },
+  {
+    type: 'text',
+    exportsType: 'graphql',
+    options: { type: 'application/graphql' },
+  },
 ];
 for (const bodyParam of bodyParams) {
   addParseFunc(bodyParam);
 }
 
 
-const sendJson = function ({ res, message = {}, contentType = 'application/json' }) {
+const sendJson = function ({
+  res,
+  message = {},
+  contentType = 'application/json',
+}) {
   message = JSON.stringify(message, null, 2);
   res.setHeader('Content-Type', contentType);
   res.setHeader('Content-Length', Buffer.byteLength(message));
