1456e40889e306dcabd481a8e60ee94671d36273
ehmicky <ehmicky@gmail.com>
2017-05-22 12:30:42 +0200

    Refactor JSL

--- src/idl/jsl.js
+++ src/idl/jsl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-const { compileJsl, getJslVariables } = require('../jsl');
+const { compileJsl, getJslVariables, runJsl } = require('../jsl');
 const { EngineStartupError } = require('../error');
 const { transform, memoize, map, recurseMap } = require('../utilities');
 
@@ -14,14 +14,14 @@ const compileIdlJsl = function ({ idl }) {
   return idl;
 };
 
-// Top-level attributes that can contain JSL
-const wrappedJslAttributes = ['helpers', 'variables'];
-
 // Compile top-level attributes's JSL, e.g. `helpers` or `variables`
 const compileTopLevelJsl = function ({ idl }) {
-  for (const name of wrappedJslAttributes) {
-    idl[name] = map(idl[name] || {}, jsl => wrapJsl({ jsl, idl, name }));
-  }
+  idl.helpers = map(idl.helpers || {}, jsl => {
+    return wrapHelpersJsl({ jsl, idl });
+  });
+  idl.variables = map(idl.variables || {}, jsl => {
+    return wrapVariablesJsl({ jsl, idl });
+  });
   return idl;
 };
 
@@ -35,41 +35,47 @@ const compileValidationJsl = function ({ idl }) {
   });
 };
 
-// Take JSL, inline or not, and turns into `function (...args)`
-// firing the first one,
-// with $1, $2, etc. provided as extra arguments
-const wrapJsl = ({ jsl, idl, name }) => {
-  const jslFunc = compileJslValue({ jsl, idl, target: name });
+const wrapVariablesJsl = ({ jsl, idl }) => {
+  const jslFunc = compileJslValue({ jsl, idl, target: 'variables' });
 
-  // We memoize for performance reasons, i.e. helpers|variables should
-  // be pure functions
+  // We memoize for performance reasons, i.e. variables should be pure functions
   // The memoizer is recreated at each request though, to avoid memory leaks
   // The first invocation is done per request, providing
   // request-specific information
-  // The second invovation is done when the helper|variables is actually used
+  // The second invovation is done when the variables is actually used
+  return ({ jslInput }) => memoize(() => {
+    return runJsl(jslFunc, jslInput);
+  });
+};
+
+// Take JSL, inline or not, and turns into `function (...args)`
+// firing the first one,
+// with $1, $2, etc. provided as extra arguments
+const wrapHelpersJsl = ({ jsl, idl }) => {
+  const jslFunc = compileJslValue({ jsl, idl, target: 'helpers' });
+
+  // We memoize for performance reasons, i.e. helpers should be pure functions
+  // The memoizer is recreated at each request though, to avoid memory leaks
+  // The first invocation is done per request, providing
+  // request-specific information
+  // The second invovation is done when the helper is actually used
   return ({ jslInput }) => memoize((...args) => {
-    // Helpers|variables can be non-JSL, but still needs to be fired
+    // Helpers can be non-JSL, but still needs to be fired
     // as function by consumers
-    if (typeof jslFunc !== 'function') {
-      return jslFunc;
-    }
+    if (typeof jslFunc !== 'function') { return jslFunc; }
 
     // Non-inline helpers only get positional arguments, no variables
-    if (!jslFunc.isInlineJsl && name === 'helpers') {
+    if (!jslFunc.isInlineJsl) {
       return jslFunc(...args);
     }
 
-    // Make sure variables are fired runtime,
-    // so variables can be evaluated lazily
     // jslInput will contain other variables|helpers,
     // so they can reference each other
     const jslArgs = getJslVariables(jslFunc, jslInput);
 
     // Provide $1, $2, etc. to inline JSL
-    if (name === 'helpers') {
-      const [$1, $2, $3, $4, $5, $6, $7, $8, $9] = args;
-      Object.assign(jslArgs, { $1, $2, $3, $4, $5, $6, $7, $8, $9 });
-    }
+    const [$1, $2, $3, $4, $5, $6, $7, $8, $9] = args;
+    Object.assign(jslArgs, { $1, $2, $3, $4, $5, $6, $7, $8, $9 });
 
     return jslFunc(jslArgs);
   });
