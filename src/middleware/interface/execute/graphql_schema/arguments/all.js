5d99e2eb09dc6b3f71259b5ed68242f5506a8ad1
autoserver-founder <founder@autoserver.io>
2017-05-24 17:51:29 +0200

    Add no_output GraphQL description

--- src/middleware/interface/execute/graphql_schema/arguments/all.js
+++ src/middleware/interface/execute/graphql_schema/arguments/all.js
@@ -6,6 +6,7 @@ const { getFilterArgument } = require('./filter');
 const { getOrderArgument } = require('./order');
 const { getPaginationArgument } = require('./pagination');
 const { getDryRunArguments } = require('./dry_run');
+const { getNoOutputArguments } = require('./no_output');
 
 
 // Retrieves all resolver arguments, before resolve function is fired
@@ -17,7 +18,8 @@ const getArguments = function (def, opts) {
 		getFilterArgument(options),
     getOrderArgument(options),
     getPaginationArgument(options),
-    getDryRunArguments(options)
+    getDryRunArguments(options),
+    getNoOutputArguments(options)
   );
 };
 
