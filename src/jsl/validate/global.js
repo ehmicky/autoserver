22cb6e2e10d6925fa08bd31959c22e16d63e6eab
ehmicky <ehmicky@gmail.com>
2017-05-26 19:17:20 +0200

    Validate arg.filter JSL

--- src/jsl/validate/global.js
+++ src/jsl/validate/global.js
@@ -7,7 +7,7 @@ const { memoize } = require('../../utilities');
 // Retrieves all global variables, to make sure JSL does not access them
 // This is memoized, i.e. no global variables should be added runtime,
 // as they could be accessed in JSL
-const getGlobalKeys = memoize(function () {
+const getGlobalKeys = memoize(function ({ type }) {
   const globalObjects = [
     global,
     // Since global is an object, Object constructor and prototype are
@@ -19,7 +19,7 @@ const getGlobalKeys = memoize(function () {
     .reduce((keys, globalObj) => {
       // Retrieves all global properties
       const globalObjKeys = Object.getOwnPropertyNames(globalObj)
-        .filter(key => !whitelistedGlobalKeys.includes(key));
+        .filter(key => !whitelistedGlobalKeys[type].includes(key));
       return [...keys, ...globalObjKeys];
     }, [])
     // Make sure it is sorted, for the memoizer
@@ -28,7 +28,7 @@ const getGlobalKeys = memoize(function () {
 });
 
 // Whitelist those global properties as safe, i.e. they can be used in JSL
-const whitelistedGlobalKeys = [
+const systemWhitelistedGlobalKeys = [
   'String',
   'Number',
   'Boolean',
@@ -53,6 +53,17 @@ const whitelistedGlobalKeys = [
   'decodeURI',
   'encodeURI',
 ];
+const filterWhitelistedGlobalKeys = [
+  'Date',
+  'undefined',
+  'NaN',
+  'Infinity',
+];
+const whitelistedGlobalKeys = {
+  system: systemWhitelistedGlobalKeys,
+  startup: systemWhitelistedGlobalKeys,
+  filter: filterWhitelistedGlobalKeys,
+};
 
 module.exports = {
   getGlobalKeys,
