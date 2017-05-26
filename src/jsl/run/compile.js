6b9f465890572a2a75cb154d8a1f2a59a4c36990
ehmicky <ehmicky@gmail.com>
2017-05-26 17:26:53 +0200

    Validate JSL at runtime

--- src/jsl/run/compile.js
+++ src/jsl/run/compile.js
@@ -4,12 +4,13 @@
 const { isJsl, isEscapedJsl } = require('../test');
 const { getRawJsl } = require('../tokenize');
 const { memoizeUnlessClient } = require('../memoize');
+const { validateJsl } = require('../validate');
 
 
 // Transform JSL into a function with the JSL as body
 // Returns as it is not JSL
 // This can throw if JSL's JavaScript is wrong
-const compileJsl = memoizeUnlessClient(function ({ jsl, paramsKeys }) {
+const compileJsl = memoizeUnlessClient(function ({ jsl, paramsKeys, type }) {
   // If this is not JSL, abort
   if (!isJsl({ jsl })) {
     // Can escape (...) from being interpreted as JSL by escaping
@@ -19,6 +20,9 @@ const compileJsl = memoizeUnlessClient(function ({ jsl, paramsKeys }) {
     }
     return jsl;
   }
+
+  // Make sure JSL is valid and safe security-wise
+  validateJsl({ jsl, type });
 
   // Removes outer parenthesis
   const rawJsl = getRawJsl({ jsl });
