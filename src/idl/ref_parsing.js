bf0cb6683f99c79f630e7fd4b2b940bbd27a14f5
autoserver-founder <founder@autoserver.io>
2017-05-29 11:28:55 +0200

    Simplify importing libraries

--- src/idl/ref_parsing.js
+++ src/idl/ref_parsing.js
@@ -7,8 +7,7 @@ const { EngineStartupError } = require('../error');
 
 const resolveRefs = async function ({ idl, baseDir }) {
   const parsedIdl = await resolveJsonRefs({ idl, baseDir });
-  const mergedIdl = mergeAllLibraries({ idl: parsedIdl });
-  return mergedIdl;
+  return parsedIdl;
 };
 
 // Resolve JSON references, i.e. $ref
@@ -37,45 +36,6 @@ const resolveJsonRefs = async function ({ idl, baseDir }) {
 
   return parsedIdl;
 };
-
-// Merge idl.helpers.libraries.* into idl.helpers.*
-const mergeAllLibraries = function ({ idl }) {
-  for (const attrName of librariesAttrs) {
-    idl = mergeLibraries({ idl, attrName });
-  }
-  return idl;
-};
-const mergeLibraries = function ({ idl, attrName }) {
-  if (!idl[attrName] || !idl[attrName].libraries) { return idl; }
-
-  // Each library must an object
-  // This translates this array of object into a single object,
-  // which is merged into `idl[attrName]`
-  if (idl[attrName].libraries instanceof Array) {
-    const libraries = idl[attrName].libraries
-      .map(library => {
-        // If there is any function, it is considered a library,
-        // and non-functions are ignored
-        const isLibrary = Object.values(library)
-          .some(value => typeof value === 'function');
-        return Object.entries(library)
-          .filter(([, value]) => {
-            return !isLibrary ||
-              (typeof value === 'function' && Object.keys(value).length === 0);
-          })
-          .map(([name, value]) => ({ [name]: value }))
-          .reduce((memo, val) => Object.assign(memo, val), {});
-      })
-      .reduce((memo, val) => Object.assign(memo, val), {});
-    idl[attrName] = Object.assign({}, libraries, idl[attrName]);
-  }
-
-  delete idl[attrName].libraries;
-
-  return idl;
-};
-// Which attributes in idl.* can use libraries
-const librariesAttrs = ['helpers'];
 
 
 module.exports = {
