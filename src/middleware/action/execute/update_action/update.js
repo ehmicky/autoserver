280a1fc2c90dd73f914aece5631e4ee3ca0f9d3b
ehmicky <ehmicky@gmail.com>
2017-05-29 17:26:52 +0200

    Add support for arg.data JSL

--- src/middleware/action/execute/update_action/update.js
+++ src/middleware/action/execute/update_action/update.js
@@ -5,35 +5,40 @@ const { omit, cloneDeep } = require('lodash');
 
 const { commands } = require('../../../../constants');
 const { EngineError } = require('../../../../error');
+const { isJsl } = require('../../../../jsl');
 
 
 // Retrieves the input for the "update" command
 const getUpdateInput = function ({ input, models }) {
   input = cloneDeep(input);
-  const { sysArgs, args, action } = input;
+  const { sysArgs, args, action, jsl } = input;
 
   const isMultiple = action.multiple;
   const command = commands.find(({ type, multiple }) => {
     return type === 'update' && multiple === isMultiple;
   });
-  const updateArgs = getUpdateArgs({ args, models });
+  const updateArgs = getUpdateArgs({ args, models, jsl });
   Object.assign(sysArgs, { pagination: isMultiple });
   Object.assign(input, { command, args: updateArgs, sysArgs });
 
   return input;
 };
 
-const getUpdateArgs = function ({ args, models }) {
+const getUpdateArgs = function ({ args, models, jsl }) {
   const { data } = args;
   // arg.filter is only used by first "read" command
   const updateArgs = omit(args, ['filter']);
 
+  // Keys in arg.* using JSL
+  const jslKeys = Object.keys(data)
+    .filter(key => isJsl({ jsl: data[key] }));
+
   if (models instanceof Array) {
     updateArgs.data = models.map(model => {
-      return getUpdateData({ model, data });
+      return getUpdateData({ model, data, jsl, jslKeys });
     });
   } else {
-    updateArgs.data = getUpdateData({ model: models, data });
+    updateArgs.data = getUpdateData({ model: models, data, jsl, jslKeys });
   }
 
   return updateArgs;
@@ -41,10 +46,12 @@ const getUpdateArgs = function ({ args, models }) {
 
 // Merge current models with the data we want to update,
 // to obtain the final models we want to use as replacement
-const getUpdateData = function ({ model, data }) {
+const getUpdateData = function ({ model, data, jsl, jslKeys }) {
   validateUpdateData({ data });
 
-  const updateData = Object.assign({}, model, data);
+  const transformedData = transformData({ model, data, jsl, jslKeys });
+
+  const updateData = Object.assign({}, model, transformedData);
   return updateData;
 };
 
@@ -58,6 +65,25 @@ const validateUpdateData = function ({ data }) {
     const message = 'Argument \'data\' must be an object';
     throw new EngineError(message, { reason: 'INPUT_VALIDATION' });
   }
+};
+
+// Apply arg.data JSL
+const transformData = function ({ model, data, jsl, jslKeys }) {
+  data = Object.assign({}, data);
+
+  for (const attrName of jslKeys) {
+    // If current attribute value is null|undefined, leave it as is
+    // This simplifies JSL, as $ is guaranteed to be defined
+    if (model[attrName] == null) {
+      data[attrName] = model[attrName];
+      continue;
+    }
+
+    const params = { $$: model, $: model[attrName] };
+    data[attrName] = jsl.run({ value: data[attrName], params, type: 'data' });
+  }
+
+  return data;
 };
 
 
