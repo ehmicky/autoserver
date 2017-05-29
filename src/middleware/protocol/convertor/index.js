8070fc57319d05e7b8cd629603d49bdfcee0c70f
autoserver-founder <founder@autoserver.io>
2017-05-29 16:36:14 +0200

    Small refactoring

--- src/middleware/protocol/convertor/index.js
+++ src/middleware/protocol/convertor/index.js
@@ -7,8 +7,9 @@ const { Jsl } = require('../../../jsl');
 // Converts from no format to Protocol format
 const protocolConvertor = function ({ idl: { helpers, exposeMap } }) {
   return async function protocolConvertor(input) {
-    const jsl = (new Jsl({ exposeMap })).addHelpers({ helpers });
-    const nextInput = { jsl, specific: input };
+    const jsl = new Jsl({ exposeMap });
+    const jslWithHelpers = jsl.addHelpers({ helpers });
+    const nextInput = { jsl: jslWithHelpers, specific: input };
 
     const response = await this.next(nextInput);
     return response;
