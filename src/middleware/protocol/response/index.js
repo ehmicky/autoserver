179d576b59693d6c769a2e70574efd6202cec89e
autoserver-founder <founder@autoserver.io>
2017-05-30 18:54:21 +0200

    Make sendResponse more protocol-agnostic

--- src/middleware/protocol/response/index.js
+++ src/middleware/protocol/response/index.js
@@ -1,41 +1,7 @@
 'use strict';
 
 
-const { httpSendResponse } = require('./http');
-
-
-// Sends the response at the end of the request
-const sendResponse = async function () {
-  return async function sendResponse(input) {
-    const { logInfo } = input;
-    const send = sendResponseMap[input.protocol].bind(null, input);
-
-    try {
-      const response = await this.next(input);
-      const { content, type } = response;
-
-      logInfo.add({ response: { content, type } });
-
-      send(response);
-
-      return response;
-    } catch (error) {
-      if (!(error instanceof Error)) {
-        error = new Error(String(error));
-      }
-
-      // Handler to send response error
-      error.sendError = send;
-      throw error;
-    }
-  };
-};
-
-const sendResponseMap = {
-  http: httpSendResponse,
-};
-
-
-module.exports = {
-  sendResponse,
-};
+module.exports = Object.assign(
+  {},
+  require('./handler')
+);
