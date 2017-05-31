ce90716d1e11c3b75d127d9dc9399a466aa6aea7
autoserver-founder <founder@autoserver.io>
2017-05-31 18:31:08 +0200

    Only report errors as "failure"

--- src/middleware/error_handler/report.js
+++ src/middleware/error_handler/report.js
@@ -3,10 +3,22 @@
 
 const chalk = require('chalk');
 
+const { STATUS_LEVEL_MAP } = require('../../logging');
 
-const reportError = function ({ log, error }) {
+
+const reportError = function ({ log, error = {} }) {
+  if (!isError({ error })) { return; }
+
   const message = getErrorMessage(error);
   log.error(message, { type: 'failure', errorInfo: error });
+};
+
+// Only report except with status 'error'
+// If we haven't reached the request logging middleware yet, error.status
+// will be undefined, so it will still be caught and reporter.
+const isError = function ({ error }) {
+  const level = STATUS_LEVEL_MAP[error.status];
+  return !level || level === 'error';
 };
 
 const getErrorMessage = function ({
