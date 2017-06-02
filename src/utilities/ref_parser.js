c163913069e9e2c8a9ced889f81042e245d6d506
ehmicky <ehmicky@gmail.com>
2017-06-02 11:43:58 +0200

    Fix line width

--- src/utilities/ref_parser.js
+++ src/utilities/ref_parser.js
@@ -8,12 +8,16 @@ const { getYaml } = require('./yaml');
 
 
 /**
- * Dereference JSON references. RFC: https://tools.ietf.org/id/draft-pbryan-zyp-json-ref-03.html
- * I.e. { $ref: "path|url" } will be replaced by the target, which can be accessed locally (local path) or remotely (HTTP[S])
+ * Dereference JSON references.
+ * RFC: https://tools.ietf.org/id/draft-pbryan-zyp-json-ref-03.html
+ * I.e. { $ref: "path|url" } will be replaced by the target, which can be
+ * accessed locally (local path) or remotely (HTTP[S])
  * Targets can be JSON or YAML files.
  * Circular references are supported.
- * Siblings attributes to `$ref` will be merged (with higher priority), although this is not standard|spec behavior.
- * This function might throw for several reasons, e.g. YAML|JSON parsing error, cannot access remote|local file, etc.
+ * Siblings attributes to `$ref` will be merged (with higher priority),
+ * although this is not standard|spec behavior.
+ * This function might throw for several reasons, e.g. YAML|JSON parsing error,
+ * cannot access remote|local file, etc.
  */
 const dereferenceRefs = async function (obj) {
   return await RefParser.dereference(obj, {
@@ -27,13 +31,15 @@ const dereferenceRefs = async function (obj) {
       },
       yaml: {
         allowEmpty: false,
-        // We need to override YAML parsing, as we use stricter YAML parsing (CORE_SCHEMA only)
+        // We need to override YAML parsing, as we use stricter YAML
+        // parsing (CORE_SCHEMA only)
         async parse({ data }) {
           if (Buffer.isBuffer(data)) {
             data = data.toString();
           }
           if (typeof data !== 'string') { return data; }
-          // `content` cannot be `null` because of a bug with `json-schema-ref-parser`
+          // `content` cannot be `null` because of a bug
+          // with `json-schema-ref-parser`
           const content = await getYaml({ content: data }) || undefined;
           return content;
         },
@@ -78,7 +84,8 @@ const nodeRefs = {
 
 // Enhanced version of `require()`
 const requireFile = function (url) {
-  // The new required file's require() calls should be relative to the file itself, so we temporarily change cwd
+  // The new required file's require() calls should be relative to the file
+  // itself, so we temporarily change cwd
   const currenDir = process.cwd();
   process.chdir(dirname(url));
   let file;
