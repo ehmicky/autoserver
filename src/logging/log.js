bb493551f89165b1191c4c4fece7392e1004d467
autoserver-founder <founder@autoserver.io>
2017-06-02 11:06:24 +0200

    Small refactoring

--- src/logging/log.js
+++ src/logging/log.js
@@ -6,17 +6,19 @@ const { EngineError } = require('../error');
 const { report } = require('./logger');
 const { getRequestInfo } = require('./request_info');
 const { getRequestMessage } = require('./request_message');
+const { LEVELS } = require('./constants');
 
-
-// We use symbols to make those members private
-const reportSym = Symbol('report');
 
 class Log {
 
   constructor({ opts: { logger, loggerLevel, loggerFilter }, phase }) {
     this._info = {};
 
-    this[reportSym] = buffer(this[reportSym], this);
+    this._report = buffer(this._report, this);
+
+    for (const level of LEVELS) {
+      this[level] = this._report.bind(this, level);
+    }
 
     Object.assign(this, { logger, loggerLevel, loggerFilter, phase });
   }
@@ -25,23 +27,7 @@ class Log {
     this._info = deepMerge(this._info, obj);
   }
 
-  info(...args) {
-    this[reportSym]('info', ...args);
-  }
-
-  log(...args) {
-    this[reportSym]('log', ...args);
-  }
-
-  warn(...args) {
-    this[reportSym]('warn', ...args);
-  }
-
-  error(...args) {
-    this[reportSym]('error', ...args);
-  }
-
-  [reportSym](level, rawMessage = '', logObj = {}){
+  _report(level, rawMessage = '', logObj = {}){
     if (typeof rawMessage !== 'string') {
       const message = `Message must be a string: '${rawMessage}'`;
       throw new EngineError(message, { reason: 'UTILITY_ERROR' });
@@ -51,14 +37,16 @@ class Log {
       throw new EngineError(message, { reason: 'UTILITY_ERROR' });
     }
 
-    if (this.phase === 'request') {
+    const phase = this.phase;
+    const type = logObj.type || 'message';
+    Object.assign(logObj, { phase, type });
+
+    if (phase === 'request') {
       logObj.requestInfo = getRequestInfo(this._info, this.loggerFilter);
-      if (logObj.type === 'call') {
+      if (type === 'call') {
         rawMessage = getRequestMessage(logObj.requestInfo);
       }
     }
-
-    logObj.phase = this.phase;
 
     report(this.logger, this.loggerLevel, level, rawMessage, logObj);
   }
@@ -67,7 +55,7 @@ class Log {
   // E.g. used in requests when requestInfo is not completely built yet
   _setBuffered(isBuffered) {
     const method = isBuffered ? 'cork' : 'uncork';
-    this[reportSym][method]();
+    this._report[method]();
   }
 
 }
