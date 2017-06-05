6af62dc7356fbb3a5a65549466a9f5b07ac9b1b7
autoserver-founder <founder@autoserver.io>
2017-06-05 10:33:50 +0200

    Improve ApiEngineServer

--- src/server/api_server.js
+++ src/server/api_server.js
@@ -6,6 +6,9 @@ const EventEmitter2 = require('eventemitter2');
 const { getServerInfo } = require('../info');
 
 
+// Object returned by main function `startServer()`
+// Contains general information (options, server name, version, etc.)
+// Emits events related to server lifecycle and logging.
 class ApiEngineServer extends EventEmitter2 {
 
   constructor() {
--- src/server/startup_error.js
+++ src/server/startup_error.js
@@ -5,13 +5,13 @@ const { getStandardError, getErrorMessage } = require('../error');
 
 
 // Handle exceptions thrown at server startup
-const handleStartupError = function ({ startupLog }, server, error) {
+const handleStartupError = function ({ startupLog }, apiServer, error) {
   const standardError = getStandardError({ log: startupLog, error });
   const message = getErrorMessage({ error: standardError });
   startupLog.error(message, { type: 'failure', errorInfo: standardError });
 
   // Throws if no listener was setup
-  server.emit('error');
+  apiServer.emit('error');
 };
 
 
