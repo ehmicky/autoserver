939628e8c42e8fbe7addacff704c117fcd8e6229
autoserver-founder <founder@autoserver.io>
2017-06-05 18:50:01 +0200

    Start startup performance logging

--- src/server/startup_error.js
+++ src/server/startup_error.js
@@ -6,9 +6,14 @@ const { getStandardError, getErrorMessage } = require('../error');
 
 // Handle exceptions thrown at server startup
 const handleStartupError = function ({ startupLog }, apiServer, error) {
+  const perf = startupLog.perf.start('mainHandler', 'exception');
+
   const standardError = getStandardError({ log: startupLog, error });
   const message = getErrorMessage({ error: standardError });
   startupLog.error(message, { type: 'failure', errorInfo: standardError });
+
+  perf.stop();
+  startupLog.perf.report();
 
   // Throws if no listener was setup
   apiServer.emit('error');
