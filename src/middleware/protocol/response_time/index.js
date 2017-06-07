e620d720f78acde8e9a58545a69e052ec35a4904
autoserver-founder <founder@autoserver.io>
2017-06-07 14:04:51 +0200

    Add X-Response-Time header

--- src/middleware/protocol/response_time/index.js
+++ src/middleware/protocol/response_time/index.js
@@ -1,14 +1,20 @@
 'use strict';
 
 
+const parsing = require('../../../parsing');
+
+
 const setResponseTime = function () {
   return async function setResponseTime(input) {
-    const { log } = input;
+    const { log, protocol, specific } = input;
 
     const response = await this.next(input);
 
     const responseTime = log.perf.all.stop();
     log.add({ responseTime });
+
+    const headers = { 'X-Response-Time': Math.round(responseTime) };
+    parsing[protocol].headers.send({ specific, headers });
 
     return response;
   };
