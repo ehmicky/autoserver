9347883e991d3e1e5ae60f5475838a07b8bb0dda
ehmicky <ehmicky@gmail.com>
2017-06-08 14:42:34 +0200

    Add transform `using`

--- src/middleware/command/transform/handler.js
+++ src/middleware/command/transform/handler.js
@@ -40,13 +40,23 @@ const handleTransforms = function ({ idl, startupLog }) {
 // e.g. { my_model: [{ attrName, transform }, ...], ... }
 const getTransformsMap = function ({ idl: { models } }) {
   return Object.entries(models)
-    .map(([modelName, { properties = {} }]) => {
+    .map(([modelName, { transformOrder, properties = {} }]) => {
       const props = Object.entries(properties)
         .filter(([, { transform }]) => transform)
         .map(([attrName, { transform }]) => ({ attrName, transform }));
-      return { [modelName]: props };
+      const sortedProps = sortProps({ props, transformOrder });
+      return { [modelName]: sortedProps };
     })
     .reduce((memo, obj) => Object.assign(memo, obj), {});
+};
+
+// Sort transforms according to `using` property
+const sortProps = function ({ props, transformOrder }) {
+  return props.sort((a, b) => {
+    const indexA = transformOrder.indexOf(a.attrName);
+    const indexB = transformOrder.indexOf(b.attrName);
+    return indexA > indexB ? 1 : indexA < indexB ? -1 : 0;
+  });
 };
 
 const applyTransforms = function ({ data, transforms, jsl }) {
