630e258d60f198220d5973599dc1a621d8666f7f
ehmicky <ehmicky@gmail.com>
2017-06-08 10:56:11 +0200

    Fix order

--- src/middleware/command/index.js
+++ src/middleware/command/index.js
@@ -5,8 +5,8 @@ module.exports = Object.assign(
   {},
   require('./convertor'),
   require('./validation'),
-  require('./system_defaults'),
-  require('./user_defaults'),
   require('./normalization'),
+  require('./user_defaults'),
+  require('./system_defaults'),
   require('./transform')
 );
