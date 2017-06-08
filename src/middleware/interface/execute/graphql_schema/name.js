c1db47c2bf7a0f017224dd3b86c4c922cce28c94
ehmicky <ehmicky@gmail.com>
2017-06-08 18:41:15 +0200

    Rename method to GraphQLMethod

--- src/middleware/interface/execute/graphql_schema/name.js
+++ src/middleware/interface/execute/graphql_schema/name.js
@@ -6,17 +6,21 @@ const { plural, singular } = require('pluralize');
 
 
 const nameSym = Symbol('modelName');
-// Returns type name, titleized with action prepended, in singular form, e.g. `FindPet`, for schema type name
+// Returns type name, titleized with action prepended, in singular form,
+// e.g. `FindPet`, for schema type name
 const getTypeName = function ({ def, opts: { inputObjectType, action = {} } }) {
   const { model, [nameSym]: modelName } = def;
-  // Top-level methods do not have `def.model`, so use def[nameSym] instead
+  // Top-level graphqlMethods do not have `def.model`,
+  // so use def[nameSym] instead
   const actualModel = model || modelName;
   const name = action.multiple ? plural(actualModel) : singular(actualModel);
   const nestedPostfix = !def.isTopLevel ? ' Nested' : '';
-  return camelize(capitalize(`${action.type || ''} ${name} ${inputObjectType}${nestedPostfix}`));
+  const typeName = `${action.type || ''} ${name} ${inputObjectType}${nestedPostfix}`;
+  return camelize(capitalize(typeName));
 };
 
-// Returns action name, camelized, in plural form, e.g. `findPets` or `deletePets`
+// Returns action name, camelized, in plural form,
+// e.g. `findPets` or `deletePets`
 const getActionName = function ({ modelName, action, noChange }) {
   const model = noChange
     ? modelName
