17df409a7a89db948082551d94e2ecbe58fb68fb
ehmicky <ehmicky@gmail.com>
2017-06-08 14:54:19 +0200

    Check `using` circular dependencies

--- src/idl/normalize.js
+++ src/idl/normalize.js
@@ -66,9 +66,9 @@ const normalizeAllTransforms = function ({ models }) {
     const props = Object.entries(properties)
       .filter(([, { transform }]) => transform)
       .map(([attrName, { transformUsing }]) => ({ attrName, transformUsing }));
-    const transformOrder = getTransformOrder({ props });
-
+    const transformOrder = getTransformOrder({ props, modelName });
     Object.assign(model, { properties, transformOrder });
+
     return model;
   });
 };
@@ -111,7 +111,9 @@ const getTransformUsing = function ({ modelName, attributes, transforms }) {
 };
 
 // Get transforms order according to `using` property
-const getTransformOrder = function ({ props }) {
+const getTransformOrder = function ({ props, modelName, triedProps = [] }) {
+  checkTransformCircular({ props, modelName, triedProps });
+
   for (const [index, prop] of props.entries()) {
     const nextProps = props.slice(index + 1);
     const isWrongOrder = prop.transformUsing.some(orderAttr => {
@@ -120,10 +122,24 @@ const getTransformOrder = function ({ props }) {
     if (isWrongOrder) {
       const previousProps = props.slice(0, index);
       const newProps = [...previousProps, ...nextProps, prop];
-      return getTransformOrder({ props: newProps });
+      return getTransformOrder({ props: newProps, modelName, triedProps });
     }
   }
-  return props.map(({ attrName }) => attrName);
+
+  const transformOrder = props.map(({ attrName }) => attrName);
+  return transformOrder;
+};
+
+// If two properties point to each other with `using` property, it means
+// each must be before the other, which is invalid, and will throw
+// exceptions during `getTransformOrder()`
+const checkTransformCircular = function ({ props, modelName, triedProps }) {
+  const strProps = props.map(({ attrName }) => attrName).join(',');
+  if (triedProps.includes(strProps)) {
+    const message = `Circular dependencies in 'using' properties of model '${modelName}'`;
+    throw new EngineError(message, { reason: 'IDL_VALIDATION' });
+  }
+  triedProps.push(strProps);
 };
 
 // List of transformations to apply to normalize IDL models
