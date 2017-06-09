09d50346f526025dad8955d6e041b50a5a50f8e7
autoserver-founder <founder@autoserver.io>
2017-06-09 17:39:52 +0200

    Start alias middleware

--- src/middleware/command/index.js
+++ src/middleware/command/index.js
@@ -6,6 +6,7 @@ module.exports = Object.assign(
   require('./convertor'),
   require('./validation'),
   require('./normalization'),
+  require('./aliases'),
   require('./read_only'),
   require('./transform'),
   require('./user_defaults'),
