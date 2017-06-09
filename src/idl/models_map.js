b4693c9918e1b4c5f0db9457b61e4a002d48d70c
autoserver-founder <founder@autoserver.io>
2017-06-09 17:20:21 +0200

    Fix line width

--- src/idl/models_map.js
+++ src/idl/models_map.js
@@ -6,14 +6,17 @@ const { mapValues } = require('lodash');
 
 /**
  * Returns a simplified map of all the models in the IDL
- * Example: { my_model: { my_sub_model: { multiple: true, model: 'another_model' } } }
+ * Example: { my_model: { my_sub_model:
+ * { multiple: true, model: 'another_model' } } }
  **/
 const getModelsMap = function ({ idl }) {
-  return mapValues(idl.models, ({ properties }) => mapValues(properties, prop => {
-    const multiple = prop.items !== undefined;
-    const model = multiple ? prop.items.model : prop.model;
-    return { multiple, model };
-  }));
+  return mapValues(idl.models, ({ properties }) => {
+    return mapValues(properties, prop => {
+      const multiple = prop.items !== undefined;
+      const model = multiple ? prop.items.model : prop.model;
+      return { multiple, model };
+    });
+  });
 };
 
 
