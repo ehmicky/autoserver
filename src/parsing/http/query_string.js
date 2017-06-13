984ade66e999a16b4dfdc3979218eac74a588bfd
ehmicky <ehmicky@gmail.com>
2017-06-13 18:20:37 +0200

    Fix line width

--- src/parsing/http/query_string.js
+++ src/parsing/http/query_string.js
@@ -1,9 +1,8 @@
 'use strict';
 
-/**
- * Parses and serializes URL query strings
- * Can encode types such as: null, undefined, '', strings, integers, floats, booleans
- */
+// Parses and serializes URL query strings
+// Can encode types such as: null, undefined, '', strings, integers,
+// floats, booleans
 
 
 const qs = require('qs');
@@ -15,22 +14,21 @@ const { EngineError } = require('../../error');
 const MAX_DEPTH = 10;
 const MAX_ARRAY_LENGTH = 100;
 
-/**
- * Parse query string as query object
- * Can use the following notations:
- *  - ?var[subvar]=val -> { var: { subvar: val } }
- *  - ?var.subvar=val -> { var: { subvar: val } }
- *  - ?var[0]=val -> { var: [ val ] }
- *  - ?var[]=val&var[]=secondval -> { var: [ val, secondval ] }
- * Can be nested, with max 10 levels of depts
- * Array max length is 100
- * Performs proper URI decoding, using decodeURIComponent()
- * Differentiates between undefined, null and '' (see serialize() below)
- * Try to guess types: numbers, booleans, null
- *
- * @param {string|Url} url - Can either be a full URL, a path, a query string (with leading '?') or a URL object
- * @returns {object} queryObject
- */
+// Parse query string as query object
+// Can use the following notations:
+//  - ?var[subvar]=val -> { var: { subvar: val } }
+//  - ?var.subvar=val -> { var: { subvar: val } }
+//  - ?var[0]=val -> { var: [ val ] }
+//  - ?var[]=val&var[]=secondval -> { var: [ val, secondval ] }
+// Can be nested, with max 10 levels of depts
+// Array max length is 100
+// Performs proper URI decoding, using decodeURIComponent()
+// Differentiates between undefined, null and '' (see serialize() below)
+// Try to guess types: numbers, booleans, null
+//
+// @param {string|Url} url - Can either be a full URL, a path,
+// a query string (with leading '?') or a URL object
+// @returns {object} queryObject
 const parse = function ({ specific: { req: { url } } }) {
   const queryString = getQueryString(url);
   try {
@@ -43,16 +41,19 @@ const parse = function ({ specific: { req: { url } } }) {
     });
     return queryObject;
   } catch (innererror) {
-    throw new EngineError(`Request query string is invalid: ${url}`, { reason: 'QUERY_STRING_PARSE', innererror });
+    const message = `Request query string is invalid: ${url}`;
+    throw new EngineError(message, {
+      reason: 'QUERY_STRING_PARSE',
+      innererror,
+    });
   }
 };
 
-/**
- * Retrieves query string from a URL
- *
- * @param {string|Url} url - Can either be a full URL, a path, a query string (with leading '?') or a URL object
- * @returns {string} query - does not include the leading `?`
- **/
+// Retrieves query string from a URL
+//
+// @param {string|Url} url - Can either be a full URL, a path,
+// a query string (with leading '?') or a URL object
+// @returns {string} query - does not include the leading `?`
 const getQueryString = function (url) {
   if (!(url instanceof urlParser.URL)) {
     try {
@@ -65,7 +66,8 @@ const getQueryString = function (url) {
         }
         url = new urlParser.URL(`http://localhost${path}`);
       } catch (e) {
-        throw new EngineError(`Could not retrieve query string from: ${url}`, { reason: 'QUERY_STRING_PARSE' });
+        const message = `Could not retrieve query string from: ${url}`;
+        throw new EngineError(message, { reason: 'QUERY_STRING_PARSE' });
       }
     }
   }
@@ -78,18 +80,16 @@ const decodeValue = function (str) {
    return decodeURIComponent(str.replace(/\+/g, ' '));
 };
 
-/**
- * Serialize a plain object into a query string (without the leading '?')
- * Performs proper URI encoding, using encodeURIComponent()
- * Differentiates between:
- *  - undefined, empty array or object with only empty properties -> no variable
- *  - null -> ?var
- *  - '' -> ?var=
- * Serializes dates as well, but it will be parsed back (by parse()) as a string
- *
- * @param {object} queryObject
- * @returns {string} queryString
- */
+// Serialize a plain object into a query string (without the leading '?')
+// Performs proper URI encoding, using encodeURIComponent()
+// Differentiates between:
+//  - undefined, empty array or object with only empty properties -> no variable
+//  - null -> ?var
+//  - '' -> ?var=
+// Serializes dates as well, but it will be parsed back (by parse()) as a string
+//
+// @param {object} queryObject
+// @returns {string} queryString
 const serialize = function (queryObject) {
   try {
     const queryString = qs.stringify(queryObject, {
@@ -99,7 +99,8 @@ const serialize = function (queryObject) {
     });
     return queryString;
   } catch (innererror) {
-    throw new EngineError(`Response query string is invalid: ${JSON.stringify(queryObject)}`, {
+    const message = `Response query string is invalid: ${JSON.stringify(queryObject)}`;
+    throw new EngineError(message, {
       reason: 'QUERY_STRING_SERIALIZE',
       innererror,
     });
