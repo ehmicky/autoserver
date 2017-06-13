8257188df67221d4627bc342cd991ad2e7341331
autoserver-founder <founder@autoserver.io>
2017-06-13 14:33:48 +0200

    Flatten directory structure

---
+++ src/middleware/action/execute/replace/read.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+const { cloneDeep } = require('lodash');
+
+const { commands } = require('../../../../constants');
+const { getFilter } = require('../upsert/filter');
+
+
+// Retrieves the input for the "read" command
+const getReadInput = function ({ input }) {
+  input = Object.assign({}, input);
+  input.args = cloneDeep(input.args);
+
+  const { action } = input;
+
+  const isMultiple = action.multiple;
+  const command = commands.find(({ type, multiple }) => {
+    return type === 'read' && multiple === isMultiple;
+  });
+
+  const filter = getFilter({ input });
+  const newArgs = { filter, pagination: false };
+  Object.assign(input, { command, args: newArgs });
+
+  return input;
+};
+
+
+module.exports = {
+  getReadInput,
+};
---
+++ src/middleware/protocol/validation.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+const { validate } = require('../../validation');
+const { EngineError } = require('../../error');
+
+
+/**
+ * Protocol layer validation
+ * Those errors should not happen, i.e. server-side (e.g. 500)
+ **/
+const protocolValidation = function () {
+  return async function protocolValidation(input) {
+    const { protocol, log } = input;
+
+    const perf = log.perf.start('protocol.validation', 'middleware');
+
+    if (!protocol) {
+      const message = 'Unsupported protocol';
+      throw new EngineError(message, { reason: 'UNSUPPORTED_PROTOCOL' });
+    }
+
+    validate({ schema, data: input, reportInfo });
+
+    perf.stop();
+    const response = await this.next(input);
+    return response;
+  };
+};
+
+const schema = {
+  type: 'object',
+  required: ['specific', 'jsl', 'protocol', 'protocolFullName'],
+  properties: {
+    specific: { type: 'object' },
+    jsl: { type: 'object' },
+    protocol: { type: 'string' },
+    protocolFullName: { type: 'string' },
+  },
+};
+const reportInfo = { type: 'serverInputSyntax', dataVar: 'input' };
+
+
+module.exports = {
+  protocolValidation,
+};
