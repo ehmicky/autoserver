257cf879692c3fdfe9206a8b075fe7414b394dac
ehmicky <ehmicky@gmail.com>
2017-06-13 10:49:54 +0200

    Move args.data validateLimits

--- src/middleware/operation/handle_args/index.js
+++ src/middleware/operation/handle_args/index.js
@@ -1,35 +1,7 @@
 'use strict';
 
 
-const { cloneDeep } = require('lodash');
-
-
-// Process client-supplied args: validates them and add them to JSL variables
-const handleArgs = function () {
-  return async function handleArgs(input) {
-    const { log, args, jsl } = input;
-    const perf = log.perf.start('operation.handleArgs', 'middleware');
-
-    const clonedArgs = cloneDeep(args);
-    input.jsl = jsl.add({ $ARGS: clonedArgs });
-
-    try {
-      perf.stop();
-      const response = await this.next(input);
-      return response;
-    } catch (error) {
-      const perf = log.perf.start('operation.handleArgs', 'exception');
-
-      // Added only for final error handler
-      log.add({ args: clonedArgs });
-
-      perf.stop();
-      throw error;
-    }
-  };
-};
-
-
-module.exports = {
-  handleArgs,
-};
+module.exports = Object.assign(
+  {},
+  require('./handler'),
+);
---
+++ src/middleware/operation/handle_args/validate.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+const { EngineError } = require('../../../error');
+
+
+const validateArgs = function ({ args, maxDataLength }) {
+  validateLimits({ args, maxDataLength });
+};
+
+// Check input is not too big
+const validateLimits = function ({ args: { data }, maxDataLength }) {
+  const isDataTooBig = data instanceof Array &&
+    data.length > maxDataLength &&
+    maxDataLength > 0;
+  if (isDataTooBig) {
+    const message = `argument 'data' must contain at most ${maxDataLength} items`;
+    throw new EngineError(message, { reason: 'INPUT_LIMIT' });
+  }
+};
+
+
+module.exports = {
+  validateArgs,
+};
