bc756809e992b347118fb9fbc2d95460eeb30518
ehmicky <ehmicky@gmail.com>
2017-06-16 13:39:29 +0200

    Validates method and goal

---
+++ src/constants/goals.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+const { makeImmutable } = require('../utilities');
+
+
+const GOALS = ['find', 'create', 'replace', 'update', 'delete'];
+makeImmutable(GOALS);
+
+
+module.exports = {
+  GOALS,
+};
