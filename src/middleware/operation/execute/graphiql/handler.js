92268ea064070d45ba8673da768f9d448ceb68f4
autoserver-founder <founder@autoserver.io>
2017-06-16 18:31:26 +0200

    Remove startup phase in GraphQL operation

--- src/middleware/operation/execute/graphiql/handler.js
+++ src/middleware/operation/execute/graphiql/handler.js
@@ -4,30 +4,28 @@
 const { renderGraphiQL } = require('./render');
 
 
-const executeGraphiql = function () {
-  return async function executeGraphiql(input) {
-    const { queryVars, payload = {}, origin, log } = input;
-    const perf = log.perf.start('operation.executeGraphiql', 'middleware');
+const executeGraphiql = async function (input) {
+  const { queryVars, payload = {}, origin, log } = input;
+  const perf = log.perf.start('operation.executeGraphiql', 'middleware');
 
-    const endpointURL = `${origin}/graphql`;
-    const {
-      query,
-      variables,
-      operationName,
-    } = Object.assign({}, queryVars, payload);
+  const endpointURL = `${origin}/graphql`;
+  const {
+    query,
+    variables,
+    operationName,
+  } = Object.assign({}, queryVars, payload);
 
-    const content = await renderGraphiQL({
-      endpointURL,
-      query,
-      variables,
-      operationName,
-    });
+  const content = await renderGraphiQL({
+    endpointURL,
+    query,
+    variables,
+    operationName,
+  });
 
-    perf.stop();
-    return {
-      type: 'html',
-      content,
-    };
+  perf.stop();
+  return {
+    type: 'html',
+    content,
   };
 };
 
