508f606d659ac54e59d1f3f4fd62ea74b627357c
ehmicky <ehmicky@gmail.com>
2017-06-16 17:52:53 +0200

    Add IDL transforms shortcuts

--- src/idl/normalize/shortcuts.js
+++ src/idl/normalize/shortcuts.js
@@ -3,14 +3,17 @@
 
 const { getReadOnlyMap } = require('./read_only');
 const { getUserDefaultsMap } = require('./user_defaults');
+const { getTransformsMap } = require('./transform_shortcut');
 
 
 // Compile-time transformations just meant for runtime performance optimization
 const normalizeShortcuts = function ({ idl }) {
   const readOnlyMap = getReadOnlyMap({ idl });
   const userDefaultsMap = getUserDefaultsMap({ idl });
+  const transformsMap = getTransformsMap({ idl, type: 'transform' });
+  const computesMap = getTransformsMap({ idl, type: 'compute' });
 
-  idl.shortcuts = { readOnlyMap, userDefaultsMap };
+  idl.shortcuts = { readOnlyMap, userDefaultsMap, transformsMap, computesMap };
   return idl;
 };
 
