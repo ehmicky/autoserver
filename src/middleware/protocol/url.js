566404d3ddc602940152a9e52b8d1087731000f5
autoserver-founder <founder@autoserver.io>
2017-06-16 13:48:32 +0200

    Validate origin, path, queryString

--- src/middleware/protocol/url.js
+++ src/middleware/protocol/url.js
@@ -1,6 +1,9 @@
 'use strict';
 
 
+const { EngineError } = require('../../error');
+
+
 // Fill in:
 //  - `input.url`: full URL, e.g. used for logging
 //  - `input.path`: URL's path, e.g. used by router
@@ -11,8 +14,8 @@ const parseUrl = function () {
     const { protocolHandler, log, specific } = input;
     const perf = log.perf.start('protocol.parseUrl', 'middleware');
 
-    const origin = protocolHandler.getOrigin({ specific });
-    const path = protocolHandler.getPath({ specific });
+    const origin = getOrigin({ specific, protocolHandler });
+    const path = getPath({ specific, protocolHandler });
     const url = `${origin}${path}`;
 
     log.add({ url, path, origin });
@@ -22,6 +25,28 @@ const parseUrl = function () {
     const response = await this.next(input);
     return response;
   };
+};
+
+const getOrigin = function ({ specific, protocolHandler }) {
+  const origin = protocolHandler.getOrigin({ specific });
+
+  if (typeof origin !== 'string') {
+    const message = `'origin' must be a string, not '${origin}'`;
+    throw new EngineError(message, { reason: 'SERVER_INPUT_VALIDATION' });
+  }
+
+  return origin;
+};
+
+const getPath = function ({ specific, protocolHandler }) {
+  const path = protocolHandler.getPath({ specific });
+
+  if (typeof path !== 'string') {
+    const message = `'path' must be a string, not '${path}'`;
+    throw new EngineError(message, { reason: 'SERVER_INPUT_VALIDATION' });
+  }
+
+  return path;
 };
 
 
