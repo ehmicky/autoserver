26a818c421e6f49594d7db2d3fef2cc15eff79c4
autoserver-founder <founder@autoserver.io>
2017-06-16 17:55:09 +0200

    Add aliases IDL shortcuts

--- src/idl/normalize/shortcuts.js
+++ src/idl/normalize/shortcuts.js
@@ -4,6 +4,7 @@
 const { getReadOnlyMap } = require('./read_only');
 const { getUserDefaultsMap } = require('./user_defaults');
 const { getTransformsMap } = require('./transform_shortcut');
+const { getAliasesMap } = require('./aliases');
 
 
 // Compile-time transformations just meant for runtime performance optimization
@@ -12,12 +13,14 @@ const normalizeShortcuts = function ({ idl }) {
   const userDefaultsMap = getUserDefaultsMap({ idl });
   const transformsMap = getTransformsMap({ idl, type: 'transform' });
   const computesMap = getTransformsMap({ idl, type: 'compute' });
+  const aliasesMap = getAliasesMap({ idl });
 
   idl.shortcuts = {
     readOnlyMap,
     userDefaultsMap,
     transformsMap,
     computesMap,
+    aliasesMap,
   };
   return idl;
 };
