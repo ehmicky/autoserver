db5a7383ba212704b2c6521df88b8e07b206738e
ehmicky <ehmicky@gmail.com>
2017-06-16 17:47:07 +0200

    Add readOnly IDL shortcut

---
+++ src/idl/normalize/read_only.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+const { mapValues, pickBy } = require('../../utilities');
+
+
+// Gets a map of models' readonly attributes,
+// e.g. { my_model: ['my_readonly_attribute', ...], ... }
+const getReadOnlyMap = function ({ idl: { models } }) {
+  return mapValues(models, ({ properties = {} }) => {
+    const readOnlyProps = pickBy(properties, ({ readOnly }) => readOnly);
+    return Object.keys(readOnlyProps);
+  });
+};
+
+
+module.exports = {
+  getReadOnlyMap,
+};
