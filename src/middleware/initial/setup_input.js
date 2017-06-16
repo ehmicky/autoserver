b0cabb22e48c4c16d585248f0bc13a72bcd59f7b
ehmicky <ehmicky@gmail.com>
2017-06-16 13:22:21 +0200

    Make sure socket never hangs

--- src/middleware/initial/setup_input.js
+++ src/middleware/initial/setup_input.js
@@ -2,14 +2,16 @@
 
 
 const { Log } = require('../../logging');
+const { protocolHandlers } = require('../../protocols');
 
 
 // Setup basic input
 const setupInput = function ({ serverOpts, serverState }) {
   return async function setupInput(protocol, specific) {
     const log = new Log({ serverOpts, serverState, phase: 'request' });
+    const protocolHandler = protocolHandlers[protocol];
 
-    const input = { specific, protocol, log };
+    const input = { specific, protocol, protocolHandler, log };
 
     const response = await this.next(input);
     return response;
--- src/middleware/protocol/convertor.js
+++ src/middleware/protocol/convertor.js
@@ -2,19 +2,16 @@
 
 
 const { Jsl } = require('../../jsl');
-const { protocolHandlers } = require('../../protocols');
 
 
 // Converts from no format to Protocol format
 const protocolConvertor = function ({ idl: { helpers, exposeMap } }) {
   return async function protocolConvertor(input) {
-    const { specific, log, protocol, now } = input;
+    const { specific = {}, log, protocol, protocolHandler, now } = input;
 
     const perf = log.perf.start('protocol.convertor', 'middleware');
 
-    const protocolHandler = protocolHandlers[protocol];
-
-    const protocolFullName = protocolHandler.getFullName(input);
+    const protocolFullName = protocolHandler.getFullName({ specific });
     log.add({ protocol, protocolFullName });
 
     const jsl = new Jsl({ exposeMap });
