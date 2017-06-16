a467b93a827a8c3ab3895a75dec0106edcd3852d
autoserver-founder <founder@autoserver.io>
2017-06-16 19:07:10 +0200

    Fix startup performance logging

--- src/idl/normalize/graphql.js
+++ src/idl/normalize/graphql.js
@@ -5,9 +5,14 @@ const { getSchema } = require('./graphql_schema');
 const { printSchema } = require('./graphql_print');
 
 // Add GraphQL schema, both as AST and as HTML output
-const normalizeGraphQL = async function ({ idl, serverOpts }) {
+const normalizeGraphQL = async function ({ idl, serverOpts, startupLog }) {
+  const schemaPerf = startupLog.perf.start('schema', 'graphql');
   const GraphQLSchema = getSchema({ idl, serverOpts });
+  schemaPerf.stop();
+
+  const printPerf = startupLog.perf.start('print', 'graphql');
   const GraphQLPrintedSchema = await printSchema({ schema: GraphQLSchema });
+  printPerf.stop();
 
   Object.assign(idl, { GraphQLSchema, GraphQLPrintedSchema });
   return idl;
