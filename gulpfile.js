694759a2baa030920ced79ed247f0b32fdff7fc3
autoserver-founder <founder@autoserver.io>
2017-06-27 23:04:10 +0200

    Add more ESLint rules

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,16 @@ const eslint = require('gulp-eslint');
 const { gulpSrc } = require('./gulp');
 
 
-gulp.task('lint', () => {
-  return gulpSrc('lint')
+gulp.task('lint', () =>
+  gulpSrc('lint')
     .pipe(eslint({
       cache: true,
       ignorePath: '.gitignore',
       // fix: true,
     }))
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
+    .pipe(eslint.format('codeframe'))
+    .pipe(eslint.failAfterError())
+);
 
 
 gulp.task('test', ['lint']);
