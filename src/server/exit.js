ec32c17f1dba2ee751638ae81091ca33a36f6de0
ehmicky <ehmicky@gmail.com>
2017-07-17 15:21:54 +0200

    Fix block-spacing ESLint rule

--- src/server/exit.js
+++ src/server/exit.js
@@ -146,7 +146,7 @@ const exit = async function ({ isSuccess, apiServer }) {
   const eventName = isSuccess ? 'stop.success' : 'stop.fail';
   try {
     await apiServer.emitAsync(eventName);
-  } catch (error) {/* */}
+  } catch (error) { /* */ }
 
   // Used by Nodemon
   process.kill(process.pid, 'SIGUSR2');
