e76698abc66331713079f83b00784b88bd551164
ehmicky <ehmicky@gmail.com>
2017-07-19 16:46:21 +0200

    Simplify IDL normalization

--- src/idl/normalize/main.js
+++ src/idl/normalize/main.js
@@ -6,33 +6,22 @@ const { normalizeHelpers } = require('./helpers');
 const { normalizeShortcuts } = require('./shortcuts');
 const { normalizeGraphQL } = require('./graphql');
 
+const normalizers = [
+  normalizeCommands,
+  normalizeModels,
+  normalizeHelpers,
+  normalizeShortcuts,
+  normalizeGraphQL,
+];
+
 // Normalize IDL definition
 const normalizeIdl = function ({ idl: oIdl, serverOpts, startupLog }) {
-  const commandsPerf = startupLog.perf.start('commands', 'normalize');
-  const idlWithCommands = normalizeCommands({ idl: oIdl });
-  commandsPerf.stop();
-
-  const modelsPerf = startupLog.perf.start('models', 'normalize');
-  const idlWithModels = normalizeModels({ idl: idlWithCommands });
-  modelsPerf.stop();
-
-  const helpersPerf = startupLog.perf.start('helpers', 'normalize');
-  const idlWithHelpers = normalizeHelpers({ idl: idlWithModels });
-  helpersPerf.stop();
-
-  const shortcutsPerf = startupLog.perf.start('shortcuts', 'normalize');
-  const idlWithShortcuts = normalizeShortcuts({ idl: idlWithHelpers });
-  shortcutsPerf.stop();
-
-  const graphqlPerf = startupLog.perf.start('graphql', 'normalize');
-  const idl = normalizeGraphQL({
-    idl: idlWithShortcuts,
-    serverOpts,
-    startupLog,
-  });
-  graphqlPerf.stop();
-
-  return idl;
+  return normalizers.reduce((idl, normalizer) => {
+    const perf = startupLog.perf.start(normalizer.name, 'normalize');
+    const newIdl = normalizer({ idl, serverOpts, startupLog });
+    perf.stop();
+    return newIdl;
+  }, oIdl);
 };
 
 module.exports = {
