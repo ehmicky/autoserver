295d5d83108b3351c1653ce7a85fb395b855bc4c
autoserver-founder <founder@autoserver.io>
2017-07-20 18:50:37 +0200

    Refactor args validation

---
+++ src/middleware/action/handle_args/syntax/validate.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { pick, omit, mapKeys, mapValues } = require('../../../../utilities');
+const { validate } = require('../../../../validation');
+
+const { actionsArgs } = require('./actions');
+const { commonSchema, argsSchema } = require('./schemas');
+
+/**
+ * Check arguments, for client-side errors.
+ * In a nutshell, checks that:
+ *  - required arguments are defined
+ *  - disabled or unknown arguments are not defined
+ *  - arguments that are defined follow correct syntax
+ *    Does not check for semantics (e.g. IDL validation)
+ **/
+const validateSyntax = function ({ args, action }) {
+  const type = 'clientInputSyntax';
+  const schema = actionSchemas[action.name];
+  validate({ schema, data: args, reportInfo: { type, dataVar: 'args' } });
+};
+
+const getActionSchema = function ({ optional = [], required = [] }) {
+  const optionalProps = pick(argsSchema, [...optional, ...required]);
+  const mappedProps = mapKeys(optionalProps, (key, { name }) => name || key);
+  const requiredProps = required.map(name => argsSchema[name].name || name);
+  const properties = mapValues(mappedProps, value => omit(value, 'name'));
+
+  const schema = Object.assign(
+    { properties, required: requiredProps },
+    commonSchema,
+  );
+  return schema;
+};
+
+// JSON schemas validating `args` for each action
+const actionSchemas = mapValues(actionsArgs, args => getActionSchema(args));
+
+module.exports = {
+  validateSyntax,
+};
