430ca796d8212be2c9b71fb71c25e3bff5f68d2d
autoserver-founder <founder@autoserver.io>
2017-07-21 11:58:33 +0200

    Refactor YAML loading

--- src/utilities/template.js
+++ src/utilities/template.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const { readFile } = require('fs');
-const { promisify } = require('util');
-
 const { render } = require('mustache');
 
+const { readFile } = require('./filesystem');
+
 const renderTemplate = async function ({ template, data }) {
-  const htmlFile = await promisify(readFile)(template, { encoding: 'utf-8' });
+  const htmlFile = await readFile(template);
   const htmlString = render(htmlFile, data);
   return htmlString;
 };
