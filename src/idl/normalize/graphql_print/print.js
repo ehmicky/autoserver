3c6fa04d9724c457b8d18476758a1f5488aa754e
autoserver-founder <founder@autoserver.io>
2017-07-21 11:27:20 +0200

    Refactor templating

--- src/idl/normalize/graphql_print/print.js
+++ src/idl/normalize/graphql_print/print.js
@@ -1,23 +1,19 @@
 'use strict';
 
-const { readFile } = require('fs');
 const { resolve } = require('path');
-const { promisify } = require('util');
 
 const { printSchema: graphQLPrintSchema } = require('graphql');
-const { render } = require('mustache');
 
-const PRINT_HTML_FILE = resolve(__dirname, './print.mustache');
+const { renderTemplate } = require('../../../utilities');
+
+const template = resolve(__dirname, './print.mustache');
 
 const printSchema = async function ({ schema }) {
-  const data = {
-    printedSchema: graphQLPrintSchema(schema).trim(),
-    prismVersion: '1.6.0',
-  };
-  const htmlFile = await promisify(readFile)(PRINT_HTML_FILE, {
-    encoding: 'utf-8',
+  const printedSchema = graphQLPrintSchema(schema).trim();
+  const htmlString = await renderTemplate({
+    template,
+    data: { printedSchema, prismVersion: '1.6.0' },
   });
-  const htmlString = render(htmlFile, data);
   return htmlString;
 };
 
