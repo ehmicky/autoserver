59a9ea56d8042864151b0872ec718a7f4981286e
ehmicky <ehmicky@gmail.com>
2017-07-21 16:16:44 +0200

    Small refactoring

--- src/middleware/operation/execute/graphql/resolver/utilities/name.js
+++ src/middleware/operation/execute/graphql/resolver/utilities/name.js
@@ -11,18 +11,22 @@ const nameRegExp = /^([a-z0-9]+)([A-Z][a-zA-Z0-9]*)/;
  * E.g. `findMyModels` -> { actionType: 'find', attrName: 'my_models' }
  **/
 const parseName = function ({ name }) {
-  const parts = nameRegExp.exec(name);
-  if (!parts) { return {}; }
-
-  const actionType = parts[1].trim();
-  if (!actionType) { return {}; }
-
-  const rawAttrName = parts[2].trim();
-  if (!rawAttrName) { return {}; }
+  const [actionType, rawAttrName] = parseNameParts({ name });
+  if (!actionType || !rawAttrName) { return {}; }
 
   const attrName = underscored(rawAttrName);
 
   return { actionType, attrName };
+};
+
+const parseNameParts = function ({ name }) {
+  const parts = nameRegExp.exec(name);
+  if (!parts) { return []; }
+
+  const actionType = parts[1].trim();
+  const rawAttrName = parts[2].trim();
+
+  return [actionType, rawAttrName];
 };
 
 // Similar to introspection utility `getTypeName`
