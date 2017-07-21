cfca28b0c9c415e8b5757060b4bc80753a060927
autoserver-founder <founder@autoserver.io>
2017-07-21 17:20:26 +0200

    Small refactoring

--- src/logging/perf/log.js
+++ src/logging/perf/log.js
@@ -144,38 +144,41 @@ const validateOptions = function ({ label, category }) {
 // Returns measures but as a single string, for console debugging
 const getMeasuresMessage = function ({ measures }) {
   return measures
-    // Sort by category (asc) then by duration (desc)
-    .sort((
-      { category: catA, duration: timeA },
-      { category: catB, duration: timeB },
-    ) => {
-      const indexCatA = CATEGORIES.indexOf(catA);
-      const indexCatB = CATEGORIES.indexOf(catB);
-      if (indexCatA < indexCatB) { return -1; }
-      if (indexCatA > indexCatB) { return 1; }
-      if (timeA < timeB) { return 1; }
-      if (timeA > timeB) { return -1; }
-      return 0;
-    })
-    // Prints as a table
-    .map(({
-      phase,
-      category,
-      label,
-      average,
-      count,
-      duration,
-    }) => {
-      const phaseS = phase.padEnd(8);
-      const categoryS = category.padEnd(12);
-      const labelS = label.padEnd(26);
-      const durationS = `${Math.round(duration)}ms`.padEnd(8);
-      const averageS = `${Math.round(average)}ms`.padEnd(7);
-      const items = count === 1 ? 'item' : 'items';
-      const countS = `${String(count).padStart(3)} ${items}`;
-      return `${phaseS} ${categoryS} ${labelS} ${durationS} = ${averageS} * ${countS}`;
-    })
+    .sort(sortMeasures)
+    .map(stringifyMeasure)
     .join('\n');
+};
+
+// Sort by category (asc) then by duration (desc)
+const sortMeasures = function (
+  { category: catA, duration: timeA },
+  { category: catB, duration: timeB },
+) {
+  const indexCatA = CATEGORIES.indexOf(catA);
+  const indexCatB = CATEGORIES.indexOf(catB);
+  if (indexCatA < indexCatB) { return -1; }
+  if (indexCatA > indexCatB) { return 1; }
+  if (timeA < timeB) { return 1; }
+  return timeA > timeB ? -1 : 0;
+};
+
+// Prints as a table
+const stringifyMeasure = function ({
+  phase,
+  category,
+  label,
+  average,
+  count,
+  duration,
+}) {
+  const phaseS = phase.padEnd(8);
+  const categoryS = category.padEnd(12);
+  const labelS = label.padEnd(26);
+  const durationS = `${Math.round(duration)}ms`.padEnd(8);
+  const averageS = `${Math.round(average)}ms`.padEnd(7);
+  const items = count === 1 ? 'item' : 'items';
+  const countS = `${String(count).padStart(3)} ${items}`;
+  return `${phaseS} ${categoryS} ${labelS} ${durationS} = ${averageS} * ${countS}`;
 };
 
 const DEFAULT_CATEGORY = 'default';
