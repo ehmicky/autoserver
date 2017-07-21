6ebc51c0674a438c63048e191f6b25510bf12509
ehmicky <ehmicky@gmail.com>
2017-07-21 15:56:04 +0200

    Small refactoring

--- src/idl/plugins/properties.js
+++ src/idl/plugins/properties.js
@@ -22,42 +22,61 @@ const propertiesPlugin = function ({
 
     const properties = getProperties(opts);
 
-    idl.models = mapValues(models, (model, modelName) => {
-      const modelProperties = model.properties || {};
-      const propNames = Object.keys(modelProperties);
-      const newPropNames = Object.keys(properties);
-
-      // Make sure plugin does not override user-defined properties
-      const alreadyDefinedProps = intersection(propNames, newPropNames);
-
-      if (alreadyDefinedProps.length > 0) {
-        const propMessage = getPropMessage(alreadyDefinedProps);
-        const message = `In model ${modelName}, cannot override ${propMessage}`;
-        throw new EngineError(message, { reason: 'IDL_VALIDATION' });
-      }
-
-      // Make sure plugin required properties exist
-      const missingRequiredProps = difference(
-        requiredProperties,
-        [...propNames, ...newPropNames]
-      );
-
-      if (missingRequiredProps.length > 0) {
-        const propMessage = getPropMessage(missingRequiredProps);
-        const message = `In model ${modelName}, ${propMessage} should exist`;
-        throw new EngineError(message, { reason: 'IDL_VALIDATION' });
-      }
-
-      // Modifies models
-      const newModel = Object.assign(model, {
-        properties: Object.assign(modelProperties, properties),
-        required: uniq([...(model.required || []), ...requiredProperties]),
-      });
-
-      return newModel;
-    });
+    idl.models = mapValues(models, (model, modelName) =>
+      getNewModel({ model, modelName, properties, requiredProperties })
+    );
     return idl;
   };
+};
+
+const getNewModel = function ({
+  model,
+  modelName,
+  properties,
+  requiredProperties,
+}) {
+  const modelProperties = model.properties || {};
+
+  validateProps({ modelProperties, modelName, properties, requiredProperties });
+
+  // Modifies models
+  const newModel = Object.assign(model, {
+    properties: Object.assign(modelProperties, properties),
+    required: uniq([...(model.required || []), ...requiredProperties]),
+  });
+
+  return newModel;
+};
+
+const validateProps = function ({
+  modelProperties,
+  modelName,
+  properties,
+  requiredProperties,
+}) {
+  const propNames = Object.keys(modelProperties);
+  const newPropNames = Object.keys(properties);
+
+  // Make sure plugin does not override user-defined properties
+  const alreadyDefinedProps = intersection(propNames, newPropNames);
+
+  if (alreadyDefinedProps.length > 0) {
+    const propMessage = getPropMessage(alreadyDefinedProps);
+    const message = `In model ${modelName}, cannot override ${propMessage}`;
+    throw new EngineError(message, { reason: 'IDL_VALIDATION' });
+  }
+
+  // Make sure plugin required properties exist
+  const missingRequiredProps = difference(
+    requiredProperties,
+    [...propNames, ...newPropNames]
+  );
+
+  if (missingRequiredProps.length > 0) {
+    const propMessage = getPropMessage(missingRequiredProps);
+    const message = `In model ${modelName}, ${propMessage} should exist`;
+    throw new EngineError(message, { reason: 'IDL_VALIDATION' });
+  }
 };
 
 // Returns human-friendly version of properties, e.g. 'property my_prop' or
