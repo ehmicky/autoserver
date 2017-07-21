bcce5ab89882e69ba1a2285e0c122e2193f1e9c9
ehmicky <ehmicky@gmail.com>
2017-07-21 10:59:42 +0200

    Refactor pick|omit utilities

--- src/utilities/functional/filter.js
+++ src/utilities/functional/filter.js
@@ -2,46 +2,38 @@
 
 const { checkObject } = require('./validate');
 
-// Similar to lodash pick(), but faster.
-const pick = function (obj, attribute) {
-  checkObject(obj);
-
-  const attributes = Array.isArray(attribute) ? attribute : [attribute];
-  return pickBy(obj, (value, name) => attributes.includes(name));
-};
-
 // Similar to lodash pickBy(), but faster.
 const pickBy = function (obj, condition) {
   checkObject(obj);
 
-  return Object.entries(obj).reduce((memo, [name, value]) => {
-    if (condition(value, name)) {
-      memo[name] = value;
+  return Object.entries(obj).reduce((memo, [key, value]) => {
+    if (condition(value, key)) {
+      memo[key] = value;
     }
 
     return memo;
   }, {});
-};
-
-// Similar to lodash omit(), but faster.
-const omit = function (obj, attribute) {
-  checkObject(obj);
-
-  const attributes = Array.isArray(attribute) ? attribute : [attribute];
-  return omitBy(obj, (value, name) => attributes.includes(name));
 };
 
 // Similar to lodash omitBy(), but faster.
 const omitBy = function (obj, condition) {
-  checkObject(obj);
+  return pickBy(obj, (...args) => !condition(...args));
+};
 
-  return Object.entries(obj).reduce((memo, [name, value]) => {
-    if (!condition(value, name)) {
-      memo[name] = value;
-    }
+// Similar to lodash pick(), but faster.
+const pick = function (obj, attribute) {
+  return picker({ obj, attribute, shouldOmit: false });
+};
 
-    return memo;
-  }, {});
+// Similar to lodash omit(), but faster.
+const omit = function (obj, attribute) {
+  return picker({ obj, attribute, shouldOmit: true });
+};
+
+const picker = function ({ obj, attribute, shouldOmit = false }) {
+  const attributes = Array.isArray(attribute) ? attribute : [attribute];
+  const pickFunc = shouldOmit ? omitBy : pickBy;
+  return pickFunc(obj, (value, key) => attributes.includes(key));
 };
 
 module.exports = {
