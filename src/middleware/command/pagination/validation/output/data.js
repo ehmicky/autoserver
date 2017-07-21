c708cffb3978de3858a3c14d2ceab52d77e51265
ehmicky <ehmicky@gmail.com>
2017-07-21 16:24:52 +0200

    Small refactoring

--- src/middleware/command/pagination/validation/output/data.js
+++ src/middleware/command/pagination/validation/output/data.js
@@ -5,26 +5,29 @@ const { decode } = require('../../encoding');
 
 // Returns response.metadata related to pagination, after decoding token
 const getOutputData = function ({ metadata }) {
-  return metadata.map(({ pages }) => {
-    const { token } = pages;
-    if (token === undefined || token === '') { return pages; }
+  return metadata.map(metadatum => getOutputMetadata({ metadatum }));
+};
 
-    if (typeof token !== 'string') {
-      const message = 'Wrong response: \'token\' must be a string';
-      throw new EngineError(message, { reason: 'OUTPUT_VALIDATION' });
-    }
+const getOutputMetadata = function ({
+  metadatum: { pages, pages: { token } },
+}) {
+  if (token === undefined || token === '') { return pages; }
 
-    try {
-      const parsedToken = decode({ token });
-      return Object.assign({}, pages, { token: parsedToken });
-    } catch (error) {
-      const message = 'Wrong response: \'token\' is invalid';
-      throw new EngineError(message, {
-        reason: 'OUTPUT_VALIDATION',
-        innererror: error,
-      });
-    }
-  });
+  if (typeof token !== 'string') {
+    const message = 'Wrong response: \'token\' must be a string';
+    throw new EngineError(message, { reason: 'OUTPUT_VALIDATION' });
+  }
+
+  try {
+    const parsedToken = decode({ token });
+    return Object.assign({}, pages, { token: parsedToken });
+  } catch (error) {
+    const message = 'Wrong response: \'token\' is invalid';
+    throw new EngineError(message, {
+      reason: 'OUTPUT_VALIDATION',
+      innererror: error,
+    });
+  }
 };
 
 module.exports = {
