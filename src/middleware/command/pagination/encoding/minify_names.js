4794c27666faaaa782604e57f5d772b941ee3b1d
ehmicky <ehmicky@gmail.com>
2017-07-24 10:22:11 +0200

    Simplify pagination minify_names

--- src/middleware/command/pagination/encoding/minify_names.js
+++ src/middleware/command/pagination/encoding/minify_names.js
@@ -1,35 +1,27 @@
 'use strict';
 
-const { omit, assignObject } = require('../../../../utilities');
+const { invert, mapKeys } = require('../../../../utilities');
 
 // Name shortcuts, e.g. { nFilter: value } -> { f: value }
 const addNameShortcuts = function (token) {
-  const shortcuts = namesShortcuts
-    .filter(({ from }) => token[from] !== undefined)
-    .map(({ from, to }) => ({ [to]: token[from] }))
-    .reduce(assignObject, {});
-  const attrsToRemove = namesShortcuts
-    .filter(({ from }) => token[from] !== undefined)
-    .map(({ from }) => from);
-  return Object.assign({}, omit(token, attrsToRemove), shortcuts);
+  return mapKeys(token, (value, attrName) =>
+    shortcuts[attrName] || attrName
+  );
 };
 
 const removeNameShortcuts = function (token) {
-  const shortcuts = namesShortcuts
-    .filter(({ to }) => token[to] !== undefined)
-    .map(({ from, to }) => ({ [from]: token[to] }))
-    .reduce(assignObject, {});
-  const attrsToRemove = namesShortcuts
-    .filter(({ to }) => token[to] !== undefined)
-    .map(({ to }) => to);
-  return Object.assign({}, omit(token, attrsToRemove), shortcuts);
+  return mapKeys(token, (value, attrName) =>
+    reverseShortcuts[attrName] || attrName
+  );
 };
 
-const namesShortcuts = [
-  { from: 'nFilter', to: 'f' },
-  { from: 'nOrderBy', to: 'o' },
-  { from: 'parts', to: 'p' },
-];
+const shortcuts = {
+  nFilter: 'f',
+  nOrderBy: 'o',
+  parts: 'p',
+};
+
+const reverseShortcuts = invert(shortcuts);
 
 module.exports = {
   addNameShortcuts,
