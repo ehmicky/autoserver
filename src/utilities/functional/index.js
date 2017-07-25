20a208ea83e172c186e480ecc6b6318e74a31774
ehmicky <ehmicky@gmail.com>
2017-07-25 14:07:31 +0200

    Remove dead code

--- src/utilities/functional/index.js
+++ src/utilities/functional/index.js
@@ -3,16 +3,13 @@
 module.exports = Object.assign(
   {},
   require('./map'),
-  require('./recurse_map'),
   require('./reduce'),
   require('./filter'),
-  require('./transform'),
   require('./invert'),
   require('./merge'),
   require('./immutable'),
   require('./memoize'),
   require('./once'),
   require('./buffer'),
-  // eslint-disable-next-line import/max-dependencies
   require('./identity'),
 );
