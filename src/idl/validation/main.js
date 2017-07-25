1af9507c6b09acdff9f68454b85cd0b915ed44bc
ehmicky <ehmicky@gmail.com>
2017-07-25 18:05:37 +0200

    Perf monitoring of IDL validation

--- src/idl/validation/main.js
+++ src/idl/validation/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { reduceAsync } = require('../../utilities');
+
 const { validateIdlCircularRefs } = require('./circular_refs');
 const { validateData } = require('./data');
 const { validateIdlSyntax } = require('./syntax');
@@ -16,12 +18,13 @@ const validators = [
 ];
 
 // Validate IDL definition
-const validateIdl = async function ({ idl }) {
-  const validatedIdl = await validators.reduce(
-    async (newIdl, validator) => validator(await newIdl),
-    idl,
-  );
-  return validatedIdl;
+const validateIdl = function ({ idl: oIdl, startupLog }) {
+  return reduceAsync(validators, async (idl, validator) => {
+    const perf = startupLog.perf.start(validator.name, 'validate');
+    const newIdl = await validator(idl);
+    perf.stop();
+    return newIdl;
+  }, oIdl);
 };
 
 module.exports = {
