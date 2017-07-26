72a7cb2a9ff3501ebc19fd05f9e354c67ff9811d
autoserver-founder <founder@autoserver.io>
2017-07-26 15:41:13 +0200

    Add groupBy utility

---
+++ src/utilities/functional/group.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Group array of objects together according to a specific key
+// `key` can a string (the object key), an array (several object keys) or
+// a function returning a string.
+const groupBy = function (array, key) {
+  return array.reduce((groups, obj) => {
+    const groupName = getGroupName(key, obj);
+    const currentGroup = groups[groupName] || [];
+    const newGroup = [...currentGroup, obj];
+    return Object.assign({}, groups, { [groupName]: newGroup });
+  }, []);
+};
+
+const getGroupName = function (key, obj) {
+  if (typeof key === 'function') {
+    return key(obj);
+  }
+
+  if (Array.isArray(key)) {
+    return key.map(name => obj[name]).join(',');
+  }
+
+  return obj[key];
+};
+
+module.exports = {
+  groupBy,
+};
