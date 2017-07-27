37198820c6689a5c8022ddbf78328765c27a6997
ehmicky <ehmicky@gmail.com>
2017-07-27 11:03:34 +0200

    Refactor jsl checkNames()

--- src/jsl/run/validation.js
+++ src/jsl/run/validation.js
@@ -1,24 +1,29 @@
 'use strict';
 
 const { throwError } = require('../../error');
+const { mapKeys } = require('../../utilities');
 
 // Make sure there is no name conflicts between system helpers and
 // user-supplied helpers, by forcing the former to be $UPPER_CASE, and
 // forbidding the latter to be so
-const checkNames = function (input, type) {
-  const isSystemType = type === 'SYSTEM';
+const checkNames = function ({ params, type }) {
+  return mapKeys(params, (value, name) => checkName({ name, type }));
+};
 
-  for (const name of Object.keys(input)) {
-    const isSystemName = systemNameRegExp.test(name);
+const checkName = function ({ name, type }) {
+  const isSystemName = systemNameRegExp.test(name);
 
-    if (isSystemType && !isSystemName) {
-      const message = `JSL helper named '${name}' should be uppercase/underscore only and start with $`;
-      throwError(message, { reason: 'UTILITY_ERROR' });
-    } else if (!isSystemType && isSystemName) {
-      const message = `JSL helper named '${name}' should not be uppercase/underscore only and start with $`;
-      throwError(message, { reason: 'UTILITY_ERROR' });
-    }
+  if (type === 'SYSTEM' && !isSystemName) {
+    const message = `JSL helper named '${name}' should be uppercase/underscore only and start with $`;
+    throwError(message, { reason: 'UTILITY_ERROR' });
   }
+
+  if (type !== 'SYSTEM' && isSystemName) {
+    const message = `JSL helper named '${name}' should not be uppercase/underscore only and start with $`;
+    throwError(message, { reason: 'UTILITY_ERROR' });
+  }
+
+  return name;
 };
 
 const systemNameRegExp = /^\$[A-Z_]+$/;
