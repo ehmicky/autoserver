55de7be68c4ca61c703770a96df23f94f403b998
ehmicky <ehmicky@gmail.com>
2017-07-28 19:18:03 +0200

    Refactor freeze utility

--- src/utilities/functional/immutable.js
+++ src/utilities/functional/immutable.js
@@ -2,21 +2,29 @@
 
 const { ENV } = require('../env');
 
+const { mapValues } = require('./map');
+
 // Deeply Object.freeze() over an object.
 const deepFreeze = function (obj) {
-  const isObjectOrArray = obj &&
-    (obj.constructor === Object || Array.isArray(obj));
-  if (!isObjectOrArray) { return obj; }
+  const isArray = Array.isArray(obj);
+  const isObject = !isArray && obj && obj.constructor === Object;
+  if (!isArray && !isObject) { return obj; }
 
   // Avoid infinite recursions
   const isFrozen = Object.isFrozen(obj);
-  if (isFrozen) { return; }
+  if (isFrozen) { return obj; }
 
   Object.freeze(obj);
 
-  for (const child of Object.values(obj)) {
-    deepFreeze(child);
+  return freezeChildren(obj);
+};
+
+const freezeChildren = function (obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(child => deepFreeze(child));
   }
+
+  return mapValues(obj, child => deepFreeze(child));
 };
 
 // Not in production, because Object.freeze() can be slow.
