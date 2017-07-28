df31ec9a0759cb9f2696c20f6650efe7029fd8d8
ehmicky <ehmicky@gmail.com>
2017-07-28 18:11:34 +0200

    Refactor pagination validation

--- src/middleware/command/pagination/validation/input/data.js
+++ src/middleware/command/pagination/validation/input/data.js
@@ -13,44 +13,61 @@ const getInputData = function ({ args }) {
 };
 
 const validateInputData = function ({ args }) {
-  validateSingleDirection({ args });
-  validateSingleType({ args });
-  validateForbiddenArg({ args });
+  return validators.reduce((argsA, validator) => validator(argsA), args);
 };
 
-const validateSingleDirection = function ({ args }) {
+const validateSingleDirection = function (args) {
   const hasTwoDirections = args.before !== undefined &&
     args.after !== undefined;
-  if (!hasTwoDirections) { return; }
+  if (!hasTwoDirections) { return args; }
 
   const message = 'Wrong parameters: cannot specify both \'before\' and \'after\'';
   throwError(message, { reason: 'INPUT_VALIDATION' });
+
+  return args;
 };
 
-const validateSingleType = function ({ args }) {
+const validateSingleType = function (args) {
   // Cannot mix offset-based pagination and cursor-based pagination
   const hasTwoPaginationTypes = args.page !== undefined &&
     (args.before !== undefined || args.after !== undefined);
-  if (!hasTwoPaginationTypes) { return; }
+  if (!hasTwoPaginationTypes) { return args; }
 
   const message = 'Wrong parameters: cannot use both \'page\' and \'before|after\'';
   throwError(message, { reason: 'INPUT_VALIDATION' });
+
+  return args;
 };
 
-const validateForbiddenArg = function ({ args }) {
+const validateForbiddenArgs = function (args) {
   // Also, cannot specify 'nFilter' or 'nOrderBy' with a cursor, because the
   // cursor already includes them.
-  for (const forbiddenArg of ['nFilter', 'nOrderBy']) {
-    const hasForbiddenArg = args[forbiddenArg] !== undefined &&
-      ((args.before !== undefined && args.before !== '') ||
-      (args.after !== undefined && args.after !== ''));
-
-    if (hasForbiddenArg) {
-      const message = `Wrong parameters: cannot use both '${forbiddenArg}' and 'before|after'`;
-      throwError(message, { reason: 'INPUT_VALIDATION' });
-    }
-  }
+  return forbiddenArgs.reduce(
+    (argsA, forbiddenArg) => validateForbiddenArg(argsA, forbiddenArg),
+    args,
+  );
 };
+
+const forbiddenArgs = ['nFilter', 'nOrderBy'];
+
+const validateForbiddenArg = function (args, forbiddenArg) {
+  const hasForbiddenArg = args[forbiddenArg] !== undefined &&
+    ((args.before !== undefined && args.before !== '') ||
+    (args.after !== undefined && args.after !== ''));
+
+  if (hasForbiddenArg) {
+    const message = `Wrong parameters: cannot use both '${forbiddenArg}' and 'before|after'`;
+    throwError(message, { reason: 'INPUT_VALIDATION' });
+  }
+
+  return args;
+};
+
+const validators = [
+  validateSingleDirection,
+  validateSingleType,
+  validateForbiddenArgs,
+];
 
 const getDecodedTokens = function ({ args }) {
   const decodedTokens = ['before', 'after']
