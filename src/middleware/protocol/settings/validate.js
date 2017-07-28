171c6827df24bd1139c9bcd85e55ea49d7a90c35
ehmicky <ehmicky@gmail.com>
2017-07-28 18:18:41 +0200

    Refactor settings validation

--- src/middleware/protocol/settings/validate.js
+++ src/middleware/protocol/settings/validate.js
@@ -4,23 +4,31 @@ const { throwError } = require('../../../error');
 
 // Generic settings validation
 const validateSettings = function ({ settings }) {
-  for (const [name, value] of Object.entries(settings)) {
-    const validator = validators[name];
-
-    if (!validator) {
-      const message = `Unknown settings: '${name}'`;
-      throwError(message, { reason: 'INPUT_VALIDATION' });
-    }
-
-    validator({ name, value });
-  }
+  return Object.entries(settings).reduce(
+    (settingsA, [name, value]) =>
+      validateSetting({ settings: settingsA, name, value }),
+    settings,
+  );
 };
 
-const noOutput = function ({ name, value }) {
+const validateSetting = function ({ settings, name, value }) {
+  const validator = validators[name];
+
+  if (!validator) {
+    const message = `Unknown settings: '${name}'`;
+    throwError(message, { reason: 'INPUT_VALIDATION' });
+  }
+
+  return validator({ settings, name, value });
+};
+
+const noOutput = function ({ settings, name, value }) {
   if (typeof value !== 'boolean') {
     const message = `'${name}' settings must be 'true' or 'false', not '${value}'`;
     throwError(message, { reason: 'INPUT_VALIDATION' });
   }
+
+  return settings;
 };
 
 const validators = {
