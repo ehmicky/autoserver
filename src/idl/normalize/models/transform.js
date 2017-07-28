68c9e17fbeff08c62abcbba93897d126cdb6feef
autoserver-founder <founder@autoserver.io>
2017-07-28 19:07:09 +0200

    Refactor transform order

--- src/idl/normalize/models/transform.js
+++ src/idl/normalize/models/transform.js
@@ -30,9 +30,14 @@ const getTransformUsing = function ({ model: { properties }, modelName }) {
         .map(({ using = [] }) => using)
         .reduce(assignArray, []);
 
-      validateUsing({ transformUsing, attributes, modelName, attrName });
+      const transformUsingA = validateUsing({
+        transformUsing,
+        attributes,
+        modelName,
+        attrName,
+      });
 
-      return { attrName, using: transformUsing };
+      return { attrName, using: transformUsingA };
     });
 };
 
@@ -43,17 +48,23 @@ const validateUsing = function ({
   modelName,
   attrName,
 }) {
-  for (const using of transformUsing) {
-    if (!attributes.includes(using)) {
-      const message = `'using' property is invalid in model '${modelName}': attribute '${using}' does not exist`;
-      throwError(message, { reason: 'IDL_VALIDATION' });
-    }
+  return transformUsing.map(
+    using => validateOneUsing({ using, attributes, modelName, attrName }),
+  );
+};
 
-    if (using === attrName) {
-      const message = `'using' property is invalid in model '${modelName}': '${using}' refers to current attribute`;
-      throwError(message, { reason: 'IDL_VALIDATION' });
-    }
+const validateOneUsing = function ({ using, attributes, modelName, attrName }) {
+  if (!attributes.includes(using)) {
+    const message = `'using' property is invalid in model '${modelName}': attribute '${using}' does not exist`;
+    throwError(message, { reason: 'IDL_VALIDATION' });
   }
+
+  if (using === attrName) {
+    const message = `'using' property is invalid in model '${modelName}': '${using}' refers to current attribute`;
+    throwError(message, { reason: 'IDL_VALIDATION' });
+  }
+
+  return using;
 };
 
 // Returns order in which transforms should be applied, according to `using`
@@ -61,24 +72,35 @@ const validateUsing = function ({
 const findTransformOrder = function ({ props, modelName, triedProps = [] }) {
   checkTransformCircular({ props, modelName, triedProps });
 
-  for (const [index, prop] of props.entries()) {
-    const nextProps = props.slice(index + 1);
-    // Means the attribute is currently behind another attribute that should be
-    // behind
-    const isWrongOrder = prop.using.some(orderAttr =>
-      nextProps.some(({ attrName }) => attrName === orderAttr)
-    );
-
-    if (isWrongOrder) {
-      // Push the current attribute to the end of the array, and try again
-      const previousProps = props.slice(0, index);
-      const propsA = [...previousProps, ...nextProps, prop];
-      return findTransformOrder({ props: propsA, modelName, triedProps });
-    }
-  }
+  const recTransformOrder = props.find((prop, index) =>
+    findRecTransformOrder({ props, modelName, triedProps, index, prop })
+  );
+  if (recTransformOrder) { return recTransformOrder; }
 
   const transformOrder = props.map(({ attrName }) => attrName);
   return transformOrder;
+};
+
+const findRecTransformOrder = function ({
+  props,
+  modelName,
+  triedProps,
+  index,
+  prop,
+}) {
+  const nextProps = props.slice(index + 1);
+  // Means the attribute is currently behind another attribute that should be
+  // behind
+  const isWrongOrder = prop.using.some(orderAttr =>
+    nextProps.some(({ attrName }) => attrName === orderAttr)
+  );
+
+  if (!isWrongOrder) { return; }
+
+  // Push the current attribute to the end of the array, and try again
+  const previousProps = props.slice(0, index);
+  const propsA = [...previousProps, ...nextProps, prop];
+  return findTransformOrder({ props: propsA, modelName, triedProps });
 };
 
 // If two properties point to each other with `using` property, it means
