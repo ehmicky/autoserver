b7c6e8621e9212cdd38569c4d6b7cdbdb6d510ce
autoserver-founder <founder@autoserver.io>
2017-07-28 18:48:34 +0200

    Refactor aliases

--- src/middleware/command/aliases/input.js
+++ src/middleware/command/aliases/input.js
@@ -5,36 +5,69 @@ const { applyOrderByAliases } = require('./order_by');
 const { applyTokenAliases } = require('./token');
 
 // Apply `alias` in server input
-const applyInputAliases = function ({ args, modelAliases }) {
-  for (const [attrName, aliases] of Object.entries(modelAliases)) {
-    applyInputAlias({ args, attrName, aliases });
-  }
+const applyInputAliases = function ({ input, input: { args }, modelAliases }) {
+  const argsB = Object.entries(modelAliases).reduce(
+    (argsA, [attrName, aliases]) =>
+      applyInputAlias({ args: argsA, attrName, aliases }),
+    args,
+  );
+  return { ...input, args: argsB };
 };
 
 const applyInputAlias = function ({ args = {}, attrName, aliases }) {
-  if (args.newData) {
-    const { currentData, newData } = args;
-    args.newData = applyDataAliases({
-      newData,
-      currentData,
-      attrName,
-      aliases,
-    });
-  }
-
-  if (args.nOrderBy) {
-    const { nOrderBy } = args;
-    args.nOrderBy = applyOrderByAliases({ nOrderBy, attrName, aliases });
-  }
-
-  for (const direction of ['after', 'before']) {
-    const token = args[direction];
-
-    if (token !== undefined && token !== '') {
-      args[direction] = applyTokenAliases({ token, attrName, aliases });
-    }
-  }
+  return modifiers.reduce(
+    (argsA, modifier) => modifier({ args: argsA, attrName, aliases }),
+    args,
+  );
 };
+
+const getNewData = function ({
+  args,
+  args: { newData, currentData },
+  attrName,
+  aliases,
+}) {
+  if (!newData) { return args; }
+
+  const newDataA = applyDataAliases({
+    newData,
+    currentData,
+    attrName,
+    aliases,
+  });
+  return { ...args, newData: newDataA };
+};
+
+const getNOrderBy = function ({ args, args: { nOrderBy }, attrName, aliases }) {
+  if (!nOrderBy) { return args; }
+
+  const nOrderByA = applyOrderByAliases({ nOrderBy, attrName, aliases });
+  return { ...args, nOrderBy: nOrderByA };
+};
+
+const getTokens = function ({ args, attrName, aliases }) {
+  return directions.reduce(
+    (argsA, direction) =>
+      getToken({ args: argsA, direction, attrName, aliases }),
+    args,
+  );
+};
+
+const directions = ['after', 'before'];
+
+const getToken = function ({ args, direction, attrName, aliases }) {
+  const token = args[direction];
+  if (token === undefined || token === '') { return args; }
+
+  const tokenA = applyTokenAliases({ token, attrName, aliases });
+  return { ...args, [direction]: tokenA };
+};
+
+const modifiers = [
+  getNewData,
+  getNOrderBy,
+  getTokens,
+];
 
 module.exports = {
   applyInputAliases,
