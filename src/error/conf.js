505a3e37885fec864c5807b36fd94247f82b4658
autoserver-founder <founder@autoserver.io>
2017-07-31 16:09:59 +0200

    Finish removing all mutations

--- src/error/conf.js
+++ src/error/conf.js
@@ -2,6 +2,7 @@
 
 // This is a global variable, i.e. affects calling code,
 // but we do want big stack traces.
+// eslint-disable-next-line fp/no-mutation
 Error.stackTraceLimit = 100;
 
 module.exports = {};
