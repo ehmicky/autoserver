b15c601684fd8249446c61a03ed100acad247c93
ehmicky <ehmicky@gmail.com>
2017-07-31 11:39:51 +0200

    Refactor circular references check

--- src/idl/validation/circular_refs.js
+++ src/idl/validation/circular_refs.js
@@ -1,50 +1,48 @@
 'use strict';
 
 const { throwError } = require('../../error');
+const { mapValues } = require('../../utilities');
 
 /**
  * There should be no circular references.
  * They may be introduced by e.g. dereferencing JSON references `$ref`
  * or YAML anchors `*var`
- * The only legal way to introduce circular references is by using
- * `model` property, which is dereferenced later.
  **/
 const validateIdlCircularRefs = function (idl) {
-  return validateCircularRefs({ value: idl });
+  return validateCircularRefs(idl);
 };
 
-const validateCircularRefs = function ({
-  value,
+const validateCircularRefs = function (value, {
   path = 'schema',
   pathSet = new WeakSet(),
-}) {
+} = {}) {
   if (pathSet.has(value)) {
     const message = `Schema cannot contain circular references: ${path}`;
     throwError(message, { reason: 'IDL_VALIDATION' });
   }
 
-  if (typeof value === 'object' && value) {
-    pathSet.add(value);
-  }
+  if (!value) { return value; }
+  if (!Array.isArray(value) && value.constructor !== Object) { return value; }
 
-  walkCircularRefs({ value, path, pathSet });
-
-  pathSet.delete(value);
-
-  return value;
+  return walkCircularRefs(value, { path, pathSet });
 };
 
-// Recursion
-const walkCircularRefs = function ({ value, path, pathSet }) {
-  if (!value) { return; }
-  if (!Array.isArray(value) && value.constructor !== Object) { return; }
+const walkCircularRefs = function (value, { path, pathSet }) {
+  pathSet.add(value);
 
-  for (const [childKey, child] of Object.entries(value)) {
+  const iterator = Array.isArray(value)
+    ? value.map.bind(value)
+    : mapValues.bind(null, value);
+  const valueA = iterator((child, childKey) => {
     const childPath = Array.isArray(value)
       ? `${path}[${childKey}]`
       : `${path}.${childKey}`;
-    validateCircularRefs({ value: child, path: childPath, pathSet });
-  }
+    return validateCircularRefs(child, { path: childPath, pathSet });
+  });
+
+  pathSet.delete(value);
+
+  return valueA;
 };
 
 module.exports = {
