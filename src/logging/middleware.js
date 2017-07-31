2f1ed824afc7fce654363d21623ddb8af48fdd9b
ehmicky <ehmicky@gmail.com>
2017-07-31 17:24:48 +0200

    Fix logging

--- src/logging/middleware.js
+++ src/logging/middleware.js
@@ -8,19 +8,30 @@ const getMiddlewareLogging = () => async function middlewareLogging (
   input,
   ...args
 ) {
+  // When an exception is thrown, unless a state variable is used like
+  // `logRef`, the current `response.log` information will be lost.
+  // eslint-disable-next-line no-param-reassign, fp/no-mutation
+  input.logRef = input.logRef || {};
+
   try {
     const response = await nextFunc(input, ...args) || {};
 
     const log = response.log || input.log;
+    // eslint-disable-next-line no-param-reassign, fp/no-mutation
+    input.logRef.log = log;
     const responseA = { ...response, log };
 
     return responseA;
   } catch (error) {
-    const errorA = normalizeError({ error });
-    const log = errorA.log || input.log;
-    const errorB = { ...errorA, log };
-    rethrowError(errorB);
+    handleError({ input, error });
   }
+};
+
+const handleError = function ({ input, input: { logRef }, error }) {
+  const errorA = normalizeError({ error });
+  const log = errorA.log || logRef.log || input.log;
+  const errorB = { ...errorA, log };
+  rethrowError(errorB);
 };
 
 module.exports = {
