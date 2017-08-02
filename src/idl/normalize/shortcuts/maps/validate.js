4d28c971c11534f6d2c5809d8a5820ca44feaa64
autoserver-founder <founder@autoserver.io>
2017-08-02 14:45:30 +0200

    Build JSON schema map compile-time

---
+++ src/idl/normalize/shortcuts/maps/validate.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const { pickBy, mapValues, omit } = require('../../../../utilities');
+
+const mapProp = prop => prop.validate;
+
+const mapProps = function (properties) {
+  return mappers.reduce((schema, mapper) => mapper(schema), properties);
+};
+
+const propsToJsonSchema = function (properties) {
+  return { type: 'object', properties };
+};
+
+// Fix `required` attribute according to the current command.name
+// JSON schema `require` attribute is a model-level array,
+// not an attribute-level boolean
+const addJsonSchemaRequire = function (schema) {
+  const requiredProps = pickBy(schema.properties, attr => attr.required);
+  const required = Object.keys(requiredProps);
+  // `id` requiredness is checked by other validators, so we skip it here
+  const requiredA = required.filter(attrName => attrName !== 'id');
+  return { ...schema, required: requiredA };
+};
+
+// JSON schema `dependencies` attribute is model-level, not attribute-level
+const addJsonSchemaDeps = function (schema) {
+  const dependencies = mapValues(schema.properties, prop => prop.dependencies);
+  const dependenciesA = pickBy(dependencies, dep => dep !== undefined);
+  return { ...schema, dependencies: dependenciesA };
+};
+
+// Remove syntax that is not JSON schema
+const removeAltSyntax = function (schema) {
+  const propertiesA = mapValues(
+    schema.properties,
+    prop => omit(prop, nonJsonSchema),
+  );
+  return { ...schema, properties: propertiesA };
+};
+
+const nonJsonSchema = [
+  'required',
+  'dependencies',
+];
+
+const mappers = [
+  propsToJsonSchema,
+  addJsonSchemaRequire,
+  addJsonSchemaDeps,
+  removeAltSyntax,
+];
+
+// Retrieves map of models's JSON schema validations
+// E.g. { model: { type: 'object', required: [...], properties: { ... } }
+const validateMap = { mapProp, mapProps };
+
+module.exports = {
+  validateMap,
+};
--- src/middleware/database/validation/schema.js
+++ src/middleware/database/validation/schema.js
@@ -1,18 +1,17 @@
 'use strict';
 
-const { pickBy, mapValues, omit } = require('../../../utilities');
+const { omit } = require('../../../utilities');
 
 // Retrieves JSON schema to validate against
-const getDataValidationSchema = function ({ idl, modelName, command }) {
-  const model = idl.models[modelName];
-  const validate = mapValues(model.properties, prop => prop.validate);
-  const schema = { type: 'object', properties: validate };
-  const schemaA = addJsonSchemaRequire({ schema });
-  const schemaB = addJsonSchemaDeps({ schema: schemaA });
-  const schemaC = removeAltSyntax({ schema: schemaB });
+const getDataValidationSchema = function ({
+  idl: { shortcuts: { validateMap } },
+  modelName,
+  command,
+}) {
+  const schema = validateMap[modelName];
 
-  const schemaD = removeRequire({ schema: schemaC, command });
-  return schemaD;
+  const schemaA = removeRequire({ schema, command });
+  return schemaA;
 };
 
 // Nothing is required for read|delete, except maybe `id` (previously validated)
@@ -27,35 +26,6 @@ const optionalInputCommands = [
   'readMany',
   'deleteOne',
   'deleteMany',
-];
-
-// Fix `required` attribute according to the current command.name
-// JSON schema `require` attribute is a model-level array,
-// not an attribute-level boolean
-const addJsonSchemaRequire = function ({ schema, schema: { properties } }) {
-  const requiredProps = pickBy(properties, attr => attr.required);
-  const required = Object.keys(requiredProps);
-  // `id` requiredness is checked by other validators, so we skip it here
-  const requiredA = required.filter(attrName => attrName !== 'id');
-  return { ...schema, required: requiredA };
-};
-
-// JSON schema `dependencies` attribute is model-level, not attribute-level
-const addJsonSchemaDeps = function ({ schema, schema: { properties } }) {
-  const dependencies = mapValues(properties, prop => prop.dependencies);
-  const dependenciesA = pickBy(dependencies, dep => dep !== undefined);
-  return { ...schema, dependencies: dependenciesA };
-};
-
-// Remove syntax that is not JSON schema
-const removeAltSyntax = function ({ schema, schema: { properties } }) {
-  const propertiesA = mapValues(properties, prop => omit(prop, nonJsonSchema));
-  return { ...schema, properties: propertiesA };
-};
-
-const nonJsonSchema = [
-  'required',
-  'dependencies',
 ];
 
 module.exports = {
