9a3c0daef95463deb5352851f3bfed28056fdfd9
autoserver-founder <founder@autoserver.io>
2017-08-02 15:47:39 +0200

    Fix type validation

---
+++ src/idl/normalize/attribute/type_validation.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Add JSON schema validation based on `type`
+const addTypeValidation = function (attr) {
+  if (!attr.type) { return attr; }
+
+  if (attr.multiple) { return addMultipleValidation(attr); }
+
+  return addSingleValidation(attr);
+};
+
+const addSingleValidation = function (attr) {
+  const { type, validate } = attr;
+  return {
+    ...attr,
+    validate: {
+      ...validate,
+      type,
+    },
+  };
+};
+
+const addMultipleValidation = function (attr) {
+  const { type, validate, validate: { items = {} } } = attr;
+  return {
+    ...attr,
+    validate: {
+      ...validate,
+      type: 'array',
+      items: { ...items, type },
+    },
+  };
+};
+
+module.exports = {
+  addTypeValidation,
+};
