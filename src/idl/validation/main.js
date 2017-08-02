61431ee4ef1a6ae2ff92049f61fd0b9cabfc9346
autoserver-founder <founder@autoserver.io>
2017-08-02 17:19:40 +0200

    Validate JSON schema

--- src/idl/validation/main.js
+++ src/idl/validation/main.js
@@ -6,7 +6,6 @@ const { validateIdlCircularRefs } = require('./circular_refs');
 const { validateData } = require('./data');
 const { validateModelNames } = require('./model_names');
 const { validateIdlSyntax } = require('./syntax');
-const { validateJsonSchema } = require('./json_schema');
 const { validateIdlJsl } = require('./jsl');
 
 // Make sure validators are run in a specific order
@@ -15,7 +14,6 @@ const validators = [
   validateData,
   validateModelNames,
   validateIdlSyntax,
-  validateJsonSchema,
   validateIdlJsl,
 ];
 
