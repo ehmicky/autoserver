864397c675623b1208b35fed705bd974ddfe58b7
autoserver-founder <founder@autoserver.io>
2017-08-04 10:50:51 +0200

    idl.default -> idl.models.default

--- src/idl/pre_normalize/model_default.js
+++ src/idl/pre_normalize/model_default.js
@@ -5,21 +5,20 @@ const { merge } = require('lodash');
 const { omit, mapValues } = require('../../utilities');
 
 // Applies `idl.default` to each model
-const applyModelDefault = function ({ idl }) {
-  if (!idl.default) { return idl; }
-
-  const { models, default: modelsDefault } = idl;
-  const idlA = omit(idl, 'default');
-
-  if (!models || typeof models !== 'object') { return idlA; }
+const applyModelDefault = function ({
+  idl,
+  idl: { models, models: { default: modelsDefault } = {} },
+}) {
+  if (!modelsDefault) { return idl; }
 
   const modelsA = mapValues(models, model => {
     if (!model || typeof model !== 'object') { return model; }
 
     return merge({}, modelsDefault, model);
   });
+  const modelsB = omit(modelsA, 'default');
 
-  return { ...idlA, models: modelsA };
+  return { ...idl, models: modelsB };
 };
 
 module.exports = {
