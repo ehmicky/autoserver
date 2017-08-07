ee9aa3b51c54db0c1ea40de94192bfaf0846a556
autoserver-founder <founder@autoserver.io>
2017-08-07 12:36:55 +0200

    Simpler boolean settings

--- src/middleware/protocol/settings_params/transform.js
+++ src/middleware/protocol/settings_params/transform.js
@@ -4,9 +4,13 @@ const { transtype, mapValues, mapKeys } = require('../../../utilities');
 
 // Normalize params|settings
 const transformValues = function ({ values }) {
-  const valuesA = mapValues(values, value => transtype(value));
-  const valuesB = mapKeys(valuesA, (value, name) => name.toLowerCase());
-  return valuesB;
+  // Transform e.g. ?settings.mysettings to ?settings.mysettings=true
+  const valuesA = mapValues(values, value => (value == null ? true : value));
+  // Transform e.g. 'Number' to actual number
+  const valuesB = mapValues(valuesA, value => transtype(value));
+  // Case-insensitive
+  const valuesC = mapKeys(valuesB, (value, name) => name.toLowerCase());
+  return valuesC;
 };
 
 module.exports = {
