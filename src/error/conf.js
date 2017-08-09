bf787ad4bdd796a57f8f4d449298e459d3193b00
autoserver-founder <founder@autoserver.io>
2017-08-09 13:58:20 +0200

    Move global variables outside of engine

--- src/error/conf.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// This is a global variable, i.e. affects calling code,
-// but we do want big stack traces.
-// eslint-disable-next-line fp/no-mutation
-Error.stackTraceLimit = 100;
-
-module.exports = {};
