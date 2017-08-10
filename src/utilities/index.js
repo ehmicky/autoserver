92147de22bb786a1fcd3147bf9776cbfd631ba9e
autoserver-founder <founder@autoserver.io>
2017-08-10 14:52:10 +0200

    Add JSON loading utility

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -3,6 +3,7 @@
 module.exports = {
   ...require('./functional'),
   ...require('./promise'),
+  ...require('./json'),
   ...require('./yaml'),
   ...require('./template'),
   ...require('./ref_parser'),
