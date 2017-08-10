aacaecda5404227958f4124f0ef83617d9e98d5d
autoserver-founder <founder@autoserver.io>
2017-08-10 12:44:11 +0200

    Add fusionMerge() utility

--- src/utilities/functional/merge.js
+++ src/utilities/functional/merge.js
@@ -2,29 +2,28 @@
 
 const { throwError } = require('../error');
 
-// Deep merge objects and arrays (concatenates for arrays)
-const deepMerge = function (objA, ...objects) {
+const merge = function (type, objA, ...objects) {
   if (objects.length === 0) { return objA; }
 
   if (objects.length === 1) {
-    return simpleMerge({ objA, objects });
+    return simpleMerge({ objA, objects, type });
   }
 
-  return recursiveMerge({ objA, objects });
+  return recursiveMerge({ objA, objects, type });
 };
 
-const simpleMerge = function ({ objA, objects }) {
+const simpleMerge = function ({ objA, objects, type }) {
   const [objB] = objects;
 
   validateInputType({ objA, objB });
   validateInputSameTypes({ objA, objB });
 
-  if (Array.isArray(objA)) {
-    return [...objA, ...objB];
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    return mergeMap[type].array({ arrayA: objA, arrayB: objB });
   }
 
   if (objA.constructor === Object) {
-    return mergeObjects({ objA, objB });
+    return mergeObjects({ objA, objB, type });
   }
 };
 
@@ -48,30 +47,61 @@ const validateInputSameTypes = function ({ objA, objB }) {
   throwError(message);
 };
 
-const mergeObjects = function ({ objA, objB }) {
+const mergeObjects = function ({ objA, objB, type }) {
   return Object.entries(objB).reduce(
     (newObjA, [objBKey, objBVal]) =>
-      mergeObjectProp({ objA: newObjA, objBKey, objBVal }),
+      mergeObjectProp({ objA: newObjA, objBKey, objBVal, type }),
     objA,
   );
 };
 
-const mergeObjectProp = function ({ objA, objBKey, objBVal }) {
+const mergeObjectProp = function ({ objA, objBKey, objBVal, type }) {
   const objAVal = objA[objBKey];
   const shouldDeepMerge = objAVal &&
     objBVal &&
     ((Array.isArray(objAVal) && Array.isArray(objBVal)) ||
     (objAVal.constructor === Object && objBVal.constructor === Object));
-  const newVal = shouldDeepMerge ? deepMerge(objAVal, objBVal) : objBVal;
+  const newVal = shouldDeepMerge
+    ? mergeMap[type].top(objAVal, objBVal)
+    : objBVal;
   return { ...objA, [objBKey]: newVal };
 };
 
-const recursiveMerge = function ({ objA, objects }) {
-  const newObjA = deepMerge(objA, objects[0]);
+const concatArrays = function ({ arrayA, arrayB }) {
+  return [...arrayA, ...arrayB];
+};
+
+const mergeArrays = function ({ arrayA, arrayB }) {
+  const bigArray = arrayA.length >= arrayB.length ? arrayA : arrayB;
+  return bigArray.map((value, index) =>
+    (arrayA[index] === undefined ? arrayB[index] : arrayA[index]),
+  );
+};
+
+const recursiveMerge = function ({ objA, objects, type }) {
+  const newObjA = mergeMap[type].top(objA, objects[0]);
   const newObjects = objects.slice(1);
-  return deepMerge(newObjA, ...newObjects);
+  return mergeMap[type].top(newObjA, ...newObjects);
+};
+
+// Deep merge objects and arrays (concatenates for arrays)
+const deepMerge = merge.bind(null, 'deep');
+
+// Deep merge objects and arrays (merge for arrays)
+const fusionMerge = merge.bind(null, 'fusion');
+
+const mergeMap = {
+  deep: {
+    top: deepMerge,
+    array: concatArrays,
+  },
+  fusion: {
+    top: fusionMerge,
+    array: mergeArrays,
+  },
 };
 
 module.exports = {
   deepMerge,
+  fusionMerge,
 };
