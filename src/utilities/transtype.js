c11c072908d601d16abbdbdfd944adc1a72b9645
autoserver-founder <founder@autoserver.io>
2017-08-10 12:43:52 +0200

    add parsePositiveInteger() utility

--- src/utilities/transtype.js
+++ src/utilities/transtype.js
@@ -4,8 +4,6 @@
 // @param {string} string
 // @param {string|integer|float|boolean} value
 const transtype = function (string) {
-  if (typeof string !== 'string') { return string; }
-
   const number = parseNumber(string);
   if (number !== undefined) { return number; }
 
@@ -16,13 +14,28 @@ const transtype = function (string) {
 };
 
 const parseNumber = function (string) {
-  const parsedNumber = Number(string);
+  if (typeof string !== 'string' || string === '') { return string; }
 
-  const isValidNumber = typeof parsedNumber === 'number' &&
-    Number.isFinite(parsedNumber);
+  const number = Number(string);
+
+  const isValidNumber = typeof number === 'number' && Number.isFinite(number);
   if (!isValidNumber) { return; }
 
-  return parsedNumber;
+  return number;
+};
+
+const parseInteger = function (string) {
+  const number = parseNumber(string);
+  if (number === undefined || Math.trunc(number) !== number) { return; }
+
+  return number;
+};
+
+const parsePositiveInt = function (string) {
+  const integer = parseInteger(string);
+  if (integer === undefined || integer < 0) { return; }
+
+  return integer;
 };
 
 const parseBoolean = function (string) {
@@ -32,4 +45,5 @@ const parseBoolean = function (string) {
 
 module.exports = {
   transtype,
+  parsePositiveInt,
 };
