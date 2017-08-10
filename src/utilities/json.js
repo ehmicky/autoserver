7029af81654a75d22ab8fd7fe398c7455fa6d364
ehmicky <ehmicky@gmail.com>
2017-08-10 14:52:10 +0200

    Add JSON loading utility

---
+++ src/utilities/json.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { pReadFile } = require('./promise');
+
+// Retrieve and parses a JSON file
+// This might throw for many different reasons, e.g. wrong JSON syntax,
+// or cannot access file (does not exist or no permissions)
+const getJson = async function ({ path }) {
+  const content = await pReadFile(path, { encoding: 'utf-8' });
+  return JSON.parse(content);
+};
+
+module.exports = {
+  getJson,
+};
