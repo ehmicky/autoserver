009cb4c08187ec62d9b1f6df97da22aea5157316
ehmicky <ehmicky@gmail.com>
2017-08-11 17:34:28 +0200

    Rename files

--- src/conf/path.js
+++ src/conf/path.js
@@ -7,8 +7,8 @@ const { validateConfFile } = require('./validate');
 // Loads a configuration file which can be:
 //  - environment variable API_ENGINE__NAME 'FILE'
 //  - runtimeOpts({ NAME: 'FILE' })
-//  - .api_engine.NAME.json in current directory, or any parent directory
-//  - same thing for: .api_engine.NAME.yml or .api_engine.NAME.yaml
+//  - api_engine.NAME.json in current directory, or any parent directory
+//  - same thing for: api_engine.NAME.yml or api_engine.NAME.yaml
 // Relative paths are based on current directory.
 const getConfFile = async function ({ path, name }) {
   const pathA = getDirectPath({ path, name });
