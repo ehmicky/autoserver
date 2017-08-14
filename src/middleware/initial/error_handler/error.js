1b8d393c9cb711567dc095c7c79bdd9ac8336131
ehmicky <ehmicky@gmail.com>
2017-08-14 17:48:21 +0200

    Finish refactoring reqInfo

--- src/middleware/initial/error_handler/error.js
+++ src/middleware/initial/error_handler/error.js
@@ -6,13 +6,13 @@ const { getResponse } = require('./response');
 const { reportError } = require('./report');
 
 const handleError = async function ({
-  reqInfo,
+  input,
+  input: { reqInfo },
   error,
   error: { protocolStatus },
-  runtimeOpts,
 }) {
   const standardError = getStandardError({ reqInfo, error });
-  await reportError({ reqInfo, error: standardError, runtimeOpts });
+  await reportError({ input, error: standardError });
 
   // Use protocol-specific way to send back the response to the client
   if (error.sendError) {
--- src/middleware/initial/error_handler/report.js
+++ src/middleware/initial/error_handler/report.js
@@ -3,7 +3,10 @@
 const { STATUS_LEVEL_MAP, emitEvent } = require('../../../events');
 
 // Report any exception thrown
-const reportError = async function ({ reqInfo, error = {}, runtimeOpts }) {
+const reportError = async function ({
+  input: { reqInfo, runtimeOpts },
+  error = {},
+}) {
   // If we haven't reached the events middleware yet, error.status
   // will be undefined, so it will still be caught and reported.
   const level = STATUS_LEVEL_MAP[error.status] || 'error';
