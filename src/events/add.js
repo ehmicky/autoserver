04e259b86b68acd43421776555163eb9c4d238e2
autoserver-founder <founder@autoserver.io>
2017-08-14 17:23:36 +0200

    More refactoring

--- src/events/add.js
+++ src/events/add.js
@@ -1,26 +1,27 @@
 'use strict';
 
-const { deepMerge, pick } = require('../utilities');
+const { merge } = require('lodash');
+
+const { pick } = require('../utilities');
 const { rethrowError } = require('../error');
 
 // Add log information to `obj.log`
 // Returns a new copy, i.e. does not modify original `obj`
-const addLogInfo = function (obj, logInfo) {
-  const { log: { logInfo: logInfoA } } = obj;
-  const logInfoB = deepMerge(logInfoA, logInfo);
-  obj.log.logInfo = logInfoB;
+const addLogInfo = function (obj, newLog) {
+  // We directly mutate `input.log` because it greatly simplifies the code.
+  merge(obj.log, newLog);
   return obj;
 };
 
-// Some logInfo should only be added when an exception is thrown
+// Some log should only be added when an exception is thrown
 // E.g. the current `action` or `model`
 const addLogIfError = function (func, props) {
   return async (nextFunc, input, ...args) => {
     try {
       return await func(nextFunc, input, ...args);
     } catch (error) {
-      const logInfo = pick(input, props);
-      addLogInfo(input, logInfo);
+      const newLog = pick(input, props);
+      addLogInfo(input, newLog);
 
       rethrowError(error);
     }
