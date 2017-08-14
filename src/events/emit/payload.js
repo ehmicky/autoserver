9d765f209b96939aec57eadd09eceb2a52bf9951
autoserver-founder <founder@autoserver.io>
2017-08-14 12:21:29 +0200

    Run event emission in a different macrotask

--- src/events/emit/payload.js
+++ src/events/emit/payload.js
@@ -3,6 +3,31 @@
 const { getServerInfo } = require('../../info');
 
 const { getRequestInfo } = require('./request_info');
+const { getConsoleMessage } = require('./console');
+
+// Retrieves information sent to event, and message printed to console
+const getPayload = function ({
+  log,
+  type,
+  phase,
+  level,
+  message,
+  runtimeOpts,
+  info,
+}) {
+  const eventPayload = getEventPayload({
+    log,
+    type,
+    phase,
+    level,
+    runtimeOpts,
+    info,
+  });
+  const messageA = getConsoleMessage({ message, ...eventPayload });
+  const eventPayloadA = { ...eventPayload, message: messageA };
+
+  return { eventPayload: eventPayloadA, message: messageA };
+};
 
 // Event information sent to handlers
 const getEventPayload = function ({
@@ -46,5 +71,5 @@ const getTimestamp = function ({ requestInfo: { timestamp } = {} }) {
 };
 
 module.exports = {
-  getEventPayload,
+  getPayload,
 };
