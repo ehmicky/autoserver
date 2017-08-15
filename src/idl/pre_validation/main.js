9618c5d2d1a4f15df575847737c400155be8455e
ehmicky <ehmicky@gmail.com>
2017-08-15 18:27:16 +0200

    Remove JSL validation

--- src/idl/pre_validation/main.js
+++ src/idl/pre_validation/main.js
@@ -6,7 +6,6 @@ const { validateIdlCircularRefs } = require('../circular_refs');
 const { validateData } = require('./data');
 const { validateModelNames } = require('./model_names');
 const { validateIdlSyntax } = require('./syntax');
-const { validateIdlJsl } = require('./jsl');
 
 // Make sure validators are run in a specific order
 const validators = [
@@ -14,7 +13,6 @@ const validators = [
   validateData,
   validateModelNames,
   validateIdlSyntax,
-  validateIdlJsl,
 ];
 
 // Validate IDL definition
--- src/jsl/run/compile.js
+++ src/jsl/run/compile.js
@@ -3,19 +3,15 @@
 const { isJsl, isEscapedJsl } = require('../test');
 const { getRawJsl } = require('../tokenize');
 const { memoizeUnlessClient } = require('../memoize');
-const { validateJsl } = require('../validate');
 
 // Transform JSL into a function with the JSL as body
 // Returns as it is not JSL
 // This can throw if JSL's JavaScript is wrong
-const compileJsl = memoizeUnlessClient(({ jsl, paramsKeys, type }) => {
+const compileJsl = memoizeUnlessClient(({ jsl, paramsKeys }) => {
   // If this is not JSL, abort
   if (!isJsl({ jsl })) {
     return getNonJsl({ jsl });
   }
-
-  // Make sure JSL is valid and safe security-wise
-  validateJsl({ jsl, type });
 
   // Removes outer parenthesis
   const rawJsl = getRawJsl({ jsl });
