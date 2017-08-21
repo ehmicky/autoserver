dd701e6b3f09fb08ee0e8f18b9ebdc8a2cd237c9
ehmicky <ehmicky@gmail.com>
2017-08-21 14:09:03 +0200

    Change comments

--- src/options/available/config.js
+++ src/options/available/config.js
@@ -3,8 +3,8 @@
 // Option `config`, shared by all commands
 const config = {
   name: 'config',
-  description: 'Configuration file',
-  validate: { type: 'object' },
+  description: 'Configuration file (YAML or JSON)',
+  validate: { type: 'string' },
 };
 
 module.exports = [
