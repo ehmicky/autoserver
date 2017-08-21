6832e609c80af2f6053c44733f3fd688ba7d6f94
autoserver-founder <founder@autoserver.io>
2017-08-21 13:56:32 +0200

    More refactoring on options

--- src/options/main.js
+++ src/options/main.js
@@ -4,12 +4,14 @@ const { monitoredReduce } = require('../perf');
 
 const { availableCommands } = require('./available');
 const { loadMainConf } = require('./main_conf');
-const { applyDefaultOptions } = require('./default');
+const { applyEnvVars } = require('./env');
 const { loadSubConf } = require('./sub_conf');
+const { applyDefaultOptions } = require('./default');
 const { validateOptions } = require('./validate');
 
 const processors = [
   loadMainConf,
+  applyEnvVars,
   applyDefaultOptions,
   loadSubConf,
   validateOptions,
