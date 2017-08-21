4214e6783f9ec4cac8bc9db7ee26b58318fa1fb4
ehmicky <ehmicky@gmail.com>
2017-08-21 12:02:26 +0200

    Add findValuesAsync() utility

--- src/utilities/functional/find.js
+++ src/utilities/functional/find.js
@@ -1,16 +1,28 @@
 'use strict';
 
 // Like Array.find(), but async
-const findAsync = async function (array, filter, index = 0) {
-  if (array.length === index) { return; }
+const findAsync = async function (array, filter, index) {
+  const [value] = await findBaseAsync(array, filter, index);
+  return value;
+};
+
+// Like Array.find(), but async, and returns value
+const findValueAsync = async function (array, filter, index) {
+  const [, value] = await findBaseAsync(array, filter, index);
+  return value;
+};
+
+const findBaseAsync = async function (array, filter, index = 0) {
+  if (array.length === index) { return []; }
 
   const val = array[index];
   const testVal = await filter(val, index, array);
-  if (testVal) { return val; }
+  if (testVal) { return [val, testVal]; }
 
-  return findAsync(array, filter, index + 1);
+  return findBaseAsync(array, filter, index + 1);
 };
 
 module.exports = {
   findAsync,
+  findValueAsync,
 };
