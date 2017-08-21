1835600f0fb28bdfdae283a0efed96450002c627
autoserver-founder <founder@autoserver.io>
2017-08-21 18:02:46 +0200

    Simulate async while we miss an ORM

--- src/middleware/database/execute/fire.js
+++ src/middleware/database/execute/fire.js
@@ -1,12 +1,18 @@
 'use strict';
 
+const { pSetTimeout } = require('../../../utilities');
+
 const { processResponse } = require('./process_response');
 const { validateResponse } = require('./validate');
 const commands = require('./commands');
 
-const fireCommand = function (commandInput) {
+const fireCommand = async function (commandInput) {
   const { command, opts } = commandInput;
   const response = commands[command.name](commandInput);
+
+  // Simulate asynchronousity
+  // TODO: remove when there is a real ORM
+  await pSetTimeout(0);
 
   const responseA = processResponse({ response, command, opts });
   const responseB = { ...response, ...responseA };
