93991434be4882077b7a8f6e1e57a1e2fdf9ab18
autoserver-founder <founder@autoserver.io>
2017-08-24 16:35:58 +0200

    Add comment

--- src/perf/event.js
+++ src/perf/event.js
@@ -5,6 +5,7 @@ const { emitEvent } = require('../events');
 const { groupMeasures } = require('./group');
 const { stringifyMeasures } = require('./stringify');
 
+// Emit 'perf' event
 const emitPerfEvent = async function ({ phase, measures, ...rest }) {
   const measuresGroups = groupMeasures({ measures });
   const measuresMessage = stringifyMeasures({ phase, measuresGroups });
