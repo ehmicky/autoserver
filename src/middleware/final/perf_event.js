4aad0cedcf57502f3a9e0f40f2b90860460d681b
ehmicky <ehmicky@gmail.com>
2017-08-24 17:12:47 +0200

    Add overall request measure

--- src/middleware/final/perf_event.js
+++ src/middleware/final/perf_event.js
@@ -5,15 +5,20 @@ const { emitPerfEvent } = require('../../perf');
 // Event performance events related to the current request,
 // e.g. how long each middleware lasted.
 const perfEvent = async function (
-  { error, runOpts, mInput },
+  { error, runOpts, mInput, respPerf },
   nextLayer,
   { measures },
 ) {
   // Do not report if an exception was thrown
   if (error) { return; }
 
-  // Total request time, stopped just before the response is sent
-  await emitPerfEvent({ mInput, phase: 'request', measures, runOpts });
+  const measuresA = [...measures, respPerf];
+  await emitPerfEvent({
+    mInput,
+    phase: 'request',
+    measures: measuresA,
+    runOpts,
+  });
 };
 
 module.exports = {
