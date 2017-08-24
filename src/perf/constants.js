14ffd899f57355bbeda07b195910974be741b34e
ehmicky <ehmicky@gmail.com>
2017-08-24 17:26:07 +0200

    Add comment

--- src/perf/constants.js
+++ src/perf/constants.js
@@ -2,6 +2,7 @@
 
 const DEFAULT_CATEGORY = 'default';
 // Whitelist categories to avoid typos
+// Order matters, as console printing uses it for sorting
 const CATEGORIES = [
   'default',
   'main',
