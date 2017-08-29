3d2dcc1604339e465180b23c3547a424108ba294
ehmicky <ehmicky@gmail.com>
2017-08-29 10:33:25 +0200

    Improve startup

--- src/run/steps.js
+++ src/run/steps.js
@@ -1,11 +1,19 @@
 'use strict';
 
+const {
+  startStartupPerf,
+  stopStartupPerf,
+  reportStartupPerf,
+} = require('./perf');
 const { getRunOpts } = require('./options');
 const { processErrorHandler } = require('./process');
 const { bootServers } = require('./boot');
 const { setupGracefulExit } = require('./exit');
+const { emitStartEvent } = require('./start_event');
 
 const startupSteps = [
+  // Start monitoring main startup time
+  startStartupPerf,
   // Retrieve `runOpts`
   getRunOpts,
   // Setup process warnings and errors handler
@@ -14,6 +22,12 @@ const startupSteps = [
   bootServers,
   // Make sure servers are closed on exit
   setupGracefulExit,
+  // Stop monitoring main startup time
+  stopStartupPerf,
+  // Emit 'start' event
+  emitStartEvent,
+  // Report startup performance monitoring
+  reportStartupPerf,
 ];
 
 module.exports = {
