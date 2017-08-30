e96a47c4b156fd615fdc8a94dfa7ee9217558032
autoserver-founder <founder@autoserver.io>
2017-08-30 18:01:27 +0200

    Better error reporting

--- examples/index.js
+++ examples/index.js
@@ -9,7 +9,10 @@ const startServer = async function () {
   try {
     const { options, servers, exit } = await apiEngine.run();
     return { options, servers, exit };
-  } catch (error) {}
+  } catch (error) {
+    // eslint-disable-next-line no-console, no-restricted-globals
+    console.log(error);
+  }
 };
 
 startServer();
