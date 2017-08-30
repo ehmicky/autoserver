2bc47c72c862d89a802636fa5332243d64bbf642
autoserver-founder <founder@autoserver.io>
2017-08-30 14:54:08 +0200

    Fix CLI errors

--- src/cli/main.js
+++ src/cli/main.js
@@ -1,22 +1,33 @@
 'use strict';
 
+const { addErrorHandler } = require('../error');
 const instructions = require('../instructions');
 
 const { parseInput } = require('./input');
 
 // Run a server instruction, from the CLI
-const startCli = function () {
+const startCli = async function () {
   // eslint-disable-next-line fp/no-mutation
   Error.stackTraceLimit = 100;
 
   const measures = [];
   const { instruction, opts } = parseInput({ measures });
 
-  instructions[instruction]({ ...opts, measures })
-    // eslint-disable-next-line no-process-exit, unicorn/no-process-exit
-    .catch(() => process.exit(1));
+  await instructions[instruction]({ ...opts, measures });
 };
 
+// If an error is thrown, print error's description,
+// then exit with exit code 1
+const cliErrorHandler = function ({ description }) {
+  // eslint-disable-next-line no-console, no-restricted-globals
+  console.error(`Error: ${description}`);
+
+  // eslint-disable-next-line no-process-exit, unicorn/no-process-exit
+  process.exit(1);
+};
+
+const eStartCli = addErrorHandler(startCli, cliErrorHandler);
+
 module.exports = {
-  startCli,
+  startCli: eStartCli,
 };
