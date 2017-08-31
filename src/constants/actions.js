135761462a807666f34c1d3c99017618e070c1ed
autoserver-founder <founder@autoserver.io>
2017-08-31 14:43:15 +0200

    Fix whitespaces

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -1,66 +1,18 @@
 'use strict';
 
 const ACTIONS = [
-  {
-    name: 'findOne',
-    type: 'find',
-    multiple: false,
-  },
-  {
-    name: 'findMany',
-    type: 'find',
-    multiple: true,
-  },
-  {
-    name: 'createOne',
-    type: 'create',
-    multiple: false,
-  },
-  {
-    name: 'createMany',
-    type: 'create',
-    multiple: true,
-  },
-  {
-    name: 'replaceOne',
-    type: 'replace',
-    multiple: false,
-  },
-  {
-    name: 'replaceMany',
-    type: 'replace',
-    multiple: true,
-  },
-  {
-    name: 'updateOne',
-    type: 'update',
-    multiple: false,
-  },
-  {
-    name: 'updateMany',
-    type: 'update',
-    multiple: true,
-  },
-  {
-    name: 'upsertOne',
-    type: 'upsert',
-    multiple: false,
-  },
-  {
-    name: 'upsertMany',
-    type: 'upsert',
-    multiple: true,
-  },
-  {
-    name: 'deleteOne',
-    type: 'delete',
-    multiple: false,
-  },
-  {
-    name: 'deleteMany',
-    type: 'delete',
-    multiple: true,
-  },
+  { name: 'findOne', type: 'find', multiple: false },
+  { name: 'findMany', type: 'find', multiple: true },
+  { name: 'createOne', type: 'create', multiple: false },
+  { name: 'createMany', type: 'create', multiple: true },
+  { name: 'replaceOne', type: 'replace', multiple: false },
+  { name: 'replaceMany', type: 'replace', multiple: true },
+  { name: 'updateOne', type: 'update', multiple: false },
+  { name: 'updateMany', type: 'update', multiple: true },
+  { name: 'upsertOne', type: 'upsert', multiple: false },
+  { name: 'upsertMany', type: 'upsert', multiple: true },
+  { name: 'deleteOne', type: 'delete', multiple: false },
+  { name: 'deleteMany', type: 'delete', multiple: true },
 ];
 
 module.exports = {
