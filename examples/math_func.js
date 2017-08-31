f9c6d6629e15d45306c8c2c565a3b31a32debf26
autoserver-founder <founder@autoserver.io>
2017-08-31 13:48:52 +0200

    Fix example

--- examples/math_func.js
+++ examples/math_func.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const MathFunc = function ({ $IP }, { numA, numB, numC, numD }) {
+const MathFunc = function ({ $IP }, helpers, { numA, numB, numC, numD }) {
   const ipNumber = Number($IP.slice(0, 3));
   return ipNumber + (numA * numB) + (numC * numD);
 };
