4569dd7b1d2e03b03de5ab7f972258894fb27584
autoserver-founder <founder@autoserver.io>
2017-08-31 12:09:55 +0200

    Fix GraphQL schema for compute|readonly attributes in args.data

--- src/idl/post_normalize/graphql/schema/types/object/filter.js
+++ src/idl/post_normalize/graphql/schema/types/object/filter.js
@@ -2,39 +2,62 @@
 
 const { omitBy } = require('../../../../../../utilities');
 
-const filterFields = function ({ fields, parentDef, opts }) {
+const filterFields = function ({
+  fields,
+  parentDef: { kind, action },
+  opts: { inputObjectType },
+}) {
   return omitBy(fields, (def, defName) =>
-    filterField({ parentDef, def, defName, opts })
+    filterField({ kind, action, def, defName, inputObjectType })
   );
 };
 
-const filterField = function ({
-  parentDef,
-  def,
-  defName,
-  opts: { inputObjectType },
-}) {
-  // Filter arguments for single actions only include `id`
-  return (
-    defName !== 'id' &&
-    inputObjectType === 'filter' &&
-    !parentDef.action.multiple
-  // Nested data arguments do not include `id`
-  ) || (
-    defName === 'id' &&
-    inputObjectType === 'data' &&
-    parentDef.kind === 'attribute'
-  // Readonly fields cannot be specified as data argument
-  ) || (
-    inputObjectType === 'data' &&
-    def.readonly
-  // `updateOne|updateMany` do not allow data.id
-  ) || (
-    defName === 'id' &&
-    inputObjectType === 'data' &&
-    parentDef.action.type === 'update'
-  );
+const filterField = function (opts) {
+  return filters.some(filter => filter(opts));
 };
+
+// Filter arguments for single actions only include `id`
+const singleIdFilter = function ({ defName, inputObjectType, action }) {
+  return defName !== 'id' &&
+    inputObjectType === 'filter' &&
+    !action.multiple;
+};
+
+// Nested data arguments do not include `id`
+const nestedIdData = function ({ defName, inputObjectType, kind }) {
+  return defName === 'id' &&
+    inputObjectType === 'data' &&
+    kind === 'attribute';
+};
+
+// Computed fields cannot be specified as data argument
+const computeData = function ({ def, inputObjectType }) {
+  return inputObjectType === 'data' &&
+    def.compute !== undefined;
+};
+
+// Readonly fields cannot be specified as data argument,
+// except during model creation
+const readonlyData = function ({ def, inputObjectType, action }) {
+  return inputObjectType === 'data' &&
+    def.readonly &&
+    !['create', 'upsert'].includes(action.type);
+};
+
+// `updateOne|updateMany` do not allow data.id
+const updateIdData = function ({ defName, inputObjectType, action }) {
+  return defName === 'id' &&
+    inputObjectType === 'data' &&
+    action.type === 'update';
+};
+
+const filters = [
+  singleIdFilter,
+  nestedIdData,
+  computeData,
+  readonlyData,
+  updateIdData,
+];
 
 module.exports = {
   filterFields,
