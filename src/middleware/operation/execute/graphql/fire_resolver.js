b9e289adbd7b5cec5a71d51d2b03d0bfedfed47e
autoserver-founder <founder@autoserver.io>
2017-09-04 17:32:58 +0200

    Fix resolver depth

--- src/middleware/operation/execute/graphql/fire_resolver.js
+++ src/middleware/operation/execute/graphql/fire_resolver.js
@@ -2,27 +2,37 @@
 
 const { isEqual } = require('lodash');
 
-const { reduceAsync } = require('../../../../utilities');
+const { assignArray } = require('../../../../utilities');
 
-const fireResolvers = function ({ actions, contextValue, resolver }) {
-  return reduceAsync(
-    actions,
-    async (results, { actionPath, actionName, args, select }) => {
-      const parentPath = actionPath.slice(0, -1);
-      const { data: parent } = results
-        .find(({ actionPath: path }) => isEqual(path, parentPath)) || {};
-      const data = await fireResolver({
-        resolver,
-        name: actionName,
-        parent,
-        args,
-        contextValue,
-      });
-      const result = getResult({ data, actionPath, select });
-      return [...results, ...result];
-    },
-    [],
-  );
+const fireResolvers = async function ({
+  actions,
+  contextValue,
+  resolver,
+  results = [],
+}) {
+  if (actions.length === 0) { return results; }
+
+  const [{ actionPath, actionName, args, select }, ...actionsA] = actions;
+
+  const parentPath = actionPath.slice(0, -1);
+  const { data: parent } = results
+    .find(({ actionPath: path }) => isEqual(path, parentPath)) || {};
+  const data = await fireResolver({
+    resolver,
+    name: actionName,
+    parent,
+    args,
+    contextValue,
+  });
+  const result = getResult({ data, actionPath, select });
+  const actionsB = getActions({ actions: actionsA, data, actionPath });
+  const resultsA = [...results, ...result];
+  return fireResolvers({
+    results: resultsA,
+    actions: actionsB,
+    contextValue,
+    resolver,
+  });
 };
 
 const fireResolver = async function ({
@@ -53,6 +63,31 @@ const getResult = function ({ data, actionPath, select }) {
     actionPath: [...actionPath, index],
     select,
   }));
+};
+
+const getActions = function ({ actions, data, actionPath }) {
+  if (!Array.isArray(data)) { return actions; }
+
+  return actions
+    .map(action => getAction({ action, data, actionPath }))
+    .reduce(assignArray, []);
+};
+
+const getAction = function ({
+  action,
+  action: { actionPath: childActionPath },
+  data,
+  actionPath,
+}) {
+  const startPath = childActionPath.slice(0, actionPath.length);
+  if (!isEqual(startPath, actionPath)) { return [action]; }
+
+  const endPath = childActionPath.slice(actionPath.length);
+
+  return data.map((datum, index) => {
+    const actionPathA = [...startPath, index, ...endPath];
+    return { ...action, actionPath: actionPathA };
+  });
 };
 
 module.exports = {
