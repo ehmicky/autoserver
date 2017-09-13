6729c0a7f6c6339d4cf96e5f1f578ecf1493c5db
autoserver-founder <founder@autoserver.io>
2017-09-13 13:05:09 +0200

    Add comment

--- src/middleware/operation/execute/graphql/assemble.js
+++ src/middleware/operation/execute/graphql/assemble.js
@@ -2,6 +2,7 @@
 
 const { set } = require('../../../../utilities');
 
+// Merge all actions into a single nested object
 const assembleActions = function ({ actions }) {
   return actions.reduce(
     (response, { data, actionPath }) => set(response, actionPath, data),
