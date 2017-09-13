4d0ae546cab20034263d5d92b117b635b4c1f2c8
ehmicky <ehmicky@gmail.com>
2017-09-13 18:08:25 +0200

    More refactoring

--- src/middleware/operation/execute/graphql/operation_summary.js
+++ src/middleware/operation/execute/graphql/operation_summary.js
@@ -6,26 +6,29 @@ const { isEqual } = require('lodash');
 const getOperationSummary = function ({ actions }) {
   const topLevelAction = actions
     .find(({ actionPath }) => actionPath.length === 1);
-  return getSummary({ action: topLevelAction, actions });
+  const { actionConstant: { type: topType } } = topLevelAction;
+  return getSummary({ action: topLevelAction, actions, topType });
 };
 
 // Returns all actions, included nested ones as a nice formatted string,
 // e.g. 'findModel{attrA,attrB,child{attrC}}'
 const getSummary = function ({
-  action: { actionName, actionPath },
+  action: { actionPath },
   actions,
+  topType,
 }) {
-  const childActions = actions.filter(({
-    actionPath: childPath,
-    // In write actions, do not use child actions only used for selections
-    usesTopAction,
-  }) =>
-    isEqual(actionPath, childPath.slice(0, -1)) && usesTopAction
+  const actionName = actionPath[actionPath.length - 1];
+
+  const childActions = actions.filter(
+    ({ actionPath: childPath, actionConstant }) =>
+      isEqual(actionPath, childPath.slice(0, -1)) &&
+      // In write actions, do not use child actions only used for selections
+      actionConstant.type === topType
   );
   if (childActions.length === 0) { return actionName; }
 
   const childActionsStr = childActions
-    .map(childAction => getSummary({ action: childAction, actions }))
+    .map(childAction => getSummary({ action: childAction, actions, topType }))
     .join(',');
   return `${actionName}{${childActionsStr}}`;
 };
