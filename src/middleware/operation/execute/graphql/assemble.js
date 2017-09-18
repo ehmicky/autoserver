968a7e5c9df6544b92145fe7cc80a4e8665c2d97
autoserver-founder <founder@autoserver.io>
2017-09-18 11:28:40 +0200

    Fix create output

--- src/middleware/operation/execute/graphql/assemble.js
+++ src/middleware/operation/execute/graphql/assemble.js
@@ -1,20 +1,55 @@
 'use strict';
 
-const { set } = require('../../../../utilities');
+const { set, assignObject, mapValues } = require('../../../../utilities');
 
 // Merge all actions into a single nested object
 const assembleActions = function ({ actions }) {
-  return actions.reduce(
-    (responseA, { response, respPaths }) => {
-      return respPaths.reduce(
-        (responseB, respPath, index) => {
-          return set(responseB, respPath, response[index]);
-        },
-        responseA,
-      );
-    },
-    {},
+  return actions.reduce(assembleAction, {});
+};
+
+const assembleAction = function (
+  fullResponse,
+  { response, respPaths, select },
+) {
+  return respPaths.reduce(
+    (fullResponseA, { id, path }) => assembleModel({
+      response,
+      fullResponse: fullResponseA,
+      select,
+      id,
+      path,
+    }),
+    fullResponse,
   );
+};
+
+const assembleModel = function ({ response, fullResponse, select, id, path }) {
+  const model = response.find(modelA => modelA.id === id);
+  const modelB = selectFields({ model, select });
+  return set(fullResponse, path, modelB);
+};
+
+// Only output the fields that were picked by the client
+// Also rename fields if the output key is different from the database one,
+// e.g. using GraphQL "aliases"
+const selectFields = function ({ model, select }) {
+  if (model == null) { return model; }
+
+  // Make sure return value is sorted in the same order as `select`
+  const modelA = select
+    .map(({ dbKey, outputKey }) => ({ [outputKey]: model[dbKey] }))
+    .reduce(assignObject, {});
+
+  const modelB = mapValues(modelA, normalizeNull);
+
+  return modelB;
+};
+
+// Transform `undefined` to `null`
+const normalizeNull = function (value) {
+  if (value !== undefined) { return value; }
+
+  return null;
 };
 
 module.exports = {
