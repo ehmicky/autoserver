7b94604a803024a5955244325a4b90b0af119146
autoserver-founder <founder@autoserver.io>
2017-09-21 12:04:07 +0200

    Separate resolver and sequencer

--- src/middleware/operation/execute/graphql/resolver/main.js
+++ src/middleware/operation/execute/graphql/resolver/main.js
@@ -1,64 +1,18 @@
 'use strict';
 
-const { reduceAsync, assignArray } = require('../../../../../utilities');
-
 const { resolveRead } = require('./read');
 const { resolveWrite } = require('./write');
 
-const sequencer = async function ({
-  actionsGroups: { writeActions, readActions },
-  nextLayer,
-  mInput,
-}) {
-  const writeResponses = await getWriteResponses({
-    writeActions,
-    nextLayer,
-    mInput,
-  });
-  const readResponses = await getReadResponses({
-    readActions,
-    writeResponses,
-    nextLayer,
-    mInput,
-  });
-  return [...writeResponses, ...readResponses];
+const resolveActions = function ({ actionsGroupType, ...rest }) {
+  const resolver = resolvers[actionsGroupType];
+  return resolver({ ...rest });
 };
 
-const getWriteResponses = async function ({ writeActions, nextLayer, mInput }) {
-  const responsesPromises = writeActions
-    .map(actionsGroup => resolveWrite({ actionsGroup, nextLayer, mInput }));
-  const responses = await Promise.all(responsesPromises);
-  const responsesA = responses.reduce(assignArray, []);
-  return responsesA;
-};
-
-const getReadResponses = function ({
-  readActions,
-  writeResponses,
-  nextLayer,
-  mInput,
-}) {
-  return reduceAsync(
-    readActions,
-    getReadResponse.bind(null, { nextLayer, mInput }),
-    writeResponses,
-  );
-};
-
-const getReadResponse = async function (
-  { nextLayer, mInput },
-  responses,
-  actionsGroup,
-) {
-  const responsesA = await resolveRead({
-    actionsGroup,
-    nextLayer,
-    mInput,
-    responses,
-  });
-  return [...responses, ...responsesA];
+const resolvers = {
+  read: resolveRead,
+  write: resolveWrite,
 };
 
 module.exports = {
-  sequencer,
+  resolveActions,
 };
