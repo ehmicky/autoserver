722b1e619f264d4c5b58d866abd10cfdc81bec96
ehmicky <ehmicky@gmail.com>
2017-09-25 12:19:51 +0200

    Fix delete action

--- src/middleware/operation/execute/graphql/resolver/write.js
+++ src/middleware/operation/execute/graphql/resolver/write.js
@@ -1,22 +1,28 @@
 'use strict';
 
 const { throwError } = require('../../../../../error');
-const { assignArray, pick } = require('../../../../../utilities');
+const { assignArray } = require('../../../../../utilities');
 const { ACTIONS } = require('../../../../../constants');
 
 const resolveWrite = async function ({
   actions,
-  actions: [{ actionConstant, modelName }],
+  actions: [{
+    actionConstant,
+    actionConstant: { type: actionType },
+    modelName,
+  }],
   nextLayer,
   mInput,
 }) {
-  const argsA = mergeArgs({ actions });
-  if (argsA.newData.length === 0) { return []; }
+  const { [actionType]: handler } = handlers;
+  const argsA = handler.mergeArgs({ actions });
 
-  const argsB = getCurrentData({ actions, args: argsA });
+  const ids = handler.getIds({ args: argsA });
+  if (ids.length === 0) { return []; }
+
+  const argsB = handler.getCurrentData({ actions, args: argsA, ids });
   const actionPath = mergeActionPaths({ actions });
-  const dataPathsA = mergeDataPaths({ actions });
-  const { command } = ACTIONS.find(action => actionConstant === action);
+  const command = findCommand({ actionConstant });
 
   const mInputA = {
     ...mInput,
@@ -26,19 +32,30 @@ const resolveWrite = async function ({
     modelName,
     args: argsB,
   };
-  const { response: { data } } = await nextLayer(mInputA);
+  const { response: { data: responses } } = await nextLayer(mInputA);
 
-  const responses = dataPathsA.map(addResponsesModel.bind(null, data));
-  return responses;
+  const responsesA = getResponses({ actions, responses, ids });
+  return responsesA;
 };
 
-const mergeArgs = function ({ actions }) {
-  const newData = actions
-    .map(({ args: { data: dataA } }) => dataA)
+const mergeDataArgs = function ({ actions }) {
+  const newData = actions.map(action => action.args.data);
+  const newDataA = removeDuplicates(newData);
+
+  return { newData: newDataA };
+};
+
+const mergeFilterArgs = function ({ actions }) {
+  const models = actions.map(({ currentData }) => currentData);
+  const modelsA = removeDuplicates(models);
+  const ids = modelsA.map(({ id }) => id);
+  return { filter: { id: ids } };
+};
+
+const removeDuplicates = function (models) {
+  return models
     .reduce(assignArray, [])
     .filter(isDuplicate);
-
-  return { newData };
 };
 
 // Removes duplicates
@@ -50,21 +67,53 @@ const isDuplicate = function (model, index, allData) {
 
   return allData
     .slice(0, index)
-    .every(({ id }) => model.id !== id);
+    .every(({ id }) => id !== model.id);
 };
 
-const getCurrentData = function ({ actions, args, args: { newData } }) {
+const getFilterIds = function ({ args: { filter: { id } } }) {
+  return id;
+};
+
+const getDataIds = function ({ args: { newData } }) {
+  return newData.map(({ id }) => id);
+};
+
+const getCurrentData = function ({ actions, args, ids }) {
   const currentData = actions
     .map(action => action.currentData)
     .reduce(assignArray, []);
-  const currentDataA = newData
-    .map(datum => findCurrentData({ datum, currentData }));
+  const currentDataA = ids.map(id => findCurrentData({ id, currentData }));
+
   return { ...args, currentData: currentDataA };
 };
 
-const findCurrentData = function ({ datum, currentData }) {
+const findCurrentData = function ({ id, currentData }) {
   return currentData
-    .find(currentDatum => currentDatum && currentDatum.id === datum.id);
+    .find(currentDatum => currentDatum && currentDatum.id === id) || null;
+};
+
+const createHandler = {
+  mergeArgs: mergeDataArgs,
+  getIds: getDataIds,
+  getCurrentData: ({ args }) => args,
+};
+const dataHandler = {
+  mergeArgs: mergeDataArgs,
+  getIds: getDataIds,
+  getCurrentData,
+};
+const filterHandler = {
+  mergeArgs: mergeFilterArgs,
+  getIds: getFilterIds,
+  getCurrentData,
+};
+
+const handlers = {
+  create: createHandler,
+  update: dataHandler,
+  upsert: dataHandler,
+  replace: dataHandler,
+  delete: filterHandler,
 };
 
 const mergeActionPaths = function ({ actions }) {
@@ -76,17 +125,43 @@ const mergeActionPaths = function ({ actions }) {
     .join(', ');
 };
 
-const mergeDataPaths = function ({ actions }) {
+const findCommand = function ({ actionConstant }) {
+  const { command } = ACTIONS.find(action => actionConstant === action);
+  return command;
+};
+
+const getResponses = function ({ actions, responses, ids }) {
+  validateResponse({ ids, responses });
+
   return actions
-    .map(({ args: { data }, dataPaths, select }) =>
-      dataPaths.map((path, index) => ({ path, id: data[index].id, select }))
-    )
+    .map(getModels.bind(null, responses))
     .reduce(assignArray, []);
 };
 
-const addResponsesModel = function (data, { path, id, select }) {
-  const model = data.find(datum => datum.id === id);
+const getModels = function (
+  responses,
+  { args, currentData, dataPaths, select },
+) {
+  const models = args.data || currentData;
+  return models
+    .map(findModel.bind(null, { responses, dataPaths, select }))
+    .filter(({ path }) => path !== undefined);
+};
+
+const findModel = function ({ responses, dataPaths, select }, { id }, index) {
+  const model = responses.find(response => response.id === id);
+  const path = dataPaths[index];
   return { path, model, select };
+};
+
+// Safety check to make sure there is no server-side bugs
+const validateResponse = function ({ ids, responses }) {
+  const sameLength = responses.length === ids.length;
+
+  if (!sameLength) {
+    const message = `'ids' and 'responses' do not have the same length`;
+    throwError(message, { reason: 'UTILITY_ERROR' });
+  }
 };
 
 module.exports = {
