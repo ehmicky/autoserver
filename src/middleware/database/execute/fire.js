5050cce97fcb0a7504ec1f4ef64a85b401066f8a
ehmicky <ehmicky@gmail.com>
2017-09-25 13:25:34 +0200

    Better database simulation

--- src/middleware/database/execute/fire.js
+++ src/middleware/database/execute/fire.js
@@ -6,12 +6,12 @@ const { processResponse } = require('./process_response');
 const commands = require('./commands');
 
 const fireCommand = async function (commandInput) {
-  const { command, opts } = commandInput;
-  const { data, metadata } = commands[command](commandInput);
-
   // Simulate asynchronousity
   // TODO: remove when there is a real ORM
   await pSetTimeout(0);
+
+  const { command, opts } = commandInput;
+  const { data, metadata } = commands[command](commandInput);
 
   const dataA = processResponse({ data, opts });
 
