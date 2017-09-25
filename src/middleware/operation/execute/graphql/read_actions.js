9c411b7140ab4bfec00e0f1db2fc1957d883644c
ehmicky <ehmicky@gmail.com>
2017-09-25 14:23:58 +0200

    Run sibling commands in parallel

--- src/middleware/operation/execute/graphql/read_actions.js
+++ src/middleware/operation/execute/graphql/read_actions.js
@@ -1,42 +1,92 @@
 'use strict';
 
-const { reduceAsync } = require('../../../../utilities');
+const { assignArray } = require('../../../../utilities');
 
 const resolveReadActions = async function ({
-  actions: allActions,
+  actions,
   nextLayer,
   otherLayer,
   mInput,
   responses,
 }) {
-  const readActions = getReadActions({ allActions });
-  const responsesA = await reduceAsync(
-    readActions,
-    getReadResponse.bind(null, { nextLayer, otherLayer, mInput }),
+  if (actions.length === 0) { return responses; }
+
+  const actionsA = getReadActions({ actions });
+
+  const parentActions = getParentActions({ actions: actionsA });
+
+  // Siblings can be run in parallel
+  const responsesPromises = parentActions.map(parentAction => fireReadActions({
+    parentAction,
+    actions,
+    nextLayer,
+    otherLayer,
+    mInput,
     responses,
-  );
-  return responsesA;
+  }));
+  const responsesA = await Promise.all(responsesPromises);
+  const responsesB = responsesA.reduce(assignArray, []);
+
+  return [...responses, ...responsesB];
 };
 
-const getReadActions = function ({ allActions }) {
-  return allActions
-    .filter(({ actionConstant }) => actionConstant.type === 'find')
-    .map(action => [action]);
+const getReadActions = function ({ actions }) {
+  return actions.filter(({ actionConstant }) => actionConstant.type === 'find');
 };
 
-const getReadResponse = async function (
-  { nextLayer, otherLayer, mInput },
-  responses,
+const fireReadActions = async function ({
+  parentAction,
   actions,
-) {
+  nextLayer,
+  otherLayer,
+  mInput,
+  responses,
+}) {
+  // Parent actions must be run first
   const responsesA = await otherLayer({
     actionsGroupType: 'read',
-    actions,
+    actions: [parentAction],
     nextLayer,
     mInput,
     responses,
   });
-  return [...responses, ...responsesA];
+
+  // Child actions must start after their parent ends
+  const childActions = getChildActions({ parentAction, actions });
+  const childResponses = resolveReadActions({
+    actions: childActions,
+    nextLayer,
+    otherLayer,
+    mInput,
+    responses: responsesA,
+  });
+
+  return childResponses;
+};
+
+const getParentActions = function ({ actions }) {
+  return actions.filter(action => isParentAction({ action, actions }));
+};
+
+const isParentAction = function ({ action: childAction, actions }) {
+  return !actions
+    .some(parentAction => isChildAction({ childAction, parentAction }));
+};
+
+const isChildAction = function ({
+  parentAction,
+  parentAction: { actionPath: parentPath },
+  childAction,
+  childAction: { actionPath: childPath },
+}) {
+  return childAction !== parentAction &&
+    childPath.length > parentPath.length &&
+    childPath.join('.').startsWith(parentPath.join('.'));
+};
+
+const getChildActions = function ({ parentAction, actions }) {
+  return actions
+    .filter(childAction => isChildAction({ childAction, parentAction }));
 };
 
 module.exports = {
