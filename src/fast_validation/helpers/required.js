510e7837dcbcfee381c1960cafdb8d2d120f7bd5
autoserver-founder <founder@autoserver.io>
2017-09-25 13:16:41 +0200

    Add unknownTest

--- src/fast_validation/helpers/required.js
+++ src/fast_validation/helpers/required.js
@@ -1,20 +1,40 @@
 'use strict';
 
+const { isEqual } = require('lodash');
+
+const { get } = require('../../utilities');
+
 const requiredTest = name => ({
-  test ({ [name]: value }) {
-    return value != null;
+  test (value) {
+    return get(value, name.split('.')) != null;
   },
   message: 'must be defined',
 });
 
 const forbiddenTest = name => ({
-  test ({ [name]: value }) {
-    return value == null;
+  test (value) {
+    return get(value, name.split('.')) == null;
   },
   message: 'must not be defined',
+});
+
+const unknownTest = (name, attributes) => ({
+  test (value) {
+    const val = get(value, name.split('.'));
+    const keys = Object.keys(val);
+
+    return isEqual(keys, attributes);
+  },
+  message () {
+    const attrNames = attributes
+      .map(attr => `'${attr}'`)
+      .join(', ');
+    return `'${name}' must only contain ${attrNames}`;
+  },
 });
 
 module.exports = {
   requiredTest,
   forbiddenTest,
+  unknownTest,
 };
