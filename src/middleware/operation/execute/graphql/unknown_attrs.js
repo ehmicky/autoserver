4f9019992271386e7a77d53f1d72a5f6eb177601
autoserver-founder <founder@autoserver.io>
2017-09-26 17:41:45 +0200

    Add 'all' attribute selection

--- src/middleware/operation/execute/graphql/unknown_attrs.js
+++ src/middleware/operation/execute/graphql/unknown_attrs.js
@@ -7,13 +7,36 @@ const validateUnknownAttrs = function ({ actions, modelsMap }) {
   return actions;
 };
 
-const validateAction = function ({
+const validateAction = function ({ action, modelsMap }) {
+  validateAllAttr({ action, modelsMap });
+  validateUnknown({ action, modelsMap });
+};
+
+const validateAllAttr = function ({
+  action: { select, actionPath, modelName },
+  modelsMap,
+}) {
+  const hasAllAttr = select.some(({ key }) => key === 'all');
+  if (!hasAllAttr) { return; }
+
+  const attr = select
+    .filter(({ key }) => key !== 'all')
+    .find(({ key }) => modelsMap[modelName][key].target === undefined);
+  if (attr === undefined) { return; }
+
+  const path = actionPath.join('.');
+  const message = `At '${path}': cannot specify both 'all' and '${attr.key}' attributes`;
+  throwError(message, { reason: 'INPUT_VALIDATION' });
+};
+
+const validateUnknown = function ({
   action: { select, actionPath, modelName },
   modelsMap,
 }) {
   const attr = select
+    .filter(({ key }) => key !== 'all')
     .find(({ key }) => modelsMap[modelName][key] === undefined);
-  if (!attr) { return; }
+  if (attr === undefined) { return; }
 
   const path = [...actionPath, attr.key].join('.');
   const message = `Attribute '${path}' is unknown`;
