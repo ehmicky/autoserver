664923fdcd376a6db24b5579a22469cb1ffc1c43
ehmicky <ehmicky@gmail.com>
2017-09-28 15:47:56 +0200

    Refactor response sending

---
+++ src/middleware/final/send_response/validate.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { throwError } = require('../../../error');
+
+const { handlers } = require('./handlers');
+
+const validateResponse = function ({ response: { type, content } }) {
+  if (!type) {
+    throwError('Server sent an response with no content type', {
+      reason: 'SERVER_INPUT_VALIDATION',
+    });
+  }
+
+  if (content === undefined) {
+    throwError('Server sent an empty response', {
+      reason: 'SERVER_INPUT_VALIDATION',
+    });
+  }
+
+  if (!handlers[type]) {
+    const message = 'Server tried to respond with an unsupported content type';
+    throwError(message, { reason: 'SERVER_INPUT_VALIDATION' });
+  }
+};
+
+module.exports = {
+  validateResponse,
+};
