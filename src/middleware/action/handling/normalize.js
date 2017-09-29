5f0345aa2969a7d932f64a70955debb5a0b9b21d
autoserver-founder <founder@autoserver.io>
2017-09-29 17:02:51 +0200

    Add mergeArrayReducer()

--- src/middleware/action/handling/normalize.js
+++ src/middleware/action/handling/normalize.js
@@ -1,15 +1,17 @@
 'use strict';
 
+const { mergeArrayReducer, mapValues, omit } = require('../../../utilities');
+
 const normalizeActions = function ({
   operationDef: { action, args: { select, ...args } },
 }) {
   const actionsA = select
     .split(',')
     .map(parseSelect.bind(null, action))
-    .reduce(groupBySelect, {});
-  const actionsB = Object.values(actionsA)
-    .map(normalizeAction.bind(null, args));
-  return actionsB;
+    .reduce(mergeArrayReducer('actionPath'), {});
+  const actionsB = mapValues(actionsA, actions => normalize({ args, actions }));
+  const actionsC = Object.values(actionsB);
+  return actionsC;
 };
 
 const parseSelect = function (action, select) {
@@ -20,16 +22,12 @@ const parseSelect = function (action, select) {
 // Turns 'aaa.bbb.ccc=alias' into ['aaa.bbb', 'ccc', 'alias']
 const selectRegExp = /^([^=]*)\.([^.=]+)=?(.*)?$/;
 
-const groupBySelect = function (actions, { actionPath, ...rest }) {
-  const { [actionPath]: { select = [] } = {} } = actions;
-  const selectA = [...select, rest];
-  return { ...actions, [actionPath]: { actionPath, select: selectA } };
-};
-
-const normalizeAction = function (args, { actionPath, select }) {
+const normalize = function ({ args, actions }) {
+  const [{ actionPath }] = actions;
+  const selectA = actions.map(action => omit(action, 'actionPath'));
   const actionPathA = actionPath.split('.');
   const argsA = actionPathA.length === 1 ? args : {};
-  return { actionPath: actionPathA, args: argsA, select };
+  return { actionPath: actionPathA, args: argsA, select: selectA };
 };
 
 module.exports = {
