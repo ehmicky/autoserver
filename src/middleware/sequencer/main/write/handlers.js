208053c602f0e41aa93f5dd73b218645de936797
autoserver-founder <founder@autoserver.io>
2017-10-02 19:29:38 +0200

    Refactoring

---
+++ src/middleware/sequencer/main/write/handlers.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const { assignArray } = require('../../../../utilities');
+
+const { removeDuplicates } = require('./duplicate');
+
+// Merge all `args.data` into `newData`, for `replace|patch|create` commands
+const mergeDataArgs = function ({ actions }) {
+  const newData = actions.map(action => action.args.data);
+
+  const newDataA = removeDuplicates(newData);
+
+  return { newData: newDataA };
+};
+
+// Merge all `currentData` into `filter.id`, for `delete` command
+const mergeFilterArgs = function ({ actions }) {
+  const models = actions.map(({ currentData }) => currentData);
+
+  const modelsA = removeDuplicates(models);
+  const ids = modelsA.map(({ id }) => id);
+
+  return { filter: { id: ids } };
+};
+
+const getFilterIds = function ({ args: { filter: { id } } }) {
+  return id;
+};
+
+const getDataIds = function ({ args: { newData } }) {
+  return newData.map(({ id }) => id);
+};
+
+// Retrieve `currentData`, so it is passed to command middleware
+// Note that `create` command does not have any `currentData`.
+const getCurrentData = function ({ actions, args, ids }) {
+  const currentData = actions
+    .map(action => action.currentData)
+    .reduce(assignArray, []);
+  // Keep the same order as `newData` or `args.filter.id`
+  const currentDataA = ids.map(id => findCurrentData({ id, currentData }));
+
+  return { ...args, currentData: currentDataA };
+};
+
+const findCurrentData = function ({ id, currentData }) {
+  return currentData
+    .find(currentDatum => currentDatum && currentDatum.id === id);
+};
+
+const createHandler = {
+  mergeArgs: mergeDataArgs,
+  getIds: getDataIds,
+  getCurrentData: ({ args }) => args,
+};
+const dataHandler = {
+  mergeArgs: mergeDataArgs,
+  getIds: getDataIds,
+  getCurrentData,
+};
+const filterHandler = {
+  mergeArgs: mergeFilterArgs,
+  getIds: getFilterIds,
+  getCurrentData,
+};
+
+// Each command has specific logic
+const handlers = {
+  create: createHandler,
+  patch: dataHandler,
+  replace: dataHandler,
+  delete: filterHandler,
+};
+
+module.exports = {
+  handlers,
+};
---
+++ src/middleware/sequencer/main/write/results.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const { assignArray } = require('../../../../utilities');
+const { throwError } = require('../../../../error');
+
+// Transform results to normalized format
+const getResults = function ({ actions, results, ids, modelName }) {
+  validateResult({ ids, results });
+
+  return actions
+    .map(action => getModels({ action, results, modelName }))
+    .reduce(assignArray, []);
+};
+
+const getModels = function ({
+  results,
+  modelName,
+  action: { args, currentData, dataPaths, select },
+}) {
+  // `results` should be in same order as `args.data` or
+  // (for `delete`) as `currentData`, and reuse their `dataPaths`
+  const models = args.data || currentData;
+  return models
+    .map(findModel.bind(null, { results, dataPaths, select, modelName }))
+    .filter(({ path }) => path !== undefined);
+};
+
+const findModel = function (
+  { results, dataPaths, select, modelName },
+  { id },
+  index,
+) {
+  const model = results.find(result => result.id === id);
+  const path = dataPaths[index];
+  return { path, model, modelName, select };
+};
+
+// Safety check to make sure there is no server-side bugs
+const validateResult = function ({ ids, results }) {
+  const sameLength = results.length === ids.length;
+
+  if (!sameLength) {
+    const message = `'ids' and 'results' do not have the same length`;
+    throwError(message, { reason: 'UTILITY_ERROR' });
+  }
+};
+
+module.exports = {
+  getResults,
+};
