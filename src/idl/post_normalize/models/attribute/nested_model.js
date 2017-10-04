ce0f16123e475240b40b85cf57c93148f3c68596
autoserver-founder <founder@autoserver.io>
2017-10-04 15:23:38 +0200

    Simplify nested model merging

--- src/idl/post_normalize/models/attribute/nested_model.js
+++ src/idl/post_normalize/models/attribute/nested_model.js
@@ -1,51 +1,17 @@
 'use strict';
 
-const { pick, omit, deepMerge } = require('../../../../utilities');
-
-// Shallow copy nested models from the `model.id` they refer to
+// Copy some attributes to nested models from the `model.id` they refer to
 const mergeNestedModel = function (attr, { idl: { models } }) {
   if (attr.target === undefined) { return attr; }
 
   const [, model] = Object.entries(models)
     .find(([name, mod]) => mod.model === attr.target || name === attr.target);
-  const modelId = getTargetModelId({ model });
 
-  // Target model has less priority than nested attribute
-  const modelIdA = deepMerge(modelId, attr);
+  const { type } = model.attributes.id;
+  const description = attr.description || model.description;
 
-  return modelIdA;
+  return { ...attr, type, description };
 };
-
-const getTargetModelId = function ({ model }) {
-  const modelId = model.attributes.id;
-
-  // For the metadata, use target model, not target model.id
-  const modelIdA = omit(modelId, metadataProps);
-  const modelMetadata = pick(model, metadataProps);
-  const modelIdB = { ...modelIdA, ...modelMetadata };
-
-  // Never using target model for those properties
-  const modelIdC = omit(modelIdB, nestedProps);
-  const validate = omit(modelIdC.validate, nestedValidateProps);
-  const modelIdD = { ...modelIdC, validate };
-
-  return modelIdD;
-};
-
-const metadataProps = [
-  'description',
-  'deprecation_reason',
-  'examples',
-];
-
-const nestedProps = [
-  'default',
-];
-
-const nestedValidateProps = [
-  'required',
-  'dependencies',
-];
 
 module.exports = {
   mergeNestedModel,
