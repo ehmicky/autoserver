0e598d07ec27117cfe46628968b185a287bcf275
ehmicky <ehmicky@gmail.com>
2017-10-04 17:33:20 +0200

    Fix args.data nesting for GraphQL schema

--- src/operations/handlers/graphql/compile/schema/types/object/nested_models.js
+++ src/operations/handlers/graphql/compile/schema/types/object/nested_models.js
@@ -6,17 +6,26 @@ const { omit } = require('../../../../../../../utilities');
 const getNestedModel = function (def, { inputObjectType, topDef }) {
   const { target } = def;
 
-  // Only for nested models, that are not data|filter arguments
-  const isNested = target !== undefined && inputObjectType === 'type';
+  // Only for nested models, that are not filter arguments
+  const isNested = target !== undefined && inputObjectType !== 'filter';
   if (!isNested) { return def; }
 
   const topLevelModel = Object.values(topDef.attributes)
-    .find(({ model }) => model === target);
+    .find(topLevelModelMatches.bind(null, def));
   // Command description is only used for Query|Mutation children,
   // not for recursive attributes, which use the normal `attr.description`
   const topLevelModelA = omit(topLevelModel, 'commandDescription');
 
   return topLevelModelA;
+};
+
+const topLevelModelMatches = function (
+  { target, isArray, command: { type: commandType } },
+  { model, command },
+) {
+  return model === target &&
+    command.multiple === isArray &&
+    command.type === commandType;
 };
 
 module.exports = {
