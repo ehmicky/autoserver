120d3f60e82fdcf7ed76a65514b237a026e77000
ehmicky <ehmicky@gmail.com>
2017-10-04 12:36:22 +0200

    Refactor GraphQL schema defaults

--- src/operations/handlers/graphql/compile/schema/types/object/final_fields/default.js
+++ src/operations/handlers/graphql/compile/schema/types/object/final_fields/default.js
@@ -2,21 +2,33 @@
 
 const { isInlineFunc } = require('../../../../../../../../idl_func');
 
-const getDefaultValue = function (
-  { command, default: defaultValue },
-  { inputObjectType },
-) {
-  // 'patch' does not require anything, nor assign defaults
-  const hasDefaultValue = inputObjectType === 'data' &&
-    command.type !== 'patch' &&
-    defaultValue;
-  if (!hasDefaultValue) { return; }
+const getDefaultValue = function (def, opts) {
+  const shouldSetDefault = defaultValueTests.every(func => func(def, opts));
+  if (!shouldSetDefault) { return; }
 
-  // IDL function are skipped
-  const isDynamic = typeof defaultValue === 'function' ||
-    isInlineFunc({ inlineFunc: defaultValue });
-  return isDynamic ? undefined : defaultValue;
+  return def.default;
 };
+
+const hasDefaultValue = function (def) {
+  return def.default != null;
+};
+
+// 'patch' does not require anything, nor assign defaults
+const isNotPatchData = function ({ command }, { inputObjectType }) {
+  return !(inputObjectType === 'data' && command.type === 'patch');
+};
+
+// IDL function are skipped
+const isStatic = function (def) {
+  return typeof def.default !== 'function' &&
+    !isInlineFunc({ inlineFunc: def.default });
+};
+
+const defaultValueTests = [
+  hasDefaultValue,
+  isNotPatchData,
+  isStatic,
+];
 
 module.exports = {
   getDefaultValue,
