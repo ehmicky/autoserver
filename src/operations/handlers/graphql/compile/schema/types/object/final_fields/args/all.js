99d79297d83965c5aa5e3d7a5b930d820174d4ca
ehmicky <ehmicky@gmail.com>
2017-10-04 14:46:56 +0200

    Add args.params to GraphQL schema

--- src/operations/handlers/graphql/compile/schema/types/object/final_fields/args/all.js
+++ src/operations/handlers/graphql/compile/schema/types/object/final_fields/args/all.js
@@ -7,6 +7,7 @@ const { getPaginationArgument } = require('./pagination');
 const { getSilentArgument } = require('./silent');
 const { getDryrunArgument } = require('./dryrun');
 const { getCascadeArgument } = require('./cascade');
+const { getParamsArgument } = require('./params');
 
 // Retrieves all resolver arguments, before resolve function is fired
 const getArgs = function (def, opts) {
@@ -19,11 +20,12 @@ const getArgs = function (def, opts) {
   return {
     ...getDataArgument(def, optsA),
     ...getFilterArgument(def, optsA),
+    ...getCascadeArgument(def, optsA),
     ...getOrderArgument(def, optsA),
     ...getPaginationArgument(def, optsA),
-    ...getSilentArgument(def, optsA),
     ...getDryrunArgument(def, optsA),
-    ...getCascadeArgument(def, optsA),
+    ...getSilentArgument(def, optsA),
+    ...getParamsArgument(def, optsA),
   };
 };
 
