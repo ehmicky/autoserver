fe061071fbef20b9d6bc364a8b91abb778313191
ehmicky <ehmicky@gmail.com>
2017-10-05 12:45:41 +0200

    Refactor IDL alias code

--- src/idl/post_normalize/models/model/alias.js
+++ src/idl/post_normalize/models/model/alias.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const { toSentence } = require('underscore.string');
-
-const { mapValues, omit, assignObject } = require('../../../../utilities');
+const { omit, assignObject } = require('../../../../utilities');
 const { throwError } = require('../../../../error');
 
 // Transforms can copy each `alias` as a real attribute,
@@ -10,15 +8,15 @@ const { throwError } = require('../../../../error');
 const normalizeAliases = function (model) {
   if (!model.attributes) { return model; }
 
-  const attrsA = Object.entries(model.attributes)
-    .reduce((attrs, [attrName, attr]) => {
-      const aliases = createAliases({ model, attrs, attr, attrName });
-      return { ...attrs, ...aliases, [attrName]: attr };
-    }, {});
-
-  const attributes = mapValues(attrsA, attr => addAliasDescription({ attr }));
+  const attributes = Object.entries(model.attributes)
+    .reduce(normalizeAlias.bind(null, model), {});
 
   return { ...model, attributes };
+};
+
+const normalizeAlias = function (model, attrs, [attrName, attr]) {
+  const aliases = createAliases({ model, attrs, attr, attrName });
+  return { ...attrs, ...aliases, [attrName]: attr };
 };
 
 const createAliases = function ({ model, attrs, attr, attrName }) {
@@ -26,15 +24,17 @@ const createAliases = function ({ model, attrs, attr, attrName }) {
   const aliases = Array.isArray(attr.alias) ? attr.alias : [attr.alias];
 
   return aliases
-    .map(alias => {
-      checkAliasDuplicates({ model, attrs, attrName, alias });
-
-      const aliasAttr = omit(attr, 'alias');
-      const attrA = { ...aliasAttr, aliasOf: attrName };
-
-      return { [alias]: attrA };
-    })
+    .map(alias => createAlias({ model, attrs, attr, attrName, alias }))
     .reduce(assignObject, {});
+};
+
+const createAlias = function ({ model, attrs, attr, attrName, alias }) {
+  checkAliasDuplicates({ model, attrs, attrName, alias });
+
+  const aliasAttr = omit(attr, 'alias');
+  const attrA = { ...aliasAttr, aliasOf: attrName };
+
+  return { [alias]: attrA };
 };
 
 const checkAliasDuplicates = function ({ model, attrs, attrName, alias }) {
@@ -48,37 +48,6 @@ const checkAliasDuplicates = function ({ model, attrs, attrName, alias }) {
     const message = `Attributes '${otherAttrName}' and '${attrName}' cannot have the same alias '${alias}'`;
     throwError(message, { reason: 'IDL_VALIDATION' });
   }
-};
-
-// Add information about aliases in `description`
-const addAliasDescription = function ({ attr }) {
-  if (!attr.description) { return attr; }
-
-  const aliasesDescription = getAliasesDescription({ attr });
-  const aliasOfDescription = getAliasOfDescription({ attr });
-
-  const description = [
-    aliasesDescription,
-    aliasOfDescription,
-    attr.description,
-  ].filter(val => val)
-    .join('\n');
-
-  return { ...attr, description };
-};
-
-const getAliasesDescription = function ({ attr }) {
-  if (!attr.alias) { return ''; }
-
-  const aliases = Array.isArray(attr.alias) ? attr.alias : [attr.alias];
-  const aliasNames = toSentence(aliases.map(alias => `'${alias}'`));
-  return `Aliases: ${aliasNames}.`;
-};
-
-const getAliasOfDescription = function ({ attr }) {
-  if (!attr.aliasOf) { return ''; }
-
-  return `Alias of: '${attr.aliasOf}'`;
 };
 
 module.exports = {
