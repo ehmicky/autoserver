e57159578cd2984d51f5b2ab6f7283533cd4834a
ehmicky <ehmicky@gmail.com>
2017-10-05 12:34:20 +0200

    Add examples to descriptions

--- src/idl/post_normalize/models/attribute/descriptions.js
+++ src/idl/post_normalize/models/attribute/descriptions.js
@@ -8,20 +8,31 @@ const addDescriptions = function (attr) {
   const { description } = attr;
   const descriptionA = description ? [description] : [];
 
-  const descriptionsA = descriptions.map(({ message }) => message);
+  const descriptionsA = descriptions.map(({ message }) => message(attr));
   const descriptionB = [...descriptionA, ...descriptionsA].join('\n');
 
   return { ...attr, description: descriptionB };
 };
 
+const getExamples = function ({ examples }) {
+  const examplesA = examples
+    .map(example => `  - ${example}`)
+    .join('\n');
+  return `Examples:\n${examplesA}`;
+};
+
 const allDescriptions = [
   {
     test: ({ readonly }) => readonly,
-    message: 'This attribute is readonly, i.e. can only be set when the model is created.',
+    message: () => 'This attribute is readonly, i.e. can only be set when the model is created.',
   },
   {
     test: ({ value }) => value !== undefined,
-    message: 'This attribute is set by the server, not the client.',
+    message: () => 'This attribute is set by the server, not the client.',
+  },
+  {
+    test: ({ examples }) => examples !== undefined,
+    message: getExamples,
   },
 ];
 
