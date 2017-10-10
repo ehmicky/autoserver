f591e21bff5e3aff6fe7f1d935d3004c6f24b083
ehmicky <ehmicky@gmail.com>
2017-10-10 15:56:40 +0200

    Simplify delete command for memory adapter

--- src/middleware/database/execute/memory/commands/delete.js
+++ src/middleware/database/execute/memory/commands/delete.js
@@ -1,15 +1,10 @@
 'use strict';
 
-const { sortArray } = require('../../../../../utilities');
-
 const deleteMany = function ({ collection, deletedIds }) {
-  const indexes = Object.entries(collection)
+  const data = Object.entries(collection)
     .filter(([, model]) => deletedIds.includes(model.id))
-    .map(([index]) => index);
-  const sortedIndexes = sortArray(indexes);
-  const data = sortedIndexes
     // eslint-disable-next-line fp/no-mutating-methods
-    .map((index, count) => collection.splice(index - count, 1)[0]);
+    .map(([index], count) => collection.splice(index - count, 1)[0]);
   return { data };
 };
 
