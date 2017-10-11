05f25009793521788cc9dafb34fa8f7c2c1b8274
autoserver-founder <founder@autoserver.io>
2017-10-11 19:00:59 +0200

    Validate that memory database is not used in production

--- src/database/adapters/memory/connect.js
+++ src/database/adapters/memory/connect.js
@@ -1,10 +1,21 @@
 'use strict';
 
+const { throwError } = require('../../../error');
+
 const database = require('./data.json');
 
 // Starts connection
-const connect = function () {
+const connect = function ({ runOpts }) {
+  validateEnv({ runOpts });
+
   return database;
+};
+
+const validateEnv = function ({ runOpts }) {
+  if (runOpts.env === 'dev') { return; }
+
+  const message = 'Memory database must not be used in production, i.e. option \'env\' must be equal to \'dev\'';
+  throwError(message, { reason: 'CONF_VALIDATION' });
 };
 
 module.exports = {
