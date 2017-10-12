7e844e42001b9b31500eca339a25512588f93452
autoserver-founder <founder@autoserver.io>
2017-10-12 14:05:05 +0200

    Add memory database adapter persistence

--- src/database/adapters/memory/disconnect.js
+++ src/database/adapters/memory/disconnect.js
@@ -1,8 +1,23 @@
 'use strict';
 
+const { generic } = require('../../../formats');
+
 // Stops connection
-const disconnect = function () {
+// Persist back to file, unless database adapter option `save` is false
+const disconnect = async function ({
+  options: {
+    save,
+    data: { path = defaultPath },
+  },
+  connection,
+}) {
+  if (save === false) { return; }
+
+  // Reuse the same file that was used during loading
+  await generic.save({ path, content: connection });
 };
+
+const defaultPath = './api_engine.run.db.memory.json';
 
 module.exports = {
   disconnect,
--- src/database/adapters/memory/opts.js
+++ src/database/adapters/memory/opts.js
@@ -2,19 +2,29 @@
 
 const { generic } = require('../../../formats');
 
-const opts = [{
-  name: 'data',
-  description: `File containing the data (${generic.description})`,
-  subConfFiles: [{
-    filename: 'db.memory',
-    extNames: generic.extNames,
-    loader: 'generic',
-    keepPath: true,
-  }],
-  validate: {
-    type: 'object',
+const opts = [
+  {
+    name: 'data',
+    description: `File containing the data (${generic.description})`,
+    subConfFiles: [{
+      filename: 'db.memory',
+      extNames: generic.extNames,
+      loader: 'generic',
+      keepPath: true,
+    }],
+    validate: {
+      type: 'object',
+    },
   },
-}];
+
+  {
+    name: 'save',
+    description: 'Saves the data when the server shuts down',
+    validate: {
+      type: 'boolean',
+    },
+  },
+];
 
 module.exports = {
   opts,
