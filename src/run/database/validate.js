eae8faaf32d8b953f57485da54616c1866e5f2a8
autoserver-founder <founder@autoserver.io>
2017-10-13 11:15:42 +0200

    Change how database target models

--- src/run/database/validate.js
+++ src/run/database/validate.js
@@ -1,110 +1,68 @@
 'use strict';
 
-const { assignArray, getWordsList } = require('../../utilities');
+const { getWordsList } = require('../../utilities');
 const { throwError } = require('../../error');
-const { KINDS } = require('../../constants');
-
-const { kindRegExp } = require('./kind_regexp');
 
 // Validate `runOpts.db` options
 const validateDbOpts = function ({ adapters, schema }) {
-  // Merge all `runOpts.db.models`
-  const allModels = adapters
-    .map(({ models }) => models)
-    .reduce(assignArray, []);
+  validateRequiredModels({ adapters });
 
-  adapters.forEach(({ models, kinds, type }) => validateModels({
-    models,
-    allModels,
-    schema,
-    kinds,
-    type,
-  }));
+  const validModels = [...Object.keys(schema.models), '...'];
+  adapters.forEach(({ models, type }) =>
+    validateModels({ models, type, validModels }));
+};
+
+// Validate whether `runOpts.db.DATABASE.models` is undefined
+const validateRequiredModels = function ({ adapters }) {
+  if (adapters.length === 0) {
+    const message = 'Invalid option \'db\': no database is defined';
+    throwError(message, { reason: 'CONF_VALIDATION' });
+  }
+
+  if (adapters.length === 1) {
+    return validateNoModels({ adapters });
+  }
+
+  validateHasModels({ adapters });
+};
+
+// `runOpts.db.DATABASE.models` must be undefined if there is only one database
+const validateNoModels = function ({ adapters }) {
+  const [{ models, type }] = adapters;
+
+  if (models === undefined) { return; }
+
+  const message = `Invalid option 'db.${type}.models': it must not be defined when there is only one database`;
+  throwError(message, { reason: 'CONF_VALIDATION' });
+};
+
+// `runOpts.db.DATABASE.models` must be defined if there are several databases
+const validateHasModels = function ({ adapters }) {
+  const adaptersA = adapters.filter(({ models }) => models === undefined);
+
+  if (adaptersA.length === 0) { return; }
+
+  const types = adaptersA.map(({ type }) => `db.${type}.models`);
+  const typesA = getWordsList(types, { op: 'and', quotes: true });
+  const message = `Invalid options ${typesA}: it must be defined`;
+  throwError(message, { reason: 'CONF_VALIDATION' });
 };
 
 // Validate `runOpts.db.models` option
-const validateModels = function ({ models, ...rest }) {
-  models.forEach(model => validateModel({ model, ...rest }));
+const validateModels = function ({ models, type, validModels }) {
+  if (models === undefined) { return; }
+
+  models.forEach(model => validateModelName({ model, type, validModels }));
 };
 
-const validateModel = function ({ model, allModels, schema, kinds, type }) {
-  validateDuplicateModel({ model, allModels, type });
+const validateModelName = function ({ model, validModels, type }) {
+  if (validModels.includes(model)) { return; }
 
-  const [, kind] = kindRegExp.exec(model) || [];
-
-  if (kind) {
-    return validateKind({ kind, kinds, type });
-  }
-
-  validateModelName({ modelName: model, schema, kinds, type });
-};
-
-const validateDuplicateModel = function ({ model, allModels, type }) {
-  const duplicates = allModels.filter(allModel => allModel === model);
-
-  if (duplicates.length !== 1) {
-    const message = `Invalid option 'db.${type}.models'. It contains '${model}' but this value is already used by another database option. Each model is only using a single database`;
-    throwError(message, { reason: 'CONF_VALIDATION' });
-  }
-};
-
-const validateKind = function ({ kind, kinds, type }) {
-  const isValidKind = KINDS.includes(kind);
-
-  if (!isValidKind) {
-    const KINDSA = KINDS.map(KIND => `kind:${KIND}`);
-    const KINDSB = getWordsList(KINDSA, { quotes: true });
-    const message = `Invalid option 'db.${type}.models'. It contains 'kind:${kind}' but this kind does not exist. Possible values: ${KINDSB}`;
-    throwError(message, { reason: 'CONF_VALIDATION' });
-  }
-
-  const isValidAdapterKind = kinds.includes(kind);
-
-  if (!isValidAdapterKind) {
-    const message = `Invalid option 'db.${type}.models'. It contains 'kind:${kind}' but this kind is not supported by the '${type}' database.`;
-    throwError(message, { reason: 'CONF_VALIDATION' });
-  }
-};
-
-const validateModelName = function ({
-  modelName,
-  schema: { models: schemaModels },
-  kinds,
-  type,
-}) {
-  const schemaModel = Object.entries(schemaModels)
-    .find(([name]) => modelName === name);
-
-  if (schemaModel === undefined) {
-    const validModelsA = Object.keys(schemaModels);
-    const validModelsB = getWordsList(validModelsA, { quotes: true });
-    const message = `Invalid option 'db.${type}.models'. It contains '${modelName}' but this model does not exist. Possible values: ${validModelsB}`;
-    throwError(message, { reason: 'CONF_VALIDATION' });
-  }
-
-  const missingKind = schemaModel[1].kind
-    .find(kindA => !kinds.includes(kindA));
-
-  if (missingKind !== undefined) {
-    const message = `Invalid option 'db.${type}.models'. It contains '${modelName}' but this model uses the '${missingKind}' kind, which is not supported by the '${type}' database.`;
-    throwError(message, { reason: 'CONF_VALIDATION' });
-  }
-};
-
-// If a `runOpts.db` is unused, it might indicate a configuration issue
-const validateUnusedAdapters = function ({ adapters, adaptersMap }) {
-  const usedAdapters = Object.values(adaptersMap);
-  const specifiedAdapters = adapters.map(({ type }) => type);
-
-  const unusedAdapter = specifiedAdapters
-    .find(adapter => !usedAdapters.includes(adapter));
-  if (unusedAdapter === undefined) { return; }
-
-  const message = `Invalid option 'db.${unusedAdapter}': this database is not used by any model`;
+  const validModelsA = getWordsList(validModels, { quotes: true });
+  const message = `Invalid option 'db.${type}.models'. It contains '${model}' but this model does not exist. Possible values: ${validModelsA}`;
   throwError(message, { reason: 'CONF_VALIDATION' });
 };
 
 module.exports = {
   validateDbOpts,
-  validateUnusedAdapters,
 };
