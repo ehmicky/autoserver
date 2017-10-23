31de86cdb85d4ea090305f76dbe53057e58cdf7c
ehmicky <ehmicky@gmail.com>
2017-10-23 15:06:16 +0200

    Refactoring

--- src/operations/handlers/graphql/index.js
+++ src/operations/handlers/graphql/index.js
@@ -1,12 +1,5 @@
 'use strict';
 
 module.exports = {
-  routes: require('./routes'),
-  name: require('./name'),
-  methods: require('./methods'),
-  ...require('./handler'),
-  ...require('./response'),
-  ...require('./compile'),
-  ...require('./startup'),
-  payload: require('./payload'),
+  ...require('./operation'),
 };
--- src/operations/handlers/graphql_print/index.js
+++ src/operations/handlers/graphql_print/index.js
@@ -1,8 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: require('./name'),
-  methods: require('./methods'),
-  routes: require('./routes'),
-  ...require('./handler'),
+  ...require('./operation'),
 };
