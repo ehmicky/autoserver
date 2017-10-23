5ee73d97a0c6d361d5e12551af5e17f615a568c8
ehmicky <ehmicky@gmail.com>
2017-10-23 14:54:27 +0200

    Refactoring

--- src/protocols/handlers/http/index.js
+++ src/protocols/handlers/http/index.js
@@ -1,18 +1,5 @@
 'use strict';
 
 module.exports = {
-  opts: require('./opts'),
-  description: require('./description'),
-  ...require('./start'),
-  ...require('./stop'),
-  ...require('./headers'),
-  ...require('./payload'),
-  ...require('./url'),
-  ...require('./method'),
-  ...require('./send'),
-  ...require('./name'),
-  ...require('./ip'),
-  ...require('./args'),
-  // eslint-disable-next-line import/max-dependencies
-  ...require('./status'),
+  ...require('./protocol'),
 };
