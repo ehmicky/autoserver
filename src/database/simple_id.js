19cf836b1a44d66df76622ff703b4c3e4e6346b2
ehmicky <ehmicky@gmail.com>
2017-10-25 12:32:39 +0200

    Improve validateMissingIds() middleware

--- src/database/simple_id.js
+++ src/database/simple_id.js
@@ -1,38 +1,54 @@
 'use strict';
 
-const { uniq } = require('lodash');
-
-const { assignArray } = require('../utilities');
+const { uniq, intersection } = require('lodash');
 
 // Try to guess the model `id`s by looking at `args.filter`
 // This won't work on top-level filter of findMany command using a complex one,
 // but that's ok because it will not have any concurrent commands.
+// Returns undefined if it is impossible to guess. Returns empty array if the
+// client specifically asked for no `id`s, e.g. `{ filter: { id: { in: [] } } }`
 const extractSimpleIds = function ({ filter: { type, attrName, value } = {} }) {
   if (type === 'and') {
-    return parseAuthorizationFilter({ value });
+    return parseAndNode({ value });
   }
 
-  if (isComplex({ type, attrName })) { return; }
+  const isSimple = isSimpleFilter({ type, attrName });
+  if (!isSimple) { return; }
 
+  const ids = getIds({ type, value });
+  return ids;
+};
+
+// Parses 'and' top-level node
+const parseAndNode = function ({ value }) {
+  const idsA = value
+    // Filter added by authorization is ignored
+    .filter(({ isAuthorization }) => !isAuthorization)
+    .map(node => extractSimpleIds({ filter: node }));
+
+  const isSimple = idsA.every(ids => Array.isArray(ids));
+  // E.g. `{ id: '5', name: '...' }`
+  if (!isSimple) { return; }
+
+  const idsB = intersection(...idsA);
+  return idsB;
+};
+
+// Check if `args.filter` is simple enough to guess model `id`s
+const isSimpleFilter = function ({ type, attrName }) {
+  // Means there is no filter
+  if (type === undefined) { return false; }
+
+  return attrName === 'id' && ['eq', 'in'].includes(type);
+};
+
+const getIds = function ({ type, value }) {
   // Use either type 'eq' or 'in'
   const ids = type === 'in' ? value : [value];
 
   const idsA = uniq(ids);
 
   return idsA;
-};
-
-// When an authorization filter was used, it adds an 'and' top-level node
-const parseAuthorizationFilter = function ({ value }) {
-  return value
-    .map(node => extractSimpleIds({ filter: node }))
-    .reduce(assignArray, []);
-};
-
-// Check if `args.filter` is too complex to guess model `id`s
-// Also if there is no filter
-const isComplex = function ({ type, attrName }) {
-  return !(attrName === 'id' && ['eq', 'in'].includes(type));
 };
 
 // Returns simple `args.filter` that only filters by `model.id`
--- src/middleware/database/missing_ids.js
+++ src/middleware/database/missing_ids.js
@@ -6,53 +6,68 @@ const { extractSimpleIds, getSimpleFilter } = require('../../database');
 
 // Check if any `id` was not found (404) or was unauthorized (403)
 const validateMissingIds = async function (
-  { command, response: { data }, args: { filter }, mInput },
+  { command, response, args: { filter }, mInput },
   nextLayer,
 ) {
+  // Other commands trigger this middleware during their `currentData` actions
   if (command !== 'find') { return; }
 
-  const filterIds = extractSimpleIds({ filter });
-
-  // Can be checked only when filtering only by `id`
-  // In particular, complex `args.filter` won't be checked
-  if (filterIds === undefined) { return; }
-
-  const responseIds = data.map(model => model && model.id);
-  const ids = filterIds.filter(id => !responseIds.includes(id));
-
+  const ids = getMissingIds({ filter, response });
   if (ids.length === 0) { return; }
 
-  if (hasAuthorizationFilter({ filter })) {
-    await checkAuthorization({ ids, nextLayer, mInput });
-  }
+  // Check whether this is because the model does not exist, or because it is
+  // not authorized
+  await checkAuthorization({ filter, ids, nextLayer, mInput });
 
   throwMissingIds({ ids });
 };
 
-// When an authorization filter was used, it adds an 'and' top-level node
-const hasAuthorizationFilter = function ({ filter: { type } = {} }) {
-  return type === 'and';
+// Retrieve missing models ids
+const getMissingIds = function ({ filter, response: { data } }) {
+  const filterIds = extractSimpleIds({ filter });
+
+  // This middleware can be checked only when filtering only by `id`.
+  // Checking complex `args.filter` is tricky. It is hard to know whether a
+  // model is missing because it does not exist, because it was filter by
+  // authorization filter, or because it was filter by user-specified
+  // `args.filter`.
+  // This means that in general, findMany|deleteMany|updateMany top-level
+  // actions won't use this middleware, unless they are using very simple
+  // `args.filter` like `{ id: { in: ['4', '5'] } }`
+  if (filterIds === undefined) { return []; }
+
+  const responseIds = data.map(({ id }) => id);
+  const ids = filterIds.filter(id => !responseIds.includes(id));
+
+  return ids;
 };
 
 // Try the same database query, but this time without the authorization filter,
 // and only on the missing models.
 // If no missing model is missing anymore, flag it as an authorization error.
 const checkAuthorization = async function ({
+  filter,
   ids,
   nextLayer,
   mInput: { mInput, args },
 }) {
+  if (!hasAuthorizationFilter({ filter })) { return; }
+
   const filterA = getSimpleFilter({ ids });
-  const argsA = { ...args, filter: filterA };
-  const mInputA = { ...mInput, args: argsA };
+  const mInputA = { ...mInput, args: { ...args, filter: filterA } };
 
   const { response } = await nextLayer(mInputA);
 
   const isAuthorizationError = response.length === ids.length;
+  if (!isAuthorizationError) { return; }
 
-  if (isAuthorizationError) {
-    throwAuthorizationError({ ids });
-  }
+  throwAuthorizationError({ ids });
+};
+
+// When an authorization filter was used, it adds an 'and' top-level node
+// with some node children with the `isAuthorization` `true` flag
+const hasAuthorizationFilter = function ({ filter: { type, value } = {} }) {
+  return type === 'and' && value.some(({ isAuthorization }) => isAuthorization);
 };
 
 const throwMissingIds = function ({ ids }) {
