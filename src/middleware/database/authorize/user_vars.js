67f67053fa4ae0a8945784b9fbff88b487d417d2
ehmicky <ehmicky@gmail.com>
2017-10-30 14:06:12 +0100

    Use filter crawler with getUserVars()

--- src/middleware/database/authorize/user_vars.js
+++ src/middleware/database/authorize/user_vars.js
@@ -2,18 +2,16 @@
 
 const { uniq, intersection } = require('lodash');
 
-const {
-  assignArray,
-  pick,
-  mapValues,
-  fullRecurseMap,
-} = require('../../../utilities');
+const { pick, mapValues } = require('../../../utilities');
 const { runSchemaFunc } = require('../../../schema_func');
+const { crawlNodes } = require('../../../filter');
 
 // Retrieve all user variables used in `model.authorize`, and resolve their
 // schema functions.
 const getUserVars = function ({ authorize, userVars, mInput }) {
-  const userVarsNames = getUserVarsNames({ authorize, userVars });
+  // Retrieve all `attrName` recursively inside filter AST
+  const attrNames = crawlNodes(authorize, ({ attrName }) => attrName);
+  const userVarsNames = getPossibleUserVars({ attrNames, userVars });
   const userVarsA = pick(userVars, userVarsNames);
   const userVarsB = mapValues(
     userVarsA,
@@ -22,41 +20,12 @@ const getUserVars = function ({ authorize, userVars, mInput }) {
   return userVarsB;
 };
 
-// Retrieve the names of all user variables used in `model.authorize`
-const getUserVarsNames = function ({ authorize, userVars }) {
-  const { attrNames: userVarsNames } = fullRecurseMap(
-    authorize,
-    recurseUserVarsNames,
-  );
-  const userVarsNamesA = getPossibleUserVars({ userVarsNames, userVars });
-  return userVarsNamesA;
-};
-
-// Retrieve all `attrName` recursively inside filter AST
-const recurseUserVarsNames = function (obj, name) {
-  if (name === 'attrName') {
-    return { attrNames: [obj] };
-  }
-
-  if (typeof obj !== 'object') { return; }
-
-  const attrNames = getAttrNames(obj);
-  return { attrNames };
-};
-
-const getAttrNames = function (obj) {
-  return Object.values(obj)
-    .filter(value => value && Array.isArray(value.attrNames))
-    .map(({ attrNames }) => attrNames)
-    .reduce(assignArray, []);
-};
-
 // Only keep the `attrName` that targets a user variable
-const getPossibleUserVars = function ({ userVarsNames, userVars }) {
+const getPossibleUserVars = function ({ attrNames, userVars }) {
   const possibleUserVars = Object.keys(userVars);
-  const userVarsNamesA = intersection(userVarsNames, possibleUserVars);
-  const userVarsNamesB = uniq(userVarsNamesA);
-  return userVarsNamesB;
+  const userVarsNames = intersection(attrNames, possibleUserVars);
+  const userVarsNamesA = uniq(userVarsNames);
+  return userVarsNamesA;
 };
 
 module.exports = {
