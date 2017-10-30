74d6f8e84fc416fb47719b8a8c65d60ba079f58c
ehmicky <ehmicky@gmail.com>
2017-10-30 12:31:46 +0100

    Add crawlAttrs()

--- src/filter/crawl.js
+++ src/filter/crawl.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const { assignArray } = require('../utilities');
+const { assignArray, groupBy } = require('../utilities');
 
 // Call `func(node)` recursively over each node of `args.filter`
 // Returns array of func() return values
 const crawlNodes = function (node, func) {
-  const returnValue = func(node);
-
-  const children = getChildren(node)
+  const children = getNodeChildren(node)
     .map(child => crawlNodes(child, func))
     .reduce(assignArray, []);
+
+  const returnValue = func(node);
 
   if (returnValue === undefined) { return children; }
 
@@ -21,7 +21,7 @@ const crawlNodes = function (node, func) {
 const mapNodes = function (node, func) {
   const nodeA = func(node);
 
-  const children = getChildren(nodeA);
+  const children = getNodeChildren(nodeA);
   if (children === undefined) { return nodeA; }
 
   const value = children
@@ -30,15 +30,54 @@ const mapNodes = function (node, func) {
   return { ...nodeA, value };
 };
 
-const getChildren = function ({ type, value }) {
-  if (!PARENT_TYPES.includes(type)) { return []; }
+const getNodeChildren = function ({ type, value }) {
+  if (!NODE_PARENT_TYPES.includes(type)) { return []; }
 
   return value;
 };
 
-const PARENT_TYPES = ['all', 'some', 'or', 'and'];
+const NODE_PARENT_TYPES = ['all', 'some', 'or', 'and'];
+
+// Call `func(node)` recursively over each attribute of `args.filter`
+// Returns array of func() return values
+const crawlAttrs = function (node, func) {
+  const children = getAttrChildren(node);
+
+  if (children.length > 0) {
+    return children
+      .map(child => crawlAttrs(child, func))
+      .reduce(assignArray, []);
+  }
+
+  const returnValue = getAttrs(node, func);
+
+  return [...returnValue, ...children];
+};
+
+const getAttrs = function (node, func) {
+  const { type, value } = node;
+
+  if (!ATTR_PARENT_TYPES.includes(type)) {
+    return func([node]) || [];
+  }
+
+  const groups = groupBy(value, 'attrName');
+
+  return Object.entries(groups)
+    .map(([attrName, group]) => func(group, attrName));
+};
+
+const getAttrChildren = function ({ type, value }) {
+  if (!ATTR_RECURSIVE_TYPES.includes(type)) { return []; }
+
+  return value;
+};
+
+const ATTR_RECURSIVE_TYPES = ['or'];
+const ATTR_PARENT_TYPES = ['and'];
 
 module.exports = {
   crawlNodes,
+  crawlAttrs,
   mapNodes,
 };
