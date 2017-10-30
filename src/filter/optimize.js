0e46346fd70012e798f1dc431b7b3221708a5598
ehmicky <ehmicky@gmail.com>
2017-10-30 14:00:44 +0100

    Use mapNodes() with filter optimizer

--- src/filter/optimize.js
+++ src/filter/optimize.js
@@ -1,18 +1,15 @@
 'use strict';
 
-const { fullRecurseMap } = require('../utilities');
-
 const { getOperator } = require('./operators');
+const { mapNodes } = require('./crawl');
 
 // Try to simplify AST
 const optimizeFilter = function ({ filter }) {
-  return fullRecurseMap(filter, optimizeNode);
+  return mapNodes(filter, optimizeNode);
 };
 
 const optimizeNode = function (node) {
   const operator = getOperator({ node });
-  // Is not a node
-  if (operator === undefined) { return node; }
 
   const { optimize } = operator;
   if (optimize === undefined) { return node; }
