33ece682739317aa154eadc3f023e958ec316b50
autoserver-founder <founder@autoserver.io>
2017-10-31 19:11:18 +0100

    Refactor commands constants files

---
+++ src/constants/commands_utils.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { uniq } = require('lodash');
+
+const { throwError } = require('../error');
+
+const COMMANDS = require('./commands');
+
+const TYPES = COMMANDS.map(({ type }) => type);
+const COMMAND_TYPES = uniq(TYPES);
+
+const getCommand = function ({ commandType, multiple }) {
+  const commandA = COMMANDS.find(command =>
+    command.multiple === multiple && command.type === commandType);
+
+  if (commandA === undefined) {
+    const message = `Command '${commandType}' is unknown`;
+    throwError(message, { reason: 'INPUT_VALIDATION' });
+  }
+
+  return commandA;
+};
+
+module.exports = {
+  COMMANDS,
+  COMMAND_TYPES,
+  getCommand,
+};
