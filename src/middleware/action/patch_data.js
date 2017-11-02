09dc0bdfd0a22a2697324af3e11c5944896482ba
autoserver-founder <founder@autoserver.io>
2017-11-02 13:56:45 +0100

    Refactor patchData middleware

--- src/middleware/action/patch_data.js
+++ src/middleware/action/patch_data.js
@@ -1,58 +1,59 @@
 'use strict';
 
-const { assignArray } = require('../../utilities');
+const { assignArray, groupBy, mapValues } = require('../../utilities');
 
 // Merge `currentData` with the `args.data` in `patch` commands,
 // to obtain the final models we want to use as replacement
 const patchData = function ({ actions, top: { command } }) {
   if (command.type !== 'patch') { return; }
 
-  const partialData = mergePartialData({ actions });
-  const actionsA = actions.map(action => mergeData({ action, partialData }));
+  const dataMap = mergePartialData({ actions });
+  const actionsA = actions.map(action => addData({ action, dataMap }));
 
   return { actions: actionsA };
 };
 
-// Several actions might target the same model, but with different args.data
-// We merge all the args.data here, with priority to the children.
+// Merge `currentData` with `args.data`
 const mergePartialData = function ({ actions }) {
+  const actionsA = flattenActions({ actions });
+  const dataMap = groupBy(actionsA, getActionKey);
+  const dataMapA = mapValues(dataMap, mergeData);
+  return dataMapA;
+};
+
+// Flatten `action.data` and `action.currentData` together
+const flattenActions = function ({ actions }) {
   return actions
-    .map(getPartialDataById)
-    .reduce(assignArray, [])
-    .reduce(assignArray, [])
-    .reduce(reducePartialData, {});
+    .map(flattenAction)
+    .reduce(assignArray, []);
 };
 
-// Group args.data according to currentData `id`
-const getPartialDataById = function ({
-  args: { data: [data] },
-  modelName,
-  currentData,
-}) {
+const flattenAction = function ({ currentData, args: { data }, modelName }) {
   return currentData
-    .map(({ id }) => ({ key: `${modelName} ${id}`, data }));
+    .map(currentDatum => ({ data: data[0], currentDatum, modelName }));
 };
 
-const reducePartialData = function (dataObj, { key, data }) {
-  const { [key]: dataA } = dataObj;
-
-  // Merges `args.data` with each other
-  const dataB = { ...dataA, ...data };
-
-  return { ...dataObj, [key]: dataB };
+// Group args.data according to currentData `id` and `modelName`
+const getActionKey = function ({ modelName, currentDatum: { id } }) {
+  return `${modelName} ${id}`;
 };
 
-// Merges `args.data` with `currentData`
-const mergeData = function ({
+// Do the actual merging
+const mergeData = function (actions) {
+  const actionsData = actions.map(({ data }) => data);
+  const [{ currentDatum }] = actions;
+  // Several actions might target the same model, but with different args.data
+  // We merge all the args.data here, with priority to the children.
+  return Object.assign({}, currentDatum, ...actionsData);
+};
+
+// Add merged `args.data` to each action
+const addData = function ({
   action,
   action: { args, modelName, currentData },
-  partialData,
+  dataMap,
 }) {
-  const data = currentData.map(currentDatum => {
-    const partialDatum = partialData[`${modelName} ${currentDatum.id}`];
-    return { ...currentDatum, ...partialDatum };
-  });
-
+  const data = currentData.map(({ id }) => dataMap[`${modelName} ${id}`]);
   return { ...action, args: { ...args, data } };
 };
 
