f29976774aceb000b21e6783bbbeaf0363a5ffb9
autoserver-founder <founder@autoserver.io>
2017-11-02 12:31:15 +0100

    Refactor currentData

--- src/middleware/action/current_data/main.js
+++ src/middleware/action/current_data/main.js
@@ -6,15 +6,11 @@ const { serialResolve } = require('./serial');
 // Add `action.currentData`, i.e. current models for the write actions about
 // to be fired.
 // Also adds `action.dataPaths` for `patch` and `delete` commands.
-const addCurrentData = function (
-  { actions, top, top: { command }, ...rest },
-  nextLayer,
-) {
+const addCurrentData = function ({ top: { command }, ...rest }, nextLayer) {
   const resolver = resolvers[command.type];
+  if (resolver === undefined) { return; }
 
-  if (resolver === undefined) { return { actions }; }
-
-  return resolver({ actions, top, ...rest }, nextLayer);
+  return resolver(rest, nextLayer);
 };
 
 // `find` and `create` commands do not use `currentData`
