31756205a991d0363d6f4327848e0e1595cb179e
ehmicky <ehmicky@gmail.com>
2017-11-02 12:26:10 +0100

    Refactor getOperationSummary middleware

--- src/middleware/action/operation_summary.js
+++ src/middleware/action/operation_summary.js
@@ -5,27 +5,24 @@ const { isEqual } = require('../../utilities');
 // Retrieves `operationSummary`
 // This is all `actions`, included nested ones as a nice formatted string,
 // e.g. 'findModel{attrA,attrB,child{attrC}}'
-const getOperationSummary = function ({ actions, top, top: { commandPath } }) {
-  const commandName = commandPath[commandPath.length - 1];
-
-  const childActions = actions.filter(({ commandPath: childPath }) =>
-    isEqual(commandPath, childPath.slice(0, -1)));
-
-  if (childActions.length === 0) {
-    return { operationSummary: commandName };
-  }
-
-  const childActionsStr = childActions
-    .map(action => getChildSummary({ action, actions, top }))
-    .join(',');
-  const operationSummary = `${commandName}{${childActionsStr}}`;
-
+const getOperationSummary = function ({ actions, top: { commandPath } }) {
+  const operationSummary = getSummary({ actions, commandPath });
   return { operationSummary };
 };
 
-const getChildSummary = function ({ action: { commandPath }, actions, top }) {
-  const topA = { ...top, commandPath };
-  const { operationSummary } = getOperationSummary({ actions, top: topA });
+const getSummary = function ({ actions, commandPath: path }) {
+  const commandName = path[path.length - 1];
+
+  const childActions = actions
+    .filter(({ commandPath }) => isEqual(path, commandPath.slice(0, -1)));
+
+  if (childActions.length === 0) { return commandName; }
+
+  const childActionsStr = childActions
+    .map(({ commandPath }) => getSummary({ actions, commandPath }))
+    .join(',');
+  const operationSummary = `${commandName}{${childActionsStr}}`;
+
   return operationSummary;
 };
 
