dc2913176dc126caec9dbbf605822fe6bbfb84d6
autoserver-founder <founder@autoserver.io>
2017-11-02 14:30:21 +0100

    Start refactoring write layer

--- src/middleware/write/main.js
+++ src/middleware/write/main.js
@@ -7,48 +7,57 @@ const { getArgs } = require('./args');
 const { getResults } = require('./results');
 
 // Fire all commands associated with a set of write actions
-const sequenceWrite = async function ({ actions, mInput }, nextLayer) {
+const sequenceWrite = async function ({ actions, top, mInput }, nextLayer) {
   // Run write commands in parallel, for each `modelName`
   const actionsGroups = groupValuesBy(actions, 'modelName');
   const resultsPromises = actionsGroups
-    .map(actionsA => fireCommand({ actions: actionsA, mInput, nextLayer }));
+    .map(actionsA => getCommandArgs({ actions: actionsA, top }))
+    .filter(isNotEmpty)
+    .map(allInput => getInput({ ...allInput, top, mInput }))
+    .map(allInput => fireCommand({ ...allInput, top, nextLayer }));
   const results = await Promise.all(resultsPromises);
 
   const resultsA = results.reduce(assignArray, []);
   return { results: resultsA };
 };
 
-const fireCommand = async function ({
-  actions,
-  actions: [{ modelName }],
-  mInput,
-  mInput: { top },
-  nextLayer,
-}) {
+// Add next layers's `args` and `ids`
+const getCommandArgs = function ({ actions, top }) {
   const { args, ids } = getArgs({ actions, top });
-
-  // No model to modify, so can return right away
-  if (ids.length === 0) { return []; }
-
-  const results = await fireWriteCommand({ actions, args, nextLayer, mInput });
-
-  const resultsA = getResults({ actions, results, ids, modelName, top });
-  return resultsA;
+  return { actions, args, ids };
 };
 
-// Fire actual write command
-const fireWriteCommand = async function ({
+// If no model to modify, can return empty array right away
+const isNotEmpty = function ({ ids }) {
+  return ids.length !== 0;
+};
+
+// Add next layers's whole input
+const getInput = function ({
   actions,
   actions: [{ modelName }],
   args,
-  nextLayer,
+  top: { command: { type: command } },
   mInput,
-  mInput: { top: { command: { type: command } } },
+  ...rest
 }) {
   const commandPath = mergeCommandPaths({ actions });
-  const mInputA = { ...mInput, commandPath, command, modelName, args };
-  const { response: { data } } = await nextLayer(mInputA, 'request_response');
-  return data;
+  const input = { ...mInput, commandPath, command, modelName, args };
+  return { input, actions, ...rest };
+};
+
+const fireCommand = async function ({
+  actions,
+  ids,
+  top,
+  input,
+  input: { modelName },
+  nextLayer,
+}) {
+  const { response: { data } } = await nextLayer(input, 'request_response');
+
+  const resultsA = getResults({ actions, results: data, ids, modelName, top });
+  return resultsA;
 };
 
 module.exports = {
