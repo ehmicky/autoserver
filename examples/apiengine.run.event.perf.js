5cd1dd3cd56de23d015b824c31d594c0393d442c
ehmicky <ehmicky@gmail.com>
2017-11-06 16:15:33 +0100

    Change measuresMessage case

--- examples/apiengine.run.event.perf.js
+++ examples/apiengine.run.event.perf.js
@@ -4,7 +4,7 @@
 // eslint-disable-next-line fp/no-let
 let perfEvent = function (payload) {
   // eslint-disable-next-line no-console, no-restricted-globals
-  console.log(`Performance logging info\n${payload.measuresMessage}`);
+  console.log(`Performance logging info\n${payload.measuresmessage}`);
 };
 
 // This file is for debugging only. Comment this to enable it.
