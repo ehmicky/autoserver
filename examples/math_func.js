05bdb6a25fe4c21ba498ae31f7155027efb2f523
ehmicky <ehmicky@gmail.com>
2017-11-06 12:02:48 +0100

    Change naming convention for schema.variables

--- examples/math_func.js
+++ examples/math_func.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const MathFunc = function ({ $ip }, { numA, numB, numC, numD }) {
+const mathFunc = function ({ $ip }, { numA, numB, numC, numD }) {
   const ipNumber = Number($ip.slice(0, IP_NUMBER_LENGTH));
   return ipNumber + (numA * numB) + (numC * numD);
 };
 
 const IP_NUMBER_LENGTH = 3;
 
-module.exports = MathFunc;
+module.exports = mathFunc;
