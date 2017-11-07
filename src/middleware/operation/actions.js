be8cabf6a24fe7e2a011ac1df9c36ec2d9ff811f
ehmicky <ehmicky@gmail.com>
2017-11-07 12:13:44 +0100

    Add topargs and summary to introspection queries

--- src/middleware/operation/actions.js
+++ src/middleware/operation/actions.js
@@ -2,9 +2,17 @@
 
 // Fire actions, unless the response is already known
 const fireActions = function ({ response, mInput }, nextLayer) {
-  if (response) { return; }
+  if (response) {
+    return alreadyHandled(mInput);
+  }
 
   return nextLayer(mInput, 'action');
+};
+
+// When the operation parser already returned the response,
+// e.g. with GraphQL introspection queries
+const alreadyHandled = function ({ protocolArgs }) {
+  return { topargs: protocolArgs };
 };
 
 module.exports = {
