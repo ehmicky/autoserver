87da9ec97a9bd1672a263d762a50b880a4c8973b
autoserver-founder <founder@autoserver.io>
2017-11-08 16:24:55 +0100

    Add METHODS constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,7 @@
 module.exports = {
   ...require('./commands_utils'),
   ...require('./content_types'),
+  METHODS: require('./methods'),
   PAYLOAD_TYPES: require('./payload_types'),
   FEATURES: require('./features'),
 };
