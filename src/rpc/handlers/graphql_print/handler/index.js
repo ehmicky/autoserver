97858b66dc3babff6b21f54b282171101123d6e4
autoserver-founder <founder@autoserver.io>
2017-11-09 10:26:23 +0100

    Move file

--- src/rpc/handlers/graphql_print/handler/index.js
+++ src/rpc/handlers/graphql_print/handler/index.js
@@ -1,21 +1,5 @@
 'use strict';
 
-const { resolve } = require('path');
-
-const { renderTemplate } = require('../../../../utilities');
-
-const TEMPLATE = resolve(__dirname, './print.mustache');
-
-// Print GraphQL schema as beautified HTML
-const handler = async function ({ schema: { graphqlPrintedSchema } }) {
-  const content = await renderTemplate({
-    template: TEMPLATE,
-    data: { graphqlPrintedSchema, prismVersion: '1.6.0' },
-  });
-
-  return { response: { type: 'html', content } };
-};
-
 module.exports = {
-  handler,
+  ...require('./main'),
 };
