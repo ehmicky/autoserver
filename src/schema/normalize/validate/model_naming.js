837a335a9eb818160265ae412ce0c45c349a3f12
ehmicky <ehmicky@gmail.com>
2017-11-09 13:15:58 +0100

    Force model names to be plural

--- src/schema/normalize/validate/model_naming.js
+++ src/schema/normalize/validate/model_naming.js
@@ -1,28 +1,28 @@
 'use strict';
 
-const { singular, plural } = require('pluralize');
+const { plural } = require('pluralize');
 
 const { throwError } = require('../../../error');
 
 // Validate models are properly named
-const validateModelNaming = function ({ schema }) {
-  if (!schema.models) { return schema; }
+const validateModelNaming = function ({ schema, schema: { models } }) {
+  if (!(models && models.constructor === Object)) { return schema; }
 
-  return Object.keys(schema.models).reduce(
-    (schemaA, modelname) => checkModelname({ schema: schemaA, modelname }),
-    schema,
-  );
-};
-
-const checkModelname = function ({ schema, modelname }) {
-  // Checks that a word (e.g. a model) is an English word with a
-  // different singular and plural form
-  if (singular(modelname) === plural(modelname)) {
-    const message = `Model name '${modelname}' must be an English word whose plural form differs from its singular form`;
-    throwError(message, { reason: 'SCHEMA_VALIDATION' });
-  }
+  Object.keys(models).forEach(checkModelname);
 
   return schema;
+};
+
+const checkModelname = function (modelname) {
+  const pluralname = plural(modelname);
+  // Model name must be plural
+  // The reason is to avoid having to handle different cases where the model
+  // name is sometimes singular, sometimes plural. This is also easier for
+  // the user to remember.
+  if (modelname === pluralname) { return; }
+
+  const message = `Model name '${modelname}' must be in plural form, i.e. should be '${pluralname}'`;
+  throwError(message, { reason: 'SCHEMA_VALIDATION' });
 };
 
 module.exports = {
