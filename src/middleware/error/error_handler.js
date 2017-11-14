a90447585aa95a11f303269782b7b2c30b37f06c
ehmicky <ehmicky@gmail.com>
2017-11-14 13:47:10 +0100

    Fix error handlers

--- src/middleware/error/error_handler.js
+++ src/middleware/error/error_handler.js
@@ -23,7 +23,12 @@ const errorHandler = async function ({
   await reportError({ runOpts, level, error, mInput });
 
   // Make sure a response is sent, even empty, or the socket will hang
-  await protocolHandler.send({ specific, protocolstatus, content: '' });
+  await protocolHandler.send({
+    specific,
+    protocolstatus,
+    content: '',
+    contentLength: 0,
+  });
 };
 
 // Report any exception thrown
