c5ebb5ed61cce7106388d630c2f812ec87ab3998
autoserver-founder <founder@autoserver.io>
2017-11-15 18:08:00 +0100

    Remove debugging code

--- src/formats/parse_serialize.js
+++ src/formats/parse_serialize.js
@@ -25,38 +25,6 @@ const genericSerialize = function ({ format, content }) {
   return contentC;
 };
 
-let aa = {
-  a: '1',
-  b: 1,
-  c: null,
-  d: undefined,
-  e: 1.5,
-  f: true,
-  g: [],
-  h: [null],
-  i: [1, 2],
-  j: {
-    k: true,
-  },
-};
-aa = {
-  "maxpagesize": 10,
-  "protocols": {
-    "http": {
-      "hostname": "myhostname"
-    }
-  },
-  "filter": {
-    "payload": ["id", "old_id"]
-  }
-}
-
-console.log(aa);
-const bb = genericSerialize({ format: 'urlencoded', content: aa });
-console.log(bb);
-const cc = genericParse({ format: 'urlencoded', content: bb });
-console.log(cc);
-
 module.exports = {
   parse: genericParse,
   serialize: genericSerialize,
