584e0696589efbeea607cc4844a854845308cd95
autoserver-founder <founder@autoserver.io>
2017-11-15 17:36:15 +0100

    Add Hjson format

---
+++ src/formats/handlers/hjson.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const hjson = require('hjson');
+
+// Parses a HJSON file
+const parse = function ({ content }) {
+  return hjson.parse(content, {
+  });
+};
+
+// Serializes a HJSON file
+const serialize = function ({ content }) {
+  return hjson.stringify(content, { bracesSameLine: true });
+};
+
+module.exports = {
+  name: 'hjson',
+  title: 'Hjson',
+  types: ['conf', 'payload', 'db'],
+  extNames: ['hjson'],
+  mimes: [
+    'application/hjson',
+    'text/hjson',
+    '+hjson',
+  ],
+  charsets: ['utf-8'],
+  jsonCompat: ['superset'],
+  parse,
+  serialize,
+};
