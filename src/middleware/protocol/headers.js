c4117d5d10925934aafe6bc8fcbe3d58c43eb7ae
ehmicky <ehmicky@gmail.com>
2017-11-17 13:09:09 +0100

    Add full protocol normalization support

--- src/middleware/protocol/headers.js
+++ src/middleware/protocol/headers.js
@@ -5,26 +5,13 @@ const { validateProtocolObject } = require('./validate_parsing');
 // Fill in `mInput.headers` using protocol-specific headers.
 // Are set in a protocol-agnostic format, i.e. each protocol sets the same
 // object.
-const parseHeaders = function ({
-  specific,
-  protocolHandler: { getHeaders },
-  topargs = {},
-}) {
-  if (getHeaders === undefined) { return { topargs }; }
+const parseHeaders = function ({ specific, protocolHandler: { getHeaders } }) {
+  if (getHeaders === undefined) { return; }
 
   const headers = getHeaders({ specific });
   validateProtocolObject({ headers });
 
-  const topargsA = getTopargs({ topargs, headers });
-
-  return { headers, topargs: topargsA };
-};
-
-// Client parameters can be specified in protocol headers
-const getTopargs = function ({ topargs, headers: { params } }) {
-  if (params === undefined) { return topargs; }
-
-  return { ...topargs, params };
+  return { headers };
 };
 
 module.exports = {
---
+++ src/middleware/protocol/normalize/main.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { normalizePartialProtocol } = require('./partial');
+
+// Normalize variables created during protocol layer
+const protocolNormalization = function ({ topargs, ...rest }) {
+  const {
+    headers: headersA,
+    method: methodA,
+    path: pathA,
+    payload: payloadA,
+    queryvars: queryvarsA,
+  } = normalizePartialProtocol({ ...rest });
+
+  const {
+    headers: headersB,
+    topargs: topargsA,
+  } = normalizeProtocol({ headers: headersA, topargs });
+
+  return {
+    payload: payloadA,
+    headers: headersB,
+    method: methodA,
+    path: pathA,
+    queryvars: queryvarsA,
+    topargs: topargsA,
+  };
+};
+
+// Normalization for any protocol, even non-partial ones
+const normalizeProtocol = function ({ headers, topargs }) {
+  const headersA = headers && headers.constructor === Object ? headers : {};
+
+  const topargsA = getTopargs({ topargs, headers: headersA });
+
+  return { headers: headersA, topargs: topargsA };
+};
+
+// Client parameters can be specified in protocol headers
+const getTopargs = function ({ topargs = {}, headers: { params } }) {
+  if (params === undefined) { return topargs; }
+
+  return { ...topargs, params };
+};
+
+module.exports = {
+  protocolNormalization,
+};
