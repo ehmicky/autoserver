3811b753b979604647e4cf56eb88f771bf77887f
ehmicky <ehmicky@gmail.com>
2017-11-17 15:38:12 +0100

    Add comment

--- src/protocols/handlers/http/ip.js
+++ src/protocols/handlers/http/ip.js
@@ -3,7 +3,21 @@
 const { getClientIp } = require('request-ip');
 
 // Retrieves request IP.
-// If unknown, should return undefined.
+// Tries, in order:
+//  - X-Client-IP [C]
+//  - X-Forwarded-For [C]
+//  - CF-Connecting-IP [C]
+//  - True-Client-Ip [C]
+//  - X-Real-IP [C]
+//  - X-Cluster-Client-IP [C]
+//  - X-Forwarded [C]
+//  - Forwarded-For [C]
+//  - REQ.connection.remoteAddress
+//  - REQ.connection.socket.remoteAddress
+//  - REQ.socket.remoteAddress
+//  - REQ.info.remoteAddress
+// If invalid IPv4|IPv6, throws.
+// If unknown, returns undefined.
 const getIp = function ({ specific: { req } }) {
   return getClientIp(req);
 };
