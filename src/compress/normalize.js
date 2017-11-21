aa8d9f4da00c5caab75d10d01240473f9d794692
ehmicky <ehmicky@gmail.com>
2017-11-21 12:41:31 +0100

    Refactoring

--- src/compress/normalize.js
+++ src/compress/normalize.js
@@ -7,9 +7,19 @@ const normalizeCompress = function ({
   compressResponse: { name: compressResponse } = DEFAULT_COMPRESS,
   compressRequest: { name: compressRequest } = DEFAULT_COMPRESS,
 }) {
-  return `${compressResponse},${compressRequest}`;
+  const compress = `${compressResponse},${compressRequest}`;
+  return compress;
+};
+
+// Using query variable ?compress=REQUEST_COMPRESSION[,RESPONSE_COMPRESSION]
+const denormalizeCompress = function ({ compress }) {
+  if (compress === undefined) { return {}; }
+
+  const [compressResponse, compressRequest] = compress.split(',');
+  return { compressResponse, compressRequest };
 };
 
 module.exports = {
   normalizeCompress,
+  denormalizeCompress,
 };
