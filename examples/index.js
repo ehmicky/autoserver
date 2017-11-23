26aabdd3c39d38cdc7dbfc5ddbae10e79d094047
autoserver-founder <founder@autoserver.io>
2017-11-23 12:42:21 +0100

    Better debugging output

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,24 @@
 'use strict';
 
+const { inspect } = require('util');
+
 const apiengine = require('..');
+
+// Set default console log printing
+const setDefaultDebug = function () {
+  // eslint-disable-next-line fp/no-mutation
+  inspect.defaultOptions = {
+    colors: true,
+    depth: null,
+    breakLength: process.stdout.columns || COLUMNS_WIDTH,
+  };
+
+  // eslint-disable-next-line fp/no-mutation
+  Error.stackTraceLimit = STACK_TRACE_LIMIT;
+};
+
+const COLUMNS_WIDTH = 80;
+const STACK_TRACE_LIMIT = 20;
 
 const startServer = async function () {
   try {
@@ -15,5 +33,7 @@ const startServer = async function () {
     console.log(details);
   }
 };
+
+setDefaultDebug();
 
 startServer();
