fd15bb36d56bb9045c9ef335dd873ec414b61576
ehmicky <ehmicky@gmail.com>
2017-11-24 17:26:27 +0100

    Add _insertstr patch operator

--- src/patch/operators/string.js
+++ src/patch/operators/string.js
@@ -17,6 +17,30 @@ const sliceOperator = {
   },
 };
 
+// `_insertstr` patch operator
+const insertOperator = {
+  attribute: ['string'],
+
+  argument: ['integer[]', 'string[]'],
+
+  check (opVal) {
+    const isValid = opVal.length === 2 &&
+      Number.isInteger(opVal[0]) &&
+      typeof opVal[1] === 'string';
+    if (isValid) { return; }
+
+    return 'the argument must be an array with one integer (the index) and a string';
+  },
+
+  apply (attrVal, [index, str]) {
+    const indexA = index < 0 ? Math.max(attrVal.length + index, 0) : index;
+    const beginning = attrVal.substr(0, indexA);
+    const end = attrVal.substr(indexA);
+    return `${beginning}${str}${end}`;
+  },
+};
+
 module.exports = {
   _slicestr: sliceOperator,
+  _insertstr: insertOperator,
 };
