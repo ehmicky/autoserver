428b2801e6f1c6ceeb8444020eaa844a2cd8ebe0
autoserver-founder <founder@autoserver.io>
2017-11-24 17:41:14 +0100

    Add _replace patch operator

--- src/patch/operators/string.js
+++ src/patch/operators/string.js
@@ -40,7 +40,53 @@ const insertOperator = {
   },
 };
 
+// `_replace` patch operator
+const replaceOperator = {
+  attribute: ['string'],
+
+  argument: ['string[]'],
+
+  check (opVal) {
+    // eslint-disable-next-line no-magic-numbers
+    const isValid = opVal.length <= 3 && opVal.length >= 2;
+
+    if (!isValid) {
+      return 'the argument must be an array with one regular expression, then a string, then an optional list of flags';
+    }
+
+    return validateRegExp({ opVal });
+  },
+
+  apply (attrVal, [regExp, str, flags]) {
+    const regExpA = getRegExp({ regExp, flags });
+    return attrVal.replace(regExpA, str);
+  },
+};
+
+const validateRegExp = function ({ opVal }) {
+  const [regExp, , flags] = opVal;
+
+  try {
+    getRegExp({ regExp, flags });
+
+    return;
+  } catch (error) {}
+
+  try {
+    getRegExp({ regExp });
+
+    return 'the regular expression\'s flags are invalid';
+  } catch (error) {
+    return 'the regular expression is invalid';
+  }
+};
+
+const getRegExp = function ({ regExp, flags = 'gi' }) {
+  return new RegExp(regExp, flags);
+};
+
 module.exports = {
   _slicestr: sliceOperator,
   _insertstr: insertOperator,
+  _replace: replaceOperator,
 };
