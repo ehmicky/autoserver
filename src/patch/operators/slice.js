5fd699eb14cdc008997a80dc786e970a4830147c
ehmicky <ehmicky@gmail.com>
2017-11-27 12:14:02 +0100

    Improve _slice, _slicestr, _insert and _insertstr patch operators

--- src/patch/operators/slice.js
+++ src/patch/operators/slice.js
@@ -9,9 +9,25 @@ const checkSlice = function ({ $arg: opVal }) {
 };
 
 const commonAttrs = {
-  argument: ['integer[]'],
+  argument: ['integer[]', 'null[]'],
 
   check: checkSlice,
+};
+
+// Negative indexes are from the end. Null indexes are representing the end.
+// Positive indexes are from the start.
+const argToIndex = function (arg, attrVal) {
+  if (arg == null) { return attrVal.length; }
+
+  if (arg >= 0) { return arg; }
+
+  return attrVal.length + arg;
+};
+
+const sliceApply = function ({ attrVal, start, end }) {
+  const startA = argToIndex(start, attrVal);
+  const endA = argToIndex(end, attrVal);
+  return attrVal.slice(startA, endA);
 };
 
 // `_slicestr` patch operator
@@ -20,8 +36,8 @@ const slicestrOperator = {
 
   attribute: ['string'],
 
-  apply ({ $val: attrVal = '', $arg: [index, length] }) {
-    return attrVal.substr(index, length);
+  apply ({ $val: attrVal = '', $arg: [start, end] }) {
+    return sliceApply({ attrVal, start, end });
   },
 };
 
@@ -31,12 +47,61 @@ const sliceOperator = {
 
   attribute: ANY_ARRAY,
 
-  apply ({ $val: attrVal = [], $arg: [index, length] }) {
-    return attrVal.slice(index, length);
+  apply ({ $val: attrVal = [], $arg: [start, end] }) {
+    return sliceApply({ attrVal, start, end });
+  },
+};
+
+const insertApply = function ({ index, attrVal }) {
+  const indexA = argToIndex(index, attrVal);
+  const start = attrVal.slice(0, indexA);
+  const end = attrVal.slice(indexA);
+  return { start, end };
+};
+
+// `_insertstr` patch operator
+const insertstrOperator = {
+  attribute: ['string'],
+
+  argument: ['integer[]', 'null[]', 'string[]'],
+
+  check ({ $arg: opVal }) {
+    const isValid = opVal.length === 2 &&
+      (Number.isInteger(opVal[0]) || opVal[0] == null) &&
+      typeof opVal[1] === 'string';
+    if (isValid) { return; }
+
+    return 'the argument must be an array with one integer (the index) and a string';
+  },
+
+  apply ({ $val: attrVal = '', $arg: [index, str] }) {
+    const { start, end } = insertApply({ index, attrVal });
+    return `${start}${str}${end}`;
+  },
+};
+
+// `_insert` patch operator
+const insertOperator = {
+  attribute: ANY_ARRAY,
+
+  argument: ANY_ARRAY,
+
+  check ({ $arg: [index] }) {
+    const isValid = Number.isInteger(index) || index == null;
+    if (isValid) { return; }
+
+    return 'the argument\'s first value must be an integer (the index)';
+  },
+
+  apply ({ $val: attrVal = [], $arg: [index, ...values] }) {
+    const { start, end } = insertApply({ index, attrVal });
+    return [...start, ...values, ...end];
   },
 };
 
 module.exports = {
   _slicestr: slicestrOperator,
   _slice: sliceOperator,
+  _insertstr: insertstrOperator,
+  _insert: insertOperator,
 };
