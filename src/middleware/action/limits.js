958d665e3edefd03d8800399546a159fd0453dfd
ehmicky <ehmicky@gmail.com>
2017-11-29 11:47:10 +0100

    Add depth limit for nested findMany

--- src/middleware/action/limits.js
+++ src/middleware/action/limits.js
@@ -6,6 +6,8 @@ const { getWordsList } = require('../../utilities');
 const { throwError } = require('../../error');
 const { getLimits } = require('../../limits');
 
+const { getColl } = require('./get_coll');
+
 // Validate request limits
 const validateRequestLimits = function ({ runOpts, mInput }) {
   const limits = getLimits({ runOpts });
@@ -18,15 +20,47 @@ const validateMaxDepth = function ({ limits: { maxDepth }, actions }) {
     .filter(({ commandpath }) => commandpath.length > maxDepth);
   if (tooNestedActions.length === 0) { return; }
 
-  const paths = tooNestedActions
+  const message = `The maximum depth level for commands is ${maxDepth}`;
+  throwNestingError({ actions: tooNestedActions, message });
+};
+
+// Nested patch|create|upsert commands use `maxmodels` instead
+// Nested delete commands are not limited, as they are meant not to be performed
+// several times
+const validateNestedFind = function ({ limits, actions, top, schema }) {
+  if (top.command.type !== 'find') { return; }
+
+  const tooNestedActions = actions
+    .filter(action => isTooNestedFind({ action, schema, top, limits }));
+  if (tooNestedActions.length === 0) { return; }
+
+  const message = '\'find\' commands can only target collections at the top level or the second level.';
+  throwNestingError({ actions: tooNestedActions, message });
+};
+
+const isTooNestedFind = function ({
+  action: { commandpath },
+  schema: { shortcuts: { collsMap } },
+  top,
+  limits: { maxFindManyDepth },
+}) {
+  if (commandpath.length <= maxFindManyDepth) { return false; }
+
+  const { multiple } = getColl({ commandpath, collsMap, top });
+  return multiple;
+};
+
+const throwNestingError = function ({ actions, message }) {
+  const paths = actions
     .map(({ commandpath }) => commandpath.slice(1).join('.'));
   const pathsA = getWordsList(paths, { op: 'and', quotes: true });
-  const message = `The following ${pluralize('command', paths.length)} ${pluralize('is', paths.length)} nested too deeply: ${pathsA}. The maximum depth level for commands is ${maxDepth}`;
-  throwError(message, { reason: 'REQUEST_LIMIT' });
+  const messageA = `The following ${pluralize('command', paths.length)} ${pluralize('is', paths.length)} nested too deeply: ${pathsA}. ${message}`;
+  throwError(messageA, { reason: 'REQUEST_LIMIT' });
 };
 
 const validators = [
   validateMaxDepth,
+  validateNestedFind,
 ];
 
 module.exports = {
