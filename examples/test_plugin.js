3950e57e3c55505fbcdb368a0bfd6e471230423e
autoserver-founder <founder@autoserver.io>
2017-11-30 19:06:30 +0100

    Change how variables are prefixed

--- examples/test_plugin.js
+++ examples/test_plugin.js
@@ -5,7 +5,7 @@ const { promisify } = require('util');
 const testPlugin = async function ({ schema, opts: { example_option } }) {
   await promisify(process.nextTick)();
 
-  return { ...schema, __plugin_attr: example_option };
+  return { ...schema, $plugin_attr: example_option };
 };
 
 module.exports = testPlugin;
