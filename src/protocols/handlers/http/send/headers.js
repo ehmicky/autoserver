3c4a04ceae21e355d5962bb3c0fe1dc8e9c1a5d3
autoserver-founder <founder@autoserver.io>
2017-11-30 16:58:36 +0100

    Move and split files

--- src/protocols/handlers/http/send/headers.js
+++ src/protocols/handlers/http/send/headers.js
@@ -1,43 +1,9 @@
 'use strict';
 
-const { promisify } = require('util');
-
 const vary = require('vary');
 
-const { OBJECT_TYPES } = require('../../../constants');
-const { compressHandlers } = require('../../../compress');
-
-const { setStatusCode } = require('./status');
-
-// Sends response
-const send = async function ({
-  specific: { req, res },
-  content,
-  response: {
-    data = {},
-    metadata = {},
-  } = {},
-  type,
-  mime,
-  compressResponse,
-  reason,
-}) {
-  // `specific` might be undefined, if initial input was wrong.
-  if (!res) { return; }
-
-  // However, we are not sure a response has already been sent or not,
-  // so we must check to avoid double responses
-  if (res.finished) { return; }
-
-  setStatusCode({ res, reason });
-
-  setHeaders({ res, mime, compressResponse, content, type, data, metadata });
-
-  const sendResponse = promisify(res.end.bind(res));
-  await sendResponse(content);
-
-  cleanup({ req, res });
-};
+const { OBJECT_TYPES } = require('../../../../constants');
+const { compressHandlers } = require('../../../../compress');
 
 // Set HTTP-specific headers and status code
 const setHeaders = function ({
@@ -46,8 +12,10 @@ const setHeaders = function ({
   compressResponse,
   content,
   type,
-  data,
-  metadata: { duration },
+  response: {
+    data = {},
+    metadata: { duration } = {},
+  } = {},
 }) {
   // Should theoritically be calculated before `args.silent` is applied,
   // to follow HTTP spec for HEAD method.
@@ -107,16 +75,6 @@ const OBJECT_VARY_HEADERS = [
   'Accept',
 ];
 
-const cleanup = function ({ req, res }) {
-  // Otherwise, socket might not be freed, e.g. if an error was thrown before
-  // the request body was fully read
-  req.socket.destroy();
-
-  // Not sure if this needed
-  req.destroy();
-  res.destroy();
-};
-
 module.exports = {
-  send,
+  setHeaders,
 };
