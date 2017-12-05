f0291cdf25d276fffd8e9929e2935bae6f2eb710
ehmicky <ehmicky@gmail.com>
2017-12-05 16:13:14 +0100

    Allow several loggers at same time

--- src/events/log/main.js
+++ src/events/log/main.js
@@ -24,27 +24,38 @@ const reportLog = function ({
 
   consolePrint({ vars: logInfo, duration, isPerf });
 
-  return fireLogger({ schema, mInput, vars, logInfo, isPerf });
+  return fireLoggers({ schema, mInput, vars, logInfo, isPerf });
 };
 
 // Can filter verbosity with `schema.log.level`
 // This won't work for very early startup errors since `schema` is not
 // parsed yet.
-const shouldLog = function ({ schema: { log = {} }, vars: { level } }) {
-  return log.level !== 'silent' &&
+const shouldLog = function ({ schema: { log }, vars: { level } }) {
+  return log &&
+    log.level !== 'silent' &&
     LEVELS.indexOf(level) >= LEVELS.indexOf(log.level);
 };
 
+// Can fire several loggers at the same time
+const fireLoggers = function ({ schema: { log }, ...rest }) {
+  if (!Array.isArray(log)) {
+    return fireLogger({ ...rest, log });
+  }
+
+  const promises = log.map(logA => fireLogger({ ...rest, log: logA }));
+  return Promise.all(promises);
+};
+
 const fireLogger = function ({
-  schema: { log: { provider, opts = {} } = {} },
+  log: { provider, opts = {} },
   mInput,
   vars,
   vars: { measures, measuresmessage },
   logInfo,
   isPerf,
 }) {
-  const logger = getLogger({ provider });
-  const reportFunc = isPerf ? logger.reportPerf : logger.report;
+  const { reportPerf, report } = getLogger({ provider });
+  const reportFunc = isPerf ? reportPerf : report;
 
   if (reportFunc === undefined) { return; }
 
