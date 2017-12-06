7eadcc98150061cfa88ee8f2a44f8510e30080f0
ehmicky <ehmicky@gmail.com>
2017-12-06 17:43:38 +0100

    Improve clientCollname recognition

--- src/middleware/action/client_names.js
+++ src/middleware/action/client_names.js
@@ -12,14 +12,16 @@ const setClientNames = function ({
   commandpaths,
   collnames,
   summary,
+  top,
 }) {
   const actionsA = actions
-    .map(action => setClientNamesActions({ schema, action }));
+    .map(action => setClientNamesActions({ schema, action, top }));
   const clientVariables = getClientVariables({
     schema,
     collnames,
     commandpaths,
     summary,
+    top,
   });
 
   return { actions: actionsA, ...clientVariables };
@@ -29,9 +31,10 @@ const setClientNamesActions = function ({
   schema,
   action,
   action: { commandpath, collname },
+  top,
 }) {
-  const clientCollname = getClientCollname({ schema, collname });
-  const clientCommandpath = getClientCommandpath({ schema, commandpath });
+  const clientCollname = getClientCollname({ schema, collname, top });
+  const clientCommandpath = getClientCommandpath({ schema, commandpath, top });
   return { ...action, clientCollname, clientCommandpath };
 };
 
@@ -40,41 +43,50 @@ const getClientVariables = function ({
   collnames,
   commandpaths,
   summary,
+  top,
 }) {
   const clientCollnames = collnames
-    .map(collname => getClientCollname({ schema, collname }));
+    .map(collname => getClientCollname({ schema, collname, top }));
   const clientCommandpaths = commandpaths
-    .map(commandpath => getClientCommandpaths({ schema, commandpath }));
-  const clientSummary = getClientSummary({ schema, summary });
+    .map(commandpath => getClientCommandpaths({ schema, commandpath, top }));
+  const clientSummary = getClientSummary({ schema, summary, top });
 
   return { clientCollnames, clientCommandpaths, clientSummary };
 };
 
-const getClientCommandpaths = function ({ schema, commandpath }) {
+const getClientCommandpaths = function ({ schema, commandpath, top }) {
   const commandpathA = commandpath.split('.');
   const commandpathB = getClientCommandpath({
     schema,
     commandpath: commandpathA,
+    top,
   });
   const commandpathC = commandpathB.join('.');
   return commandpathC;
 };
 
-const getClientCommandpath = function ({ schema, commandpath }) {
+const getClientCommandpath = function ({ schema, commandpath, top }) {
   const [collname] = commandpath;
-  const clientCollname = getClientCollname({ schema, collname });
+  const clientCollname = getClientCollname({ schema, collname, top });
   const clientCommandpath = [clientCollname, ...commandpath.slice(1)];
   return clientCommandpath;
 };
 
-const getClientSummary = function ({ schema, summary }) {
+const getClientSummary = function ({ schema, summary, top }) {
   const [, collname, children] = SUMMARY_REGEXP.exec(summary);
-  const clientCollname = getClientCollname({ schema, collname });
+  const clientCollname = getClientCollname({ schema, collname, top });
   const clientSummary = `${clientCollname}${children}`;
   return clientSummary;
 };
 
-const getClientCollname = function ({ schema: { collections }, collname }) {
+const getClientCollname = function ({
+  schema: { collections },
+  collname,
+  top,
+}) {
+  // Reuse client-supplied collection name. Otherwise, use first available
+  if (top.collname === collname) { return top.clientCollname; }
+
   const [clientCollname] = collections[collname].name;
   return clientCollname;
 };
