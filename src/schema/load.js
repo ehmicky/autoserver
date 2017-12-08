753abbc7377cb7c9e8a93864613503e6d9c4555e
ehmicky <ehmicky@gmail.com>
2017-12-08 10:47:29 +0100

    Refactoring

--- src/schema/load.js
+++ src/schema/load.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const { dereferenceSchema } = require('../json_refs');
+const { dereferenceRefs } = require('../json_refs');
 
 const { normalizeSchema } = require('./normalize');
 
 // Load schema, using its path
 const loadSchema = async function ({ runOpts: { schema } }) {
   // Resolve JSON references
-  const schemaA = await dereferenceSchema({ schema });
+  const schemaA = await dereferenceRefs({ path: schema });
   // Normalize schema, and validate it
   const schemaB = await normalizeSchema({ schema: schemaA });
   return { schema: schemaB };
