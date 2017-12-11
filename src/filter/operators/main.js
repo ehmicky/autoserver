ffe4495c84a8e780ce1af9478d4e81aa530a11a4
ehmicky <ehmicky@gmail.com>
2017-12-11 18:01:18 +0100

    Fix linting

--- src/filter/operators/main.js
+++ src/filter/operators/main.js
@@ -1,27 +1,19 @@
 'use strict';
 
-const { _or, _and } = require('./or_and');
-const { _some, _all } = require('./some_all');
-const { _eq, _neq } = require('./eq_neq');
-const { _lt, _gt, _lte, _gte } = require('./lt_gt_lte_gte');
-const { _in, _nin } = require('./in_nin');
-const { _like, _nlike } = require('./like_nlike');
+const orAnd = require('./or_and');
+const someAll = require('./some_all');
+const eqNeq = require('./eq_neq');
+const ltGtLteGte = require('./lt_gt_lte_gte');
+const inNin = require('./in_nin');
+const likeNlike = require('./like_nlike');
 
 const operators = {
-  _or,
-  _and,
-  _some,
-  _all,
-  _eq,
-  _neq,
-  _lt,
-  _gt,
-  _lte,
-  _gte,
-  _in,
-  _nin,
-  _like,
-  _nlike,
+  ...orAnd,
+  ...someAll,
+  ...eqNeq,
+  ...ltGtLteGte,
+  ...inNin,
+  ...likeNlike,
 };
 
 const getOperator = function ({ node }) {
