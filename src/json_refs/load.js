716133eb4526421436933ebcc48bce717af3c86c
ehmicky <ehmicky@gmail.com>
2017-12-12 11:59:56 +0100

    Better error messages

--- src/json_refs/load.js
+++ src/json_refs/load.js
@@ -1,18 +1,26 @@
 'use strict';
 
+const { pStat } = require('../utilities');
 const { addGenErrorHandler } = require('../error');
 const { loadFile } = require('../formats');
 
 // Load the file pointing to by the JSON reference
-const load = function ({ path }) {
-  return loadFile({ type: 'conf', path });
+const load = async function ({ path }) {
+  await eStat(path);
+
+  return eLoadFile({ type: 'conf', path });
 };
 
-const eLoad = addGenErrorHandler(load, {
-  message: ({ path }) => `Configuration file '${path}' is invalid: it does not exist or it has syntax errors`,
+const eLoadFile = addGenErrorHandler(loadFile, {
+  message: ({ path }) => `Configuration file could not be parsed because it has syntax errors: '${path}'`,
+  reason: 'SCHEMA_VALIDATION',
+});
+
+const eStat = addGenErrorHandler(pStat, {
+  message: path => `Configuration file does not exist: '${path}'`,
   reason: 'SCHEMA_VALIDATION',
 });
 
 module.exports = {
-  load: eLoad,
+  load,
 };
