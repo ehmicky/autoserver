d0ebcd5b24b47d6cda51679a69f61177a0bbfb64
ehmicky <ehmicky@gmail.com>
2017-12-18 10:34:39 +0100

    Refactoring

--- src/json_validation/validator.js
+++ src/json_validation/validator.js
@@ -6,7 +6,9 @@ const ajvKeywords = require('ajv-keywords');
 const getValidator = function () {
   const ajv = new Ajv(AJV_OPTIONS);
 
-  // Add future JSON standard keywords
+  // Add JSON keywords:
+  //  - typeof: allows checking for `typeof function`
+  //  - if|then|else: future JSON schema keyword
   ajvKeywords(ajv, ['if', 'typeof']);
 
   return ajv;
