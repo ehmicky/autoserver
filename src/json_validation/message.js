b54e989c5cdf2c6b6f9f2f47eb9e29df53d97559
ehmicky <ehmicky@gmail.com>
2017-12-18 12:10:17 +0100

    Refactoring

--- src/json_validation/message.js
+++ src/json_validation/message.js
@@ -3,17 +3,30 @@
 const { errorMessages } = require('./messages');
 
 // Customize error messages when the library's ones are unclear
-const getErrorMessage = function ({ error, hasInputPath }) {
-  const customErrorMessage = errorMessages[error.keyword];
-  // Failsafe
-  if (!customErrorMessage) { return ` ${error.message}`; }
-  const message = customErrorMessage(error);
+const getErrorMessage = function ({
+  error,
+  error: { keyword, message },
+  path,
+}) {
+  const getMessage = errorMessages[keyword];
 
-  if (!hasInputPath && message[0] === '.') {
-    return message.substring(1);
+  // Failsafe
+  if (getMessage === undefined) {
+    return ` ${message}`;
   }
 
-  return message;
+  const messageA = getMessage(error);
+
+  const messageB = removeLeadingDot({ path, message: messageA });
+
+  return messageB;
+};
+
+// When error message starts with `.var` but no `data` was prefixed, remove `.`
+const removeLeadingDot = function ({ path, message }) {
+  if (path !== '' || message[0] !== '.') { return message; }
+
+  return message.substring(1);
 };
 
 module.exports = {
--- src/json_validation/validate.js
+++ src/json_validation/validate.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const { flatten } = require('../utilities');
-
 const { reportErrors } = require('./report_error');
 
 // Perform a validation, using a JSON schema, and a `data` as input
@@ -9,23 +7,21 @@ const validate = function ({
   compiledJsonSchema,
   data,
   dataVar,
-  reason,
   message,
+  reason,
   extra = {},
 }) {
   // Hack to be able to pass information to custom validation keywords
   const dataA = { ...data, [Symbol.for('extra')]: extra };
 
   const isValid = compiledJsonSchema(dataA);
-  if (isValid) { return; }
 
   const { errors } = compiledJsonSchema;
-  const errorsA = flatten(errors);
-  const errorsB = errorsA.filter(val => val);
+  const hasErrors = Array.isArray(errors) && errors.length > 0;
 
-  if (errorsB.length === 0) { return; }
+  if (isValid || !hasErrors) { return; }
 
-  reportErrors({ errors: errorsB, dataVar, reason, message });
+  reportErrors({ errors, dataVar, message, reason });
 };
 
 module.exports = {
