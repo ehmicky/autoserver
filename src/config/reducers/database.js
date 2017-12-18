de9ce0da0e75ceceb877486314541d16fc23a56e
ehmicky <ehmicky@gmail.com>
2017-12-18 16:15:13 +0100

    Add database options validation

--- src/config/reducers/database.js
+++ src/config/reducers/database.js
@@ -3,19 +3,28 @@
 const { throwError } = require('../../error');
 const { databaseAdapters } = require('../../database');
 const { mapColls } = require('../helpers');
+const { compile, validate } = require('../../validation');
 
 const { validateFeatures } = require('./features');
 
-// Validates `collection.database`
+// Validates `collection.database` and `databases.DATABASE.*`
+const validateDatabases = function ({ config, config: { databases } }) {
+  Object.entries(databases)
+    .forEach(([name, opts]) => validateOpts({ name, opts }));
+
+  const { collections } = mapColls(mapColl, { config });
+  return { collections };
+};
+
 const mapColl = function ({ coll: { database }, coll, collname }) {
-  const adapter = getAdapter({ name: database, collname });
+  const adapter = getCollAdapter({ name: database, collname });
   validateFeatures({ adapter, coll, collname });
 
   const { features } = adapter;
   return { features };
 };
 
-const getAdapter = function ({ name, collname }) {
+const getCollAdapter = function ({ name, collname }) {
   const adapter = databaseAdapters[name];
   if (adapter !== undefined) { return adapter; }
 
@@ -23,7 +32,27 @@ const getAdapter = function ({ name, collname }) {
   throwError(message, { reason: 'CONF_VALIDATION' });
 };
 
-const validateDatabases = mapColls.bind(null, mapColl);
+// Validates `database.DATABASE.*` according to `databaseAdapter.opts`
+const validateOpts = function ({ name, opts }) {
+  const { opts: jsonSchema } = getOptsAdapter({ name });
+  const compiledJsonSchema = compile({ jsonSchema });
+
+  validate({
+    compiledJsonSchema,
+    data: opts,
+    dataVar: `databases.${name}`,
+    reason: 'CONF_VALIDATION',
+    message: 'Wrong configuration',
+  });
+};
+
+const getOptsAdapter = function ({ name }) {
+  const adapter = databaseAdapters[name];
+  if (adapter !== undefined) { return adapter; }
+
+  const message = `'databases.${name}' is unknown`;
+  throwError(message, { reason: 'CONF_VALIDATION' });
+};
 
 module.exports = {
   validateDatabases,
