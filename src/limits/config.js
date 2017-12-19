cb1c8b29fc49902806baf89a278c56ccf429340c
ehmicky <ehmicky@gmail.com>
2017-12-19 12:23:28 +0100

    Refactor /src/limits/

--- src/limits/config.js
+++ src/limits/config.js
@@ -2,16 +2,7 @@
 
 const bytes = require('bytes');
 
-// Returns the main numerical limits of the engine.
-// Some of those limits cannot be changed by the user.
-const getLimits = function ({ config } = {}) {
-  const configLimits = getConfigLimits({ config });
-
-  return {
-    ...SYSTEM_LIMITS,
-    ...configLimits,
-  };
-};
+const { maxUrlLength } = require('./system');
 
 // Limits that can be changed in `config.limits`
 const getConfigLimits = function ({ config }) {
@@ -37,7 +28,7 @@ const getConfigLimits = function ({ config }) {
     // Max URL length
     // Since URL can contain GraphQL query, it should not be less than
     // `maxpayload`
-    maxUrlLength: Math.max(MAX_URL_LENGTH, maxpayload),
+    maxUrlLength: Math.max(maxUrlLength, maxpayload),
   };
 };
 
@@ -63,48 +54,8 @@ const getMaxmodels = function ({ limits: { maxmodels }, pagesize }) {
   return maxmodels;
 };
 
-const MAX_ACTIONS = 51;
-
 const MAX_MODELS_FACTOR = 1e2;
 
-const MAX_FIND_MANY_DEPTH = 2;
-
-const MAX_ATTR_VALUE_SIZE = 2e3;
-
-const MAX_URL_LENGTH = 2e3;
-
-const MAX_QUERY_STRING_DEPTH = 1e1;
-
-const MAX_QUERY_STRING_LENGTH = 1e2;
-
-const MIN_MAXPAYLOAD = 1e2;
-
-const REQUEST_TIMEOUT = 5e3;
-
-// Limits that cannot be changed in `config.limits`
-const SYSTEM_LIMITS = {
-  // Max number of actions (including top level)
-  maxActions: MAX_ACTIONS,
-  // Max depth of findMany nested actions
-  maxFindManyDepth: MAX_FIND_MANY_DEPTH,
-  // Max size of an attribute's value, in bytes.
-  maxAttrValueSize: MAX_ATTR_VALUE_SIZE,
-
-  // Minimum value for maxpayload
-  minMaxpayload: MIN_MAXPAYLOAD,
-  // Max level of nesting in query string, e.g. ?var.subvar.subvar2=val
-  maxQueryStringDepth: MAX_QUERY_STRING_DEPTH,
-  // Max length of arrays in query string, e.g. ?var[50]=val
-  maxQueryStringLength: MAX_QUERY_STRING_LENGTH,
-
-  // How long the request can run, in milliseconds
-  requestTimeout: REQUEST_TIMEOUT,
-
-  // Enforced during config validation:
-  //  - max number of attributes per model: 50
-  //  - max model|attribute name length: 200
-};
-
 module.exports = {
-  getLimits,
+  getConfigLimits,
 };
