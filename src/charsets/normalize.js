eecadb548561a76dd490f218fb5312c7a353cd28
ehmicky <ehmicky@gmail.com>
2017-12-19 14:29:55 +0100

    Refactoring

--- src/charsets/normalize.js
+++ src/charsets/normalize.js
@@ -14,11 +14,9 @@ const normalizeCharset = function ({ charset, format }) {
   return charsetB;
 };
 
+// Add default charsets, including the format's default charset
 const addDefaultCharset = function ({ charset, format: { charsets = [] } }) {
-  return charset ||
-    // Charset specified by this format
-    charsets[0] ||
-    DEFAULT_INPUT_CHARSET;
+  return charset || charsets[0] || DEFAULT_INPUT_CHARSET;
 };
 
 module.exports = {
