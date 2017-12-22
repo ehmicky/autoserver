d208e7d268df2dc653c4792101406f256ce7ba88
autoserver-founder <founder@autoserver.io>
2017-12-22 14:36:10 +0100

    Improve debug log provider for perf measures

--- src/perf/stringify.js
+++ src/perf/stringify.js
@@ -46,21 +46,38 @@ const stringifyMeasure = function (
   const phaseS = phase.padEnd(LENGTHS.phase);
   const categoryS = category.padEnd(LENGTHS.category);
   const labelS = label.padEnd(LENGTHS.label);
-  const durationS = `${Math.round(duration)}ms`.padEnd(LENGTHS.duration);
-  const averageS = `${Math.round(average)}ms`.padEnd(LENGTHS.average);
+  const durationS = formatDuration(duration).padStart(LENGTHS.duration);
+  const averageS = formatDuration(average).padStart(LENGTHS.average);
   const items = count === 1 ? 'item' : 'items';
   const countS = `${String(count).padStart(LENGTHS.count)} ${items}`;
 
   return `${phaseS} ${categoryS} ${labelS} ${durationS} = ${averageS} * ${countS}`;
 };
 
+const formatDuration = function (duration) {
+  const durationA = Math.round(duration * DECIMALS_EXP) / DECIMALS_EXP;
+
+  const durationB = String(durationA);
+  const [, integer, decimals] = DECIMALS_REGEXP.exec(durationB);
+  const decimalsA = decimals.padEnd(DECIMALS_COUNT).replace(/ /g, '0');
+
+  const durationC = `${integer}.${decimalsA}ms`;
+  return durationC;
+};
+
+// '1.2345' -> '1', '2345'
+const DECIMALS_REGEXP = /(\d*)\.?(\d*)/;
+
+const DECIMALS_COUNT = 2;
+const DECIMALS_EXP = 1e2;
+
 // How wide each column is
 const LENGTHS = {
   phase: 8,
   category: 10,
   label: 26,
-  duration: 8,
-  average: 7,
+  duration: 11,
+  average: 10,
   count: 3,
 };
 
