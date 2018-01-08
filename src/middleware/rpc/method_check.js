826f81b5cf4e2337e406826893d4cb9739c976c1
autoserver-founder <founder@autoserver.io>
2018-01-08 11:29:00 +0100

    Refactoring

--- src/middleware/rpc/method_check.js
+++ src/middleware/rpc/method_check.js
@@ -4,16 +4,19 @@ const { throwError } = require('../../errors');
 
 // Check if protocol method is allowed for current rpc
 const methodCheck = function ({ rpcAdapter: { methods, title }, method }) {
-  const allowedMethod = methods === undefined ||
-    methods.includes(method) ||
-    // If only method is allowed by the rpc, but the protocol does not have
-    // a getMethod(), we do not force specifying `method`
-    (methods.length === 1 && method === undefined);
-  if (allowedMethod) { return; }
+  if (isAllowedMethod({ methods, method })) { return; }
 
   const message = `Protocol method '${method}' is not allowed with ${title}`;
   const allowedMethods = methods;
   throwError(message, { reason: 'WRONG_METHOD', extra: { allowedMethods } });
+};
+
+const isAllowedMethod = function ({ methods, method }) {
+  return methods === undefined ||
+    methods.includes(method) ||
+    // If only method is allowed by the rpc, but the protocol does not have
+    // a getMethod(), we do not force specifying `method`
+    (methods.length === 1 && method === undefined);
 };
 
 module.exports = {
