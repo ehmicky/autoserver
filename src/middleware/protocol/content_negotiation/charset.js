96ebde08a6b00a3e544a988d364d0a1ee7f392f0
ehmicky <ehmicky@gmail.com>
2018-01-08 12:16:12 +0100

    Refactors errors in charsets handler

--- src/middleware/protocol/content_negotiation/charset.js
+++ src/middleware/protocol/content_negotiation/charset.js
@@ -1,14 +1,21 @@
 'use strict';
 
+const { addGenErrorHandler } = require('../../../errors');
 const { normalizeCharset } = require('../../../charsets');
 
 // Retrieve charset asked by client for the request and response payload
 const getCharset = function ({ queryvars, charset, format }) {
   // E.g. ?charset query variable or charset in Content-Type HTTP header
   const charsetA = queryvars.charset || charset;
-  const charsetB = normalizeCharset({ charset: charsetA, format });
+  const charsetB = eNormalizeCharset({ charset: charsetA, format });
   return charsetB;
 };
+
+const eNormalizeCharset = addGenErrorHandler(normalizeCharset, {
+  message: ({ charset }, { message }) =>
+    message || `Unsupported charset: '${charset}'`,
+  reason: 'RESPONSE_FORMAT',
+});
 
 module.exports = {
   getCharset,
