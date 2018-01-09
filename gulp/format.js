b75ad45911304045f18bcda36435773b78f558d4
autoserver-founder <founder@autoserver.io>
2018-01-09 16:05:15 +0100

    Fix gulp format task

--- gulp/format.js
+++ gulp/format.js
@@ -5,31 +5,17 @@ const { Buffer } = require('buffer');
 const through = require('through2');
 const { PluginError, replaceExtension } = require('gulp-util');
 
-const {
-  formatExists,
-  findByExt,
-  getExtName,
-  parse,
-  serialize,
 // eslint-disable-next-line import/no-internal-modules
-} = require('../src/formats');
+const { getFormat, getByExt } = require('../src/formats');
 
 // Convert several file formats towards JSON (or any other format)
-const convertFormat = function ({ format: destFormat = 'json' } = {}) {
-  validateFormat({ format: destFormat });
+const convertFormat = function ({ format = 'json' } = {}) {
+  const destFormat = getFormat(format);
 
   return through.obj(function convert (file, encoding, done) {
     // eslint-disable-next-line no-invalid-this, fp/no-this
     return convertFile({ file, destFormat, gulp: this, done });
   });
-};
-
-const validateFormat = function ({ format }) {
-  if (formatExists({ format })) { return; }
-
-  const error = `Format '${format}' does not exit`;
-  // eslint-disable-next-line fp/no-throw
-  throw error;
 };
 
 const convertFile = function ({ file, destFormat, gulp, done }) {
@@ -70,7 +56,7 @@ const emitError = function ({ gulp, error }) {
 const PLUGIN_NAME = 'gulp-format';
 
 const convertContent = function ({ file, file: { path }, destFormat }) {
-  const srcFormat = findByExt({ path });
+  const srcFormat = getByExt({ path });
 
   const contents = getContents({ srcFormat, destFormat, file });
   const pathA = getPath({ destFormat, path });
@@ -79,22 +65,22 @@ const convertContent = function ({ file, file: { path }, destFormat }) {
   Object.assign(file, { contents, path: pathA });
 };
 
-const getPath = function ({ destFormat, path }) {
-  const extName = getExtName({ format: destFormat });
-  const pathA = replaceExtension(path, extName);
-  return pathA;
-};
-
 const getContents = function ({
   srcFormat,
   destFormat,
   file: { contents, path },
 }) {
   const content = contents.toString();
-  const contentA = parse({ content, format: srcFormat, path });
-  const contentB = serialize({ content: contentA, format: destFormat });
+  const contentA = srcFormat.parseContent(content, { path });
+  const contentB = destFormat.serializeContent(contentA);
   const contentC = Buffer.from(contentB);
   return contentC;
+};
+
+const getPath = function ({ destFormat, path }) {
+  const extension = destFormat.getExtension();
+  const pathA = replaceExtension(path, extension);
+  return pathA;
 };
 
 module.exports = {
