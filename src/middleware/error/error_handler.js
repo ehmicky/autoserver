702efa6300ad3a8e38c7a61930178c0ed68a8569
ehmicky <ehmicky@gmail.com>
2018-01-19 13:33:04 +0100

    Fix error handler not being called on final layer exception

--- src/middleware/error/error_handler.js
+++ src/middleware/error/error_handler.js
@@ -8,11 +8,7 @@ const errorHandler = async function ({
   protocolAdapter,
   config,
   mInput,
-  mInput: { status } = {},
 }) {
-  // Only fire main error handler on server-side errors
-  if (status && status !== 'SERVER_ERROR') { return; }
-
   // Make sure a response is sent, even empty, or the socket will hang
   await protocolAdapter.send({ content: '', contentLength: 0 });
 
