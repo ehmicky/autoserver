70120757106657121a14ef4fe6f2c02dc463f3e6
autoserver-founder <founder@autoserver.io>
2018-01-19 12:44:59 +0100

    Improve PLUGIN error reason

--- src/errors/reasons/plugin.js
+++ src/errors/reasons/plugin.js
@@ -1,10 +1,18 @@
 'use strict';
 
+const getMessage = function ({ plugin }) {
+  if (plugin === undefined) { return {}; }
+
+  const message = `In the plugin '${plugin}'`;
+  return { message, extra: { plugin } };
+};
+
 // Extra:
-//  - plugin STR
+//  - plugin `{string}`
 const PLUGIN = {
   status: 'SERVER_ERROR',
   title: 'Internal error related to a specific plugin',
+  getMessage,
 };
 
 module.exports = {
