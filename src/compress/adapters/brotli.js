9e8ff285dccd75fd0c2b4a964d3b0be6f3af75d5
autoserver-founder <founder@autoserver.io>
2018-04-09 13:33:26 +0200

    Upgrade iltorb 2.2.0 -> 2.3.0

--- src/compress/adapters/brotli.js
+++ src/compress/adapters/brotli.js
@@ -1,23 +1,18 @@
 'use strict';
 
-const { promisify } = require('util');
-
 const {
   compress: brotliCompress,
   decompress: brotliDecompress,
 } = require('iltorb');
 
-const pBrotliCompress = promisify(brotliCompress);
-const pBrotliDecompress = promisify(brotliDecompress);
-
 // Compress to Brotli
 const compress = function (content) {
-  return pBrotliCompress(content);
+  return brotliCompress(content);
 };
 
 // Decompress from Brotli
 const decompress = function (content) {
-  return pBrotliDecompress(content);
+  return brotliDecompress(content);
 };
 
 module.exports = {
