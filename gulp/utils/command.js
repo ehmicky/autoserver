890e03aafbf2fefc6d27c2d7d0d54479b0285f70
ehmicky <ehmicky@gmail.com>
2018-04-12 18:11:22 +0200

    Fix ./node_modules/.bin not being in $PATH for Gulp

--- gulp/utils/command.js
+++ gulp/utils/command.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { env } = require('process');
 const { spawn } = require('child_process');
 
 const PluginError = require('plugin-error');
@@ -7,12 +8,30 @@ const PluginError = require('plugin-error');
 // Execute a shell command
 const execCommand = function (command) {
   const [commandA, ...args] = command.trim().split(/ +/);
-  const child = spawn(commandA, args, { stdio: 'inherit' });
+  const envA = getEnv();
+  const child = spawn(commandA, args, { stdio: 'inherit', env: envA });
 
   // eslint-disable-next-line promise/avoid-new
   return new Promise(execCommandPromise.bind(null, child));
 };
 
+// Adds local Node modules binary to `$PATH`
+const getEnv = function () {
+  const PATH = getPath({ env });
+  const envA = { ...env, PATH };
+  return envA;
+};
+
+const getPath = function ({ env: { PATH = '' } }) {
+  const hasLocalDir = PATH.split(':').includes(LOCAL_NODE_BIN_DIR);
+  if (hasLocalDir) { return PATH; }
+
+  return `${PATH}:${LOCAL_NODE_BIN_DIR}`;
+};
+
+const LOCAL_NODE_BIN_DIR = './node_modules/.bin/';
+
+// Check command exit code
 const execCommandPromise = function (child, resolve, reject) {
   child.on('exit', execCommandExit.bind(null, resolve, reject));
 };
