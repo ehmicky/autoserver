a096485b20b659b89396fed03fc6db598827d082
ehmicky <ehmicky@gmail.com>
2018-09-01 15:50:32 +0200

    Fix validationOut error handling

--- src/middleware/action/validation_out.js
+++ src/middleware/action/validation_out.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { throwError } = require('../../errors')
+const { throwPb } = require('../../errors')
 const { CONTENT_TYPES } = require('../../content_types')
 
 // Action layer output validation
@@ -13,14 +13,14 @@ const actionValidationOut = function ({ response: { content, type } }) {
 const validateType = function ({ type }) {
   if (typeof type !== 'string') {
     const message = `'type' must be a string, not '${type}'`
-    throwError(message, { reason: 'ENGINE' })
+    throwPb({ message, reason: 'ENGINE' })
   }
 
   const isWrongType = CONTENT_TYPES[type] === undefined
 
   if (isWrongType) {
     const message = `Invalid 'type': '${type}'`
-    throwError(message, { reason: 'ENGINE' })
+    throwPb({ message, reason: 'ENGINE' })
   }
 }
 
@@ -29,7 +29,7 @@ const validateContent = function ({ content, type }) {
   if (isRightContent) { return }
 
   const message = `Invalid 'content' of type '${type}': '${content}'`
-  throwError(message, { reason: 'ENGINE' })
+  throwPb({ message, reason: 'ENGINE' })
 }
 
 module.exports = {
