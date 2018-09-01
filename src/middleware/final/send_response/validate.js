c2e97f8d3db5cf6a23832e2df82dfb7303fa0bcd
ehmicky <ehmicky@gmail.com>
2018-09-01 15:48:23 +0200

    Fix response validation error handling

--- src/middleware/final/send_response/validate.js
+++ src/middleware/final/send_response/validate.js
@@ -1,23 +1,23 @@
 'use strict'
 
-const { throwError } = require('../../../errors')
+const { throwPb } = require('../../../errors')
 
 const { TYPES } = require('./types')
 
 const validateResponse = function ({ response: { type, content } }) {
   if (!type) {
     const message = 'Server sent an response with no content type'
-    throwError(message, { reason: 'ENGINE' })
+    throwPb({ message, reason: 'ENGINE' })
   }
 
   if (content === undefined) {
     const message = 'Server sent an empty response'
-    throwError(message, { reason: 'ENGINE' })
+    throwPb({ message, reason: 'ENGINE' })
   }
 
   if (TYPES[type] === undefined) {
     const message = 'Server tried to respond with an unsupported content type'
-    throwError(message, { reason: 'ENGINE' })
+    throwPb({ message, reason: 'ENGINE' })
   }
 }
 
