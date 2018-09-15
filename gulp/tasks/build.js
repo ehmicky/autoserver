722ccdeef243c0a5f3ef66dd297f690c9bcf857a
ehmicky <ehmicky@gmail.com>
2018-09-15 15:17:10 +0200

    Add `gulp format`

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -4,23 +4,45 @@ const { src, dest, parallel } = require('gulp')
 const yamlToJson = require('gulp-yaml')
 
 const FILES = require('../files')
-const { getWatchTask } = require('../utils')
+const { execCommand, getWatchTask } = require('../utils')
 
 const format = function() {
+  const files = [
+    ...FILES.JAVASCRIPT,
+    ...FILES.MARKDOWN,
+    ...FILES.JSON,
+    ...FILES.YAML,
+  ].join(' ')
+  return execCommand(`prettier --write --loglevel warn ${files}`)
+}
+
+// eslint-disable-next-line fp/no-mutation
+format.description = 'Format files using prettier'
+
+const yaml = function() {
   return src(FILES.YAML_TO_JSON)
     .pipe(yamlToJson({ schema: 'JSON_SCHEMA', space: 2 }))
     .pipe(dest(({ base }) => base))
 }
 
 // eslint-disable-next-line fp/no-mutation
-format.description = 'Convert YAML files to JSON'
+yaml.description = 'Convert YAML files to JSON'
 
-const build = parallel(format)
+const build = parallel(format, yaml)
 
 // eslint-disable-next-line fp/no-mutation
 build.description = 'Build the application'
 
-const buildwatch = getWatchTask({ YAML_TO_JSON: format }, build)
+const buildwatch = getWatchTask(
+  {
+    JAVASCRIPT: format,
+    MARKDOWN: format,
+    JSON: format,
+    YAML: format,
+    YAML_TO_JSON: yaml,
+  },
+  build,
+)
 
 // eslint-disable-next-line fp/no-mutation
 buildwatch.description = 'Build the application in watch mode'
@@ -29,4 +51,5 @@ module.exports = {
   build,
   buildwatch,
   format,
+  yaml,
 }
