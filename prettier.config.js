5f9ccae00acd8091c426c4ef1178106d2fbff8b7
ehmicky <ehmicky@gmail.com>
2018-09-21 17:00:00 +0200

    Upgrade eslint-config-fp 0.5.2 -> 0.6.1

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line filenames/match-regex, filenames/match-exported
+'use strict'
+
+// eslint-disable-next-line import/no-internal-modules
+module.exports = require('eslint-config-fp/prettier.config')
