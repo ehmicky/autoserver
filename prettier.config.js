f265f565f99790534f7ec2730201f59eda5e8ce7
ehmicky <ehmicky@gmail.com>
2018-09-21 17:00:00 +0200

    Symlink .prettierrc.yml

--- prettier.config.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line filenames/match-regex
-'use strict'
-
-// eslint-disable-next-line import/no-internal-modules, node/no-unpublished-require
-module.exports = require('eslint-config-fp/prettier.config')
