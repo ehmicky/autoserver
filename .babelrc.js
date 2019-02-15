9627c57245ccd1a411004b3e25bfa4304061794a
ehmicky <ehmicky@gmail.com>
2019-02-15 10:00:00 +0100

    Add babelrc

---
+++ .babelrc.js
@@ -0,0 +1,16 @@
+// istanbul ignore file
+'use strict'
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: { node: '6.5.0' },
+        useBuiltIns: 'usage',
+      },
+    ],
+  ],
+  plugins: ['@babel/plugin-transform-runtime'],
+  shouldPrintComment: comment => comment.includes('istanbul ignore'),
+}
