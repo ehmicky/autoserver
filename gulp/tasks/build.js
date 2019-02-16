ecb0bba2c73877692931cdeb9ef7cd280656986e
ehmicky <ehmicky@gmail.com>
2019-02-16 10:00:00 +0100

    Add gulp build

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,26 +1,40 @@
 'use strict'
 
-const { src, dest, parallel } = require('gulp')
+const { src, dest, series, parallel } = require('gulp')
+const { include } = require('gulp-ignore')
+const del = require('del')
 const yamlToJson = require('gulp-yaml')
 
-const FILES = require('../files')
+const { BUILD, BUILD_DIST } = require('../files')
 const { getWatchTask } = require('../utils')
+const gulpExeca = require('../exec')
 
-const yaml = function() {
-  return src(FILES.YAML_TO_JSON)
+const clean = () => del(BUILD_DIST)
+
+const copy = () =>
+  src([`${BUILD}/**`, `!${BUILD}/**/*.{y{,a}ml,js,ts,jsx,tsx}`], { dot: true })
+    .pipe(dest(BUILD_DIST))
+
+const babel = () =>
+  gulpExeca(
+    `babel ${BUILD} --out-dir ${BUILD_DIST} --source-maps --no-comments --minified --retain-lines`,
+  )
+
+const yaml = () =>
+  src(`${BUILD}/**`, { dot: true })
+    .pipe(include(/\.ya?ml$/))
     .pipe(yamlToJson({ schema: 'JSON_SCHEMA', space: 2 }))
-    .pipe(dest(({ base }) => base))
-}
+    .pipe(dest(BUILD_DIST))
 
 // eslint-disable-next-line fp/no-mutation
 yaml.description = 'Convert YAML files to JSON'
 
-const build = parallel(yaml)
+const build = series(clean, parallel(copy, babel, yaml))
 
 // eslint-disable-next-line fp/no-mutation
 build.description = 'Build the application'
 
-const buildwatch = getWatchTask({ YAML_TO_JSON: yaml }, build)
+const buildwatch = getWatchTask({ BUILD: build }, build)
 
 // eslint-disable-next-line fp/no-mutation
 buildwatch.description = 'Build the application in watch mode'
@@ -28,5 +42,4 @@ buildwatch.description = 'Build the application in watch mode'
 module.exports = {
   build,
   buildwatch,
-  yaml,
 }
