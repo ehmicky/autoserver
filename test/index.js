2e1898865b3a0cd4e468f91a9ce231452a5858e3
ehmicky <ehmicky@gmail.com>
2019-02-17 10:00:00 +0100

    Add smoke tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,42 @@
 'use strict'
 
-const test = require('ava')
+const { chdir } = require('process')
 
-test('Dummy test', t => {
+const test = require('ava')
+const execa = require('execa')
+const promisify = require('util.promisify')
+
+const pSetTimeout = promisify(setTimeout)
+
+const BINARY_PATH = `${__dirname}/../bin/autoserver.js`
+const EXAMPLE_DIR = `${__dirname}/../examples`
+
+chdir(EXAMPLE_DIR)
+
+test('Smoke test', async t => {
+  const childProcess = execa(BINARY_PATH, { env: { NODE_ENV: 'dev' } })
+  await pSetTimeout(TEST_TIMEOUT)
+  childProcess.kill('SIGINT')
+  const { code, stdout, stderr } = await childProcess
+  const message = normalizeStdout({ stdout })
+  t.snapshot({ code, message, stderr })
   t.pass()
 })
+
+const normalizeStdout = function({ stdout }) {
+  // eslint-disable-next-line fp/no-mutating-methods
+  return stdout
+    .split('\n')
+    .map(normalizeLine)
+    .sort()
+    .join('\n')
+}
+
+const normalizeLine = function(line) {
+  return line.replace(START_LINE_REGEXP, '').replace(PORT_REGEXP, '$1')
+}
+
+const START_LINE_REGEXP = /^.{98}/u
+const PORT_REGEXP = /(Listening on).*/u
+
+const TEST_TIMEOUT = 5e3
