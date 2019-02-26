ea4cfb349b82054d84db640274029909a362c136
ehmicky <ehmicky@gmail.com>
2019-02-26 10:00:00 +0100

    Use mimic-fn

--- src/utils/functional/keep_props.js
+++ src/utils/functional/keep_props.js
@@ -1,26 +1,14 @@
 'use strict'
 
+const mimicFn = require('mimic-fn')
+
 // Wraps a functor so it does not modify a function `name`, `length`, etc.
 const keepProps = function(functor) {
   return (originalFunc, ...args) => {
     const wrappedFunc = functor(originalFunc, ...args)
-
-    copyProperties({ originalFunc, wrappedFunc })
-
+    mimicFn(wrappedFunc, originalFunc)
     return wrappedFunc
   }
-}
-
-const copyProperties = function({ originalFunc, wrappedFunc }) {
-  Reflect.ownKeys(originalFunc).forEach(propName =>
-    copyProperty({ originalFunc, wrappedFunc, propName }),
-  )
-}
-
-const copyProperty = function({ originalFunc, wrappedFunc, propName }) {
-  const prop = Object.getOwnPropertyDescriptor(originalFunc, propName)
-  // eslint-disable-next-line fp/no-mutating-methods
-  Object.defineProperty(wrappedFunc, propName, prop)
 }
 
 module.exports = {
