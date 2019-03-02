2a169c9106ccc24ce69bb8eb4ea25b3f4b1b2fa3
ehmicky <ehmicky@gmail.com>
2019-03-02 14:41:34 +0100

    Rename dist to build

--- gulp/run.js
+++ gulp/run.js
@@ -4,13 +4,13 @@ const { promisify } = require('util')
 
 const Nodemon = require('nodemon')
 // eslint-disable-next-line import/no-internal-modules
-const { exec } = require('gulp-shared-tasks/dist/src/exec')
+const { exec } = require('gulp-shared-tasks/build/src/exec')
 
 const EXAMPLE_PATH = `${__dirname}/../examples/index.js`
-const SRC_PATH = `${__dirname}/../dist/src`
+const SRC_PATH = `${__dirname}/../build/src`
 
 // We use this instead of requiring the application to test the CLI
-const runProd = () => exec('node ../dist/src/bin/index.js', { cwd: 'examples' })
+const runProd = () => exec('node ../build/src/bin/index.js', { cwd: 'examples' })
 
 // eslint-disable-next-line fp/no-mutation
 runProd.description = 'Run an example production server'
