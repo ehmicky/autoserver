d30fab9b5883fa97540d3280609c689c5452210a
ehmicky <ehmicky@gmail.com>
2019-04-26 10:00:00 +0200

    Fix linting

--- src/log/adapters/http/get_opts.js
+++ src/log/adapters/http/get_opts.js
@@ -1,5 +1,3 @@
-// TODO: use global variable once dropping support for Node 8 and 9
-// eslint-disable-next-line no-shadow, node/prefer-global/url
 import { URL } from 'url'
 
 // Parse `opts.url`, also ensuring it is a valid URL
--- src/protocols/adapters/http/origin.js
+++ src/protocols/adapters/http/origin.js
@@ -1,5 +1,3 @@
-// TODO: use global variable once dropping support for Node 8 and 9
-// eslint-disable-next-line no-shadow, node/prefer-global/url
 import { URL, format as urlFormat } from 'url'
 
 // Retrieves full URL
