2ee0a99138c811abc7cc1dd81fa4b6b6054410bf
ehmicky <ehmicky@gmail.com>
2019-05-28 10:00:00 +0200

    Fix linting

--- src/bin/input.js
+++ src/bin/input.js
@@ -13,6 +13,7 @@ const mParseInput = function() {
   return { instruction, opts: optsA }
 }
 
+// eslint-disable-next-line import/no-unused-modules
 export const parseInput = monitor(mParseInput, 'cli')
 
 // CLI options parsing
