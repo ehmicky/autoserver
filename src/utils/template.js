5d77ab70ed2fc5648ebf6818cfe603971b271125
ehmicky <ehmicky@gmail.com>
2021-06-13 17:56:29 +0200

    Fix imports of mustache

--- src/utils/template.js
+++ src/utils/template.js
@@ -1,9 +1,9 @@
 import { promises as fs } from 'fs'
 
-import { render } from 'mustache'
+import mustache from 'mustache'
 
 export const renderTemplate = async function ({ template, data }) {
   const htmlFile = await fs.readFile(template, 'utf8')
-  const htmlString = render(htmlFile, data)
+  const htmlString = mustache.render(htmlFile, data)
   return htmlString
 }
