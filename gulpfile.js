e42b45a1f1f31a35de3d977de9ae385e12a77969
ehmicky <ehmicky@gmail.com>
2021-06-13 11:52:49 +0200

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,7 @@
+// eslint-disable-next-line import/no-unassigned-import, import/no-unresolved, node/no-missing-import
+import '@ehmicky/dev-tasks/register.js'
+
 // eslint-disable-next-line import/no-unresolved, node/no-missing-import
 export * from '@ehmicky/dev-tasks'
+
+export { runProd, runDev, runDebug } from './gulp/run.js'
