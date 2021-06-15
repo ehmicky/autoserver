9b803de376f7cd27ea1cdd75ab2a2e1ad67f8b6b
ehmicky <ehmicky@gmail.com>
2021-06-15 21:12:22 +0200

    Use ES modules in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,2 @@
-'use strict'
-
-const gulpSharedTasks = require('@ehmicky/dev-tasks')
-
-gulpSharedTasks.buildRegister()
-
-const { runProd, runDev, runDebug } = require('./gulp/run.js')
-
-module.exports = {
-  ...gulpSharedTasks,
-  runProd,
-  runDev,
-  runDebug,
-}
+// eslint-disable-next-line import/no-unresolved, node/no-missing-import
+export * from '@ehmicky/dev-tasks'
