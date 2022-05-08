f2b1b1e61c76257f09c89168b6f55b6c7931a4d2
ehmicky <ehmicky@gmail.com>
2022-05-08 23:20:33 +0200

    Fix comment

--- src/middleware/protocol/requestid.js
+++ src/middleware/protocol/requestid.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid'
 
 // Assigns unique ID (UUIDv4) to each request
-// Available in mInput, events, system paramers and metadata
+// Available in mInput, events, system parameters and metadata
 export const setRequestid = function ({ metadata }) {
   const requestid = uuidv4()
 
