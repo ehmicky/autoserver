b630770b74a480180625e933b429a9aeb5218a8b
ehmicky <ehmicky@gmail.com>
2023-10-22 17:34:55 +0100

    Fix linting

--- src/serverinfo/versions.js
+++ src/serverinfo/versions.js
@@ -2,13 +2,12 @@ import { dirname } from 'node:path'
 import { version as nodeVersion } from 'node:process'
 import { fileURLToPath } from 'node:url'
 
-import { readPackageUpSync } from 'read-pkg-up'
+import { readPackageUp } from 'read-pkg-up'
 
 // Caches it.
-// TODO: use async instead
 const {
   packageJson: { version: autoserverVersion },
-} = readPackageUpSync({ cwd: dirname(fileURLToPath(import.meta.url)) })
+} = await readPackageUp({ cwd: dirname(fileURLToPath(import.meta.url)) })
 
 // Retrieve environment-specific versions
 export const getVersionsInfo = () => {
